15887d09a55ac4aa6fd860a2adc9c3aaaa54bda1
Rocco Sangellino <rocco.sangellino@zinnia.com>
2024-02-06 12:49:43 -0700

    remove starter docs

--- apps/docs/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ["@zdx/eslint-config/next.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-};
