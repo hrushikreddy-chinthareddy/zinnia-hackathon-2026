376d23a606307f95d646c0b197397156c0c40d6b
Rocco Sangellino <rocco.sangellino@zinnia.com>
2024-02-06 18:00:22 -0700

    updated bloom files

---
+++ packages/bloom/src/components/label/Label.tsx
@@ -0,0 +1,33 @@
+import React, { PropsWithChildren, ReactNode } from 'react';
+
+import classes from './Label.module.css';
+
+export type LabelProps = {
+  /**
+   * Interactive elements that add additional context to the value
+   */
+  interactiveElements?: ReactNode[];
+  /**
+   * Pass id of associated element to connect label to the value or input it is describing
+   */
+  labelFor: string;
+} & PropsWithChildren
+
+export const Label: React.FC<LabelProps> = ({ children, interactiveElements, labelFor }) => {
+  return (
+    <div className={classes.container}>
+      <label
+        htmlFor={labelFor}
+        className={classes.text}
+      >
+        {children}
+      </label>
+      {interactiveElements &&
+        interactiveElements.map((elem, index) => (
+          <div key={`interactive-${index}`} className={classes.item}>
+            {elem}
+          </div>
+        ))}
+    </div>
+  );
+};
