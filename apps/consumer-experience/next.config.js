3cbb737a95f94796926232765f8cfe268b5337bf
Rocco Sangellino <rocco.sangellino@zinnia.com>
2024-02-10 12:50:25 -0700

    updating configs

--- apps/consumer-experience/next.config.js
+++ apps/consumer-experience/next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  transpilePackages: ["@zdx/bloom"],
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"],
+      use: ['@svgr/webpack'],
     });
 
     return config;
--- packages/bloom/vite.config.ts
+++ packages/bloom/vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
       },
     },
     rollupOptions: {
-      external: ["react"],
+      external: ["react", "react/jsx-runtime"],
       output: {
         globals: {
           react: "React",
