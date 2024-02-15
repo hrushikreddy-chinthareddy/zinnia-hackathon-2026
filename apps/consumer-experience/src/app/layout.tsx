d043a3962c12a422b947efc775cd37779b9d6759
mclarkson-zinnia <mackenzie.clarkson@zinnia.com>
2024-02-15 07:47:26 -0800

    Updates to global

--- apps/consumer-experience/src/app/layout.tsx
+++ apps/consumer-experience/src/app/layout.tsx
@@ -27,7 +27,6 @@ export default async function RootLayout({
   children: React.ReactNode;
 }) {
   const session = await getSession();
-  console.log('session', session);
 
   if (!session) {
     return (
