3ca85f6e2b5625457adc4a6179a850ab41d943a1
Ximena Girasol <ximena.girasol@zinnia.com>
2024-02-16 20:21:42 +0000

    restore deleted files

---
+++ packages/bloom/src/components/icon/index.ts
@@ -0,0 +1,2 @@
+export { Icon } from "./Icon";
+export { IconType, type IconProps, Icons } from "./types";
---
+++ packages/bloom/src/stories/icon/Icon.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta } from "@storybook/react";
+
+import { Icon, IconProps, IconType } from "../../components/icon";
+
+export default {
+  title: "Components/Icon",
+  component: Icon,
+  tags: ["autodocs"],
+  args: {
+    type: IconType.CLOUD,
+  },
+  argTypes: {
+    color: {
+      control: {
+        type: "color",
+        default: "#000",
+      },
+    },
+  },
+} as Meta<typeof Icon>;
+
+export const IconExample = (args: IconProps) => <Icon {...args} />;
