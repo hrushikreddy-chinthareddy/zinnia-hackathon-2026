a0ac693b275625aab51db716eb40dc59739f700b
mclarkson-zinnia <mackenzie.clarkson@zinnia.com>
2024-03-01 14:09:02 -0800

    Add story options

--- packages/bloom/src/stories/button-group/ButtonGroup.stories.tsx
+++ packages/bloom/src/stories/button-group/ButtonGroup.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { ButtonGroup } from '@/components/button-group/ButtonGroup';
 import { ButtonGroupProps } from '@/components/button-group/types';
-import { Label } from '@/components';
+import { Icon, IconType, Label } from '@/components';
 
 const meta: Meta<typeof ButtonGroup> = {
   title: 'Components/ButtonGroup',
@@ -44,6 +44,41 @@ export const Inactive: StoryType = {
       { children: <span>Button Item 1</span>, value: 'button1' },
       { children: <span>Button Item 2</span>, value: 'button2' },
       { children: <span>Button Item 3</span>, value: 'button3' },
+    ],
+  },
+};
+
+export const InactiveWithLabel: StoryType = {
+  args: {
+    label: <Label labelFor="buttonGroupTestInactive">Label</Label>,
+    id: 'buttonGroupTestInactive',
+    inactive: true,
+    items: [
+      { children: <span>Button Item 1</span>, value: 'button1' },
+      { children: <span>Button Item 2</span>, value: 'button2' },
+      { children: <span>Button Item 3</span>, value: 'button3' },
+    ],
+  },
+};
+
+export const IconsOnly: StoryType = {
+  args: {
+    items: [
+      { children: <Icon type={IconType.CLOUD} />, value: 'cloud1' },
+      { children: <Icon type={IconType.CLOUD} />, value: 'cloud2' },
+      { children: <Icon type={IconType.CLOUD} />, value: 'cloud3' },
+    ],
+  },
+};
+
+export const IconsOnlyWithLabel: StoryType = {
+  args: {
+    label: <Label labelFor="buttonGroupTestIcons">Label</Label>,
+    id: 'buttonGroupTestIcons',
+    items: [
+      { children: <Icon type={IconType.CLOUD} />, value: 'cloud1' },
+      { children: <Icon type={IconType.CLOUD} />, value: 'cloud2' },
+      { children: <Icon type={IconType.CLOUD} />, value: 'cloud3' },
     ],
   },
 };
