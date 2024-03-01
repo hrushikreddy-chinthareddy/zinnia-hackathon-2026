2421a303241ed9d7860ebd82035c8edc2ba031e2
Christopher Bosserman <christopherpbosserman@gmail.com>
2024-03-01 15:55:50 -0500

    chips

---
+++ packages/bloom/src/stories/chip-x/ChipX.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { ChipX, ChipXProps } from "../../components/chip-x";
+
+const meta: Meta<typeof ChipX> = {
+  title: "Components/ChipX",
+  component: ChipX,
+  tags: ["autodocs"],
+  args: {
+    label: "Chip",
+    onDelete: () => {
+      console.log("Deleting chip");
+    },
+  },
+  parameters: {
+    layout: "centered",
+  },
+};
+
+export default meta;
+
+type StoryType = StoryObj<ChipXProps>;
+
+export const Default: StoryType = {};
