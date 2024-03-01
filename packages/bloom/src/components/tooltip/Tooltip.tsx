906dd59ad087299608141f0b115b504e8193e69a
Caryn McCarthy <caryn.mccarthy@policygenius.com>
2024-03-01 14:58:42 -0500

    tooltip

---
+++ packages/bloom/src/components/tooltip/Tooltip.tsx
@@ -0,0 +1,28 @@
+
+import * as RadixTooltip from '@radix-ui/react-tooltip';
+import clsx from 'clsx';
+
+import { zIndexOrder } from '@/utils/zIndexOrder';
+
+import { TooltipPlacement, TooltipProps, getPlacementProps } from './utils';
+import styles from './tooltip.module.css';
+
+export const Tooltip = ({ tooltipClassName, trigger, placement = TooltipPlacement.BottomRight, children}: TooltipProps) => {
+  const { side, align } = getPlacementProps(placement);
+
+  return (
+    <RadixTooltip.Provider>
+      <RadixTooltip.Root open>
+        <RadixTooltip.Trigger className={styles.tooltipTrigger}
+         style={{ zIndex: zIndexOrder.CardPopoverTrigger }}>
+          {trigger}
+        </RadixTooltip.Trigger>
+        <RadixTooltip.Portal>
+          <RadixTooltip.Content side={side} align={align} style={{ zIndex: zIndexOrder.Popover }} className={clsx(tooltipClassName, styles.tooltipContainer)}>
+            <span className="typography-content-body-sm">{children}</span>
+          </RadixTooltip.Content>
+        </RadixTooltip.Portal>
+      </RadixTooltip.Root>
+    </RadixTooltip.Provider>
+  );
+};
---
+++ packages/bloom/src/stories/tooltip/Tooltip.stories.tsx
@@ -0,0 +1,59 @@
+import { Meta, StoryObj } from "@storybook/react";
+import InfoIcon from "../../tokens/svg-assets/icons/circles/circle-info.svg";
+
+import { Tooltip, TooltipProps, TooltipPlacement} from '@/components/tooltip'
+
+const meta: Meta<typeof Tooltip> = {
+  title: "Components/Tooltip",
+  component: Tooltip,
+  tags: ['autodocs'],
+  args: {
+    children: "Tooltip",
+    placement: TooltipPlacement.TopLeft,
+    trigger:  <InfoIcon width={16} height={16} />,
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ marginTop: '50%', marginLeft: '50%'}}>
+        <Story />
+      </div>
+    ),
+  ],
+  argTypes: {
+    placement: {
+      options: [
+        TooltipPlacement.BottomLeft,
+        TooltipPlacement.BottomRight,
+        TooltipPlacement.TopLeft,
+        TooltipPlacement.TopRight,
+      ],
+      defaultValue: TooltipPlacement.BottomLeft,
+    },
+  },
+}
+
+export default meta;
+type StoryType = StoryObj<TooltipProps>;
+
+export const Default: StoryType = {};
+
+export const MoreContent: StoryType = {
+  args: {
+    children: (
+      <div>
+        <p
+          style={{
+            paddingBottom: "var(--measure-dimension-padding-xl, 0.75rem)",
+          }}
+        >
+          Your premium is the amount you pay periodically for insurance
+          coverage. What’s shown here is your next scheduled payment.
+        </p>
+         <p
+        >
+          Your base coverage is some other amount
+        </p>
+      </div>
+    )
+  }
+};
