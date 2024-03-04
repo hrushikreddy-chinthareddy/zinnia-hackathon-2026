789da8e6a2d6825294c22b66c10f5067f333a689
alysiadillon <alysia.dillon@life.io>
2024-03-04 08:46:53 -0600

    removed comments and unused lines. removed centered params for story as well.

--- packages/bloom/src/stories/radio/radio.stories.tsx
+++ packages/bloom/src/stories/radio/radio.stories.tsx
@@ -2,15 +2,9 @@ import { Meta, StoryObj } from '@storybook/react';
 
 import { Radio, RadioProps } from '@/components/radio';
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta: Meta<typeof Radio> = {
   title: "Components/Radio",
   component: Radio,
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
-    layout: "centered",
-  },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ["autodocs"],
   args: {
     isDisabled: false,
@@ -33,10 +27,6 @@ const meta: Meta<typeof Radio> = {
         value: 'option3'
       },
     ]
-  },
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    // backgroundColor: { control: 'color' },
   },
 };
 
