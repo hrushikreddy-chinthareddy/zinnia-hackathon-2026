61b9a793dffd76849623daac494be9b49faaeb71
Caryn McCarthy <caryn.mccarthy@policygenius.com>
2024-03-04 09:29:53 -0500

    remove story centered param

--- packages/bloom/src/stories/checkbox/Checkbox.stories.tsx
+++ packages/bloom/src/stories/checkbox/Checkbox.stories.tsx
@@ -6,10 +6,6 @@ import { Checkbox, CheckboxProps } from '@/components/checkbox';
 const meta: Meta<typeof Checkbox> = {
   title: "Components/Checkbox",
   component: Checkbox,
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
-    layout: "centered",
-  },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ["autodocs"],
   args: {
