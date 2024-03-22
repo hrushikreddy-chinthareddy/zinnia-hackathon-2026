5203a8474b538ee86843a185e8be760574ce1559
mclarkson-zinnia <mackenzie.clarkson@zinnia.com>
2024-03-22 15:12:57 -0700

    Use generic page on login session and not found

--- apps/consumer-experience/src/app/layout.tsx
+++ apps/consumer-experience/src/app/layout.tsx
@@ -3,6 +3,8 @@ import localFont from 'next/font/local';
 import type { Metadata } from 'next';
 
 import './styles/globals.css';
+// TODO: THIS NEEDS TO BE DYNAMIC!!!!
+import './styles/everly/theme.css';
 
 // disable because NextJS needs this to be exported from this file
 // eslint-disable-next-line react-refresh/only-export-components
