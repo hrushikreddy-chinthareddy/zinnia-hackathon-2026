1b83fb0991f166271747e56dd0141164ec3ab743
mclarkson-zinnia <mackenzie.clarkson@zinnia.com>
2024-04-17 15:57:41 -0700

    unignore coverage

---
+++ apps/consumer-experience/src/app/(authenticated)/policies/[planCode]/[policyNumber]/(interior)/coverage/page.tsx
@@ -0,0 +1,107 @@
+import { IconType, Label } from '@zinnia/bloom/internal/components';
+import { DEFAULT_UNAVAILABLE_STRING } from '@zinnia/utils';
+import clsx from 'clsx';
+import { Suspense } from 'react';
+
+import { ClickableCardContainer } from '@/components/clickable-card-container/ClickableCardContainer';
+import { FieldData } from '@/components/field-data/FieldData';
+import { Footer } from '@/components/footer/Footer';
+import { HeaderBreadcrumb } from '@/components/header-breadcrumb/HeaderBreadcrumb';
+import { InfoCard } from '@/components/info-card/InfoCard';
+import { NoDataAvailable } from '@/components/no-data-available/NoDataAvailable';
+import { getCoverage } from '@/services/policy';
+import { formatUSDollars } from '@/utils/currency';
+
+import styles from './coverage.module.css';
+
+export default async function Beneficiaries({
+  params,
+}: {
+  params: {
+    planCode: string;
+    policyNumber: string;
+  };
+}) {
+  const planCode = params.planCode;
+  const policyNumber = params.policyNumber;
+
+  const { data, error } = await getCoverage({
+    planCode,
+    policyNumber,
+  });
+
+  const pageHeader = (
+    <>
+      <HeaderBreadcrumb
+        className={styles.coverageHeader}
+        title="Increase Coverage"
+      />
+    </>
+  );
+
+  if (error || data === null) {
+    return (
+      <>
+        {pageHeader}
+        <NoDataAvailable />
+      </>
+    );
+  }
+
+  const { totalCoverageAmount, maximumCoverageIncreaseAmount } = data;
+
+  const CURRENT_COVERAGE = 'Current Coverage';
+  const INCREASE_COVERAGE = 'Available coverage increase';
+
+  const coverageContent = (amount: number | null | undefined) => {
+    if (amount && typeof amount === 'number') {
+      return (
+        <p className="typography-content-value">{formatUSDollars(amount)}</p>
+      );
+    } else {
+      return (
+        <p className="typography-content-body-sm">
+          {DEFAULT_UNAVAILABLE_STRING}
+        </p>
+      );
+    }
+  };
+
+  return (
+    <div className="container">
+      <Suspense fallback={<div style={{ height: '100vh' }}>Loading...</div>}>
+        {pageHeader}
+        <div className="card-container">
+          <InfoCard iconType={IconType.LIGHTBULB}>
+            <p className="typography-content-body-sm">
+              Coverage increases may require additional underwriting and may
+              have tax consequences. Let us walk you through your options, so
+              you can find the right amount of coverage for you.
+            </p>
+          </InfoCard>
+          <ClickableCardContainer>
+            <div className={styles.coverageValues}>
+              <FieldData Label={<Label>{CURRENT_COVERAGE}</Label>}>
+                {coverageContent(totalCoverageAmount)}
+              </FieldData>
+              <FieldData Label={<Label>{INCREASE_COVERAGE}</Label>}>
+                {coverageContent(maximumCoverageIncreaseAmount)}
+              </FieldData>
+            </div>
+          </ClickableCardContainer>
+        </div>
+        <p className="typography-content-body-bold ">
+          Do you want to increase your coverage? Call{' '}
+          <a
+            className={clsx('typography-nav-links-inline', styles.phoneNumber)}
+            href="+18002322222"
+          >
+            1-800-232-2222
+          </a>{' '}
+          to begin the process.
+        </p>
+        <Footer className={styles.coverageFooter} />
+      </Suspense>
+    </div>
+  );
+}
