ad053efa982a11381ca17823bf4c840464e48f1f
mclarkson-zinnia <mackenzie.clarkson@zinnia.com>
2024-05-03 08:33:37 -0700

    remove comment

--- apps/consumer-experience/src/app/(authenticated)/policies/[planCode]/[policyNumber]/(policy-details)/layout.tsx
+++ apps/consumer-experience/src/app/(authenticated)/policies/[planCode]/[policyNumber]/(policy-details)/layout.tsx
@@ -13,7 +13,6 @@ export default async function AuthenticatedLayout({
 }) {
   return (
     <>
-      {/* TODO: move this into flex with header....sad */}
       <HeaderPolicyDetails
         planCode={params.planCode}
         policyNumber={params.policyNumber}
