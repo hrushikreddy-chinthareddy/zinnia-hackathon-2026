14b189b6ff005dc717a7ae6e0b31caf18d82aebe
Ryan Olsen <ryanolsen5@gmail.com>
2024-05-06 09:58:31 -0600

    Exporting button-group css module

--- packages/bloom/src/components/button-group/index.ts
+++ packages/bloom/src/components/button-group/index.ts
@@ -1,2 +1,5 @@
+import ButtonGroupStyles from "./ButtonGroup.module.css";
+
 export { ButtonGroup } from "./ButtonGroup";
 export { type ButtonGroupProps } from "./types";
+export { ButtonGroupStyles };
