bfbf9713d2f732d091e5941a935baaca423b931e
Ryan Olsen <ryanolsen5@gmail.com>
2024-05-29 15:19:43 -0600

    Tab Group component

--- packages/bloom/src/components/tab-group/index.tsx
+++ packages/bloom/src/components/tab-group/index.tsx
@@ -1,2 +1,11 @@
 import tabGroupStyles from './TabGroup.module.css';
-export { tabGroupStyles };
+import { TabGroup, TabList, TabTrigger, TabContent } from './TabGroup';
+import { TabsProps } from '@radix-ui/react-tabs';
+export {
+  tabGroupStyles,
+  TabGroup,
+  TabList,
+  TabTrigger,
+  TabContent,
+  type TabsProps,
+};
