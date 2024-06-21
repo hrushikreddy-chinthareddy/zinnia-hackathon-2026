646ed96b5f9974ac879ba1a03db0c264d4ca3376
mclarkson-zinnia <mackenzie.clarkson@zinnia.com>
2024-06-21 11:16:30 -0700

    Add progress bar component

---
+++ apps/consumer-experience/src/components/progress-bar-steps/ProgressBar.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { ProgressBarSteps } from './ProgressBarSteps';
+
+const meta: Meta<typeof ProgressBarSteps> = {
+  component: ProgressBarSteps,
+  title: 'Components/ProgressBarSteps',
+  args: {},
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+export const Default: StoryObj<typeof ProgressBarSteps> = {
+  args: {
+    totalSteps: 4,
+    currentStep: 3,
+  },
+};
