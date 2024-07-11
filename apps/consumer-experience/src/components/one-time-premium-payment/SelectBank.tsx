fdba645492445d6ea98ff9f95753fa39350c1afa
mclarkson-zinnia <mackenzie.clarkson@zinnia.com>
2024-07-11 16:40:25 -0700

    Update export

--- apps/consumer-experience/src/components/one-time-premium-payment/SelectBank.tsx
+++ apps/consumer-experience/src/components/one-time-premium-payment/SelectBank.tsx
@@ -7,9 +7,12 @@ import {
   SideSheet,
 } from '@zinnia/bloom/components';
 import { ChangeEvent, useState } from 'react';
+<<<<<<< HEAD
 
 import { useFeatureFlags } from '@/hooks/use-feature-flags';
 import { FEATURE_FLAGS } from '@/utils/optimizely/flags';
+=======
+>>>>>>> d31f7c4 (Update export)
 
 import premiumStyles from './OneTimePremiumPayment.module.css';
 import { NoDataAvailable } from '../no-data-available/NoDataAvailable';
