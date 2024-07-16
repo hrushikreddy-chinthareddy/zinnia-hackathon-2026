57a61b07714c2261d10a551684f641b88f2e4a09
mclarkson-zinnia <mackenzie.clarkson@zinnia.com>
2024-07-15 17:16:40 -0700

    return from function

--- apps/consumer-experience/src/components/payment-summary-step/utils.ts
+++ apps/consumer-experience/src/components/payment-summary-step/utils.ts
@@ -1,6 +1,4 @@
 import { TransactionSummaryItem } from './PaymentSummaryStep';
 
-export const calculateTotalDeposit = (values: TransactionSummaryItem[]) => {
-  const total = values.reduce((acc, total) => acc + total.value, 0);
-  return total;
-};
+export const calculateTotalDeposit = (values: TransactionSummaryItem[]) =>
+  values.reduce((acc, total) => acc + total.value, 0);
