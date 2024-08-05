13a765efe0c49edb14ef3b7963c8c3fd78d802e3
mclarkson-zinnia <mackenzie.clarkson@zinnia.com>
2024-08-05 11:52:54 -0700

    Clean up

--- apps/consumer-experience/src/services/policy/transformers.ts
+++ apps/consumer-experience/src/services/policy/transformers.ts
@@ -373,7 +373,6 @@ export const transformPaymentHistory = (
       break;
     case TransactionType.PAYMENT_ONE_TIME_PREMIUM:
     case TransactionType.ONE_TIME_PREMIUM:
-      console.log('is it this one?!', paymentHistoryObject);
       paymentHistoryObject.amount =
         transactionType === TransactionType.PAYMENT_ONE_TIME_PREMIUM
           ? paymentAmount
