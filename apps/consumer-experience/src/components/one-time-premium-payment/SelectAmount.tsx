571d7b0991f812b8a7fa5995bfb3a8eeef87a77b
mclarkson-zinnia <mackenzie.clarkson@zinnia.com>
2024-08-09 15:14:21 -0700

    Add premium payment popover

--- apps/consumer-experience/src/components/one-time-premium-payment/SelectAmount.tsx
+++ apps/consumer-experience/src/components/one-time-premium-payment/SelectAmount.tsx
@@ -1,6 +1,12 @@
 'use client';
 
-import { Button, Label } from '@zinnia/bloom/components';
+import {
+  Button,
+  Icon,
+  IconType,
+  Label,
+  Popover,
+} from '@zinnia/bloom/components';
 import dayjs from 'dayjs';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
@@ -8,6 +14,7 @@ import { Controller, useForm } from 'react-hook-form';
 
 import { DEFAULT_DATE_FORMAT, ZAHARA_DATE_FORMAT } from '@/utils/dates';
 
+import { CancelDialogLink } from './CancelDialogLink';
 import { FormStepWrapper } from './FormStepWrapper';
 import premiumStyles from './OneTimePremiumPayment.module.css';
 import { getStepInfo, Steps } from './steps';
@@ -16,11 +23,11 @@ import { FieldStatus } from '../field/types';
 import { FieldValue } from '../field/value/FieldValue';
 import { useOttp } from '../providers/one-time-premium-payment/OttpContext';
 import { OttpAction } from '../providers/one-time-premium-payment/types';
-import { CancelDialogLink } from './CancelDialogLink';
 
 export const dateInvalidMessage = 'Please enter a valid date';
 export const dateOutOfRangeMessage =
   'Date must be between today and next 60 days';
+export const PREMIUM_PAYMENT_AMOUNT = 'Premium payment amount';
 
 const sixtyDaysInFutureDay = dayjs().add(60, 'day').format('YYYY-MM-DD');
 const dateWithinSixtyDayRange = (date: string) => {
@@ -148,7 +155,43 @@ export const SelectAmount = ({
                     : FieldStatus.DEFAULT
                 }
                 errorMessage={formState.errors.paymentAmount?.message}
-                label={<Label>Premium payment amount</Label>}
+                label={
+                  <Label
+                    interactiveElements={[
+                      <Popover
+                        key={PREMIUM_PAYMENT_AMOUNT}
+                        title={PREMIUM_PAYMENT_AMOUNT}
+                        trigger={
+                          <Icon
+                            type={IconType.CIRCLE_INFO}
+                            color="var(--color-base-icon-icon-tooltip, #ff7500)"
+                            width={16}
+                            height={16}
+                          />
+                        }
+                      >
+                        <p>
+                          Enter the amount you would like to pay into your
+                          policy. Keep in mind there are limits (set by federal
+                          laws) to the amount you can pay without impacting your
+                          coverage or losing tax advantages.
+                        </p>
+                        {/* // TODO: where does this value come from? */}
+                        {/* <p>
+                          Currently, you may pay up to [MEC limit value, CVAT
+                          value, Guideline premium value, whichever is the
+                          lesser of] without changing the nature of your policy
+                          or it's tax advantages. If you'd like to pay more than
+                          this, we suggest speaking with a financial
+                          professional (like a tax advisor) who can help walk
+                          you through the consequences first.
+                        </p> */}
+                      </Popover>,
+                    ]}
+                  >
+                    {PREMIUM_PAYMENT_AMOUNT}
+                  </Label>
+                }
                 placeholder=""
                 // TODO: what should this be?
                 name="one-time-premium-payment"
