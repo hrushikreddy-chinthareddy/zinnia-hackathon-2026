c95d7d4c4f61f1e2e56879e47981efb2cf43303e
Ed Sztukowski <edsztukowski@gmail.com>
2024-08-12 11:08:50 -0600

    Fix broken api route (#298)
    * do not need the env
    
    * only include the public base URL if it exists

--- apps/consumer-experience/src/queries/policy-queries.ts
+++ apps/consumer-experience/src/queries/policy-queries.ts
@@ -6,9 +6,17 @@ export const getPolicyProfile = async (
   planCode: string,
   policyNumber: string
 ) => {
+  // Why this? Because you get weird invalid URL errors locally. How can we improve this so its more global?
+  // https://stackoverflow.com/questions/74966208/next-js-typeerror-failed-to-parse-url-from-api-projects-or-error-connect-econ
+  // The only weird thing is that this shouldnt be used server side, so i dont know why it is even throwing the error message about the invalid URL
+
+  const baseUrl =
+    process.env.NODE_ENV === 'development'
+      ? process.env.NEXT_PUBLIC_BASE_URL
+      : '';
   const response: ApiResponse<PolicyProfile> = await (
     await ClientApi.get(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/policies/${planCode}/${policyNumber}/profile`
+      `${baseUrl}/api/policies/${planCode}/${policyNumber}/profile`
     )
   ).json();
   if (response.error || !response) {
