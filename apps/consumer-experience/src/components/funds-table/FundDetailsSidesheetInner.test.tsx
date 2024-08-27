7c0dd24984b9c788c41826c7d9fd7225ec8811d9
mclarkson-zinnia <mackenzie.clarkson@zinnia.com>
2024-08-27 15:49:57 -0700

    mroe tests

--- apps/consumer-experience/src/components/funds-table/FundDetailsSidesheetInner.test.tsx
+++ apps/consumer-experience/src/components/funds-table/FundDetailsSidesheetInner.test.tsx
@@ -5,6 +5,7 @@ import { FundDetailsSidesheetInner } from './FundDetailsSidesheetInner';
 
 describe('FundDetailsSidesheetInner', () => {
   // Renders fund details correctly when valid fundDetails are provided
+  // HOLDING
   it('should render fund details correctly when valid fundDetails are provided', () => {
     const fundDetails = {
       fundId: '1',
@@ -23,6 +24,49 @@ describe('FundDetailsSidesheetInner', () => {
     expect(getByText('Interest rate')).toBeInTheDocument();
     expect(getByText('Fund value')).toBeInTheDocument();
     expect(getByText('Next sweep date')).toBeInTheDocument();
+  });
+
+  // FIXED
+  it('should render fund details correctly when valid fundDetails are provided', () => {
+    const fundDetails = {
+      fundId: '1',
+      fundName: 'Test Fund',
+      totalFundValue: 1000,
+      fundAccountType: FundAccountTypeEnum.FIXED,
+      allocationPercentage: 50,
+      interestRate: 5,
+      sweepDate: '2023-12-31',
+    };
+
+    const { getByText } = render(
+      <FundDetailsSidesheetInner fundDetails={fundDetails} />
+    );
+
+    expect(getByText('Fund type')).toBeInTheDocument();
+    expect(getByText('Fund value')).toBeInTheDocument();
+    expect(getByText('Allocation')).toBeInTheDocument();
+    expect(getByText('Interest rate')).toBeInTheDocument();
+  });
+
+  // Indexed
+  it('should render fund details correctly when valid fundDetails are provided', () => {
+    const fundDetails = {
+      fundId: '1',
+      fundName: 'Test Fund',
+      totalFundValue: 1000,
+      fundAccountType: FundAccountTypeEnum.INDEXED,
+      allocationPercentage: 50,
+      interestRate: 5,
+      sweepDate: '2023-12-31',
+    };
+
+    const { getByText } = render(
+      <FundDetailsSidesheetInner fundDetails={fundDetails} />
+    );
+
+    expect(getByText('Fund type')).toBeInTheDocument();
+    expect(getByText('Fund value')).toBeInTheDocument();
+    expect(getByText('Allocation')).toBeInTheDocument();
   });
 
   // Returns null when fundDetails is null or undefined
