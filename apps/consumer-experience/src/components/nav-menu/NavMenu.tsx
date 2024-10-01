4dccd4f77ff0afb33ddbe83cb0bc1e7a68aba7c6
mclarkson-zinnia <mackenzie.clarkson@zinnia.com>
2024-10-01 09:14:30 -0700

    Style updates for nav

--- apps/consumer-experience/src/components/nav-menu/NavMenu.tsx
+++ apps/consumer-experience/src/components/nav-menu/NavMenu.tsx
@@ -7,33 +7,47 @@ import Link from 'next/link';
 import { useFeatureFlags } from '@/hooks/use-feature-flags';
 import { FEATURE_FLAGS } from '@/utils/optimizely/flags';
 import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+const navUrls = {
+  account: '/my-account',
+  allPolicies: '/coverage',
+};
 
 export const NavMenu = () => {
   const { data: featureFlagData } = useFeatureFlags();
   const showAnnuities = featureFlagData?.[FEATURE_FLAGS.ANNUITIES_MODE];
   const pathname = usePathname();
 
-  console.log('pathname', pathname);
-
   return (
     <div className={styles.navList}>
       <ul>
-        <li>
+        <li
+          className={clsx({
+            [styles.active as string]: pathname === navUrls.account,
+          })}
+        >
           <Icon type={IconType.CIRCLE_USER} />
           <Link href="/my-account">Account profile</Link>
         </li>
         {!showAnnuities && (
-          <li>
+          <li
+            className={clsx({
+              [styles.active as string]: pathname === navUrls.allPolicies,
+            })}
+          >
             <Icon type={IconType.MATCHES} />
             <Link href="/coverage">My policies</Link>
           </li>
         )}
         {/* TODO: add logic to show different carriers with product counts */}
       </ul>
-      <Link href="api/logout">
-        <Icon type={IconType.LOGOUT} />
-        <span>Sign out</span>
-      </Link>
+      <div style={{ position: 'relative' }}>
+        <Link href="api/logout">
+          <Icon type={IconType.LOGOUT} />
+          <span>Sign out</span>
+        </Link>
+      </div>
     </div>
   );
 };
