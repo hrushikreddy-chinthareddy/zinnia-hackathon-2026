1e285989bab9af40fb4a9465c2b6215c68f8eb75
Ed Sztukowski <edsztukowski@gmail.com>
2024-10-15 10:50:26 -0600

    Revert "try to set the refresh cookie to an absolute domain (#386)" (#387)
    This reverts commit e919b5ea3782efdfbb43b680ef298abd3e61cbb4.

--- apps/consumer-experience/src/components/providers/RefreshRouterManager.tsx
+++ apps/consumer-experience/src/components/providers/RefreshRouterManager.tsx
@@ -28,7 +28,11 @@ const RefreshRouterManager = ({ children }: { children: React.ReactNode }) => {
     const refreshRouter = Cookies.get(REFRESH_ROUTER_COOKIE_KEY) === '1';
 
     if (refreshRouter) {
-      Cookies.remove(REFRESH_ROUTER_COOKIE_KEY);
+      Cookies.remove(REFRESH_ROUTER_COOKIE_KEY, {
+        domain:
+          process.env.NEXT_PUBLIC_AUTH0_COOKIE_DOMAIN ||
+          process.env.VERCEL_BRANCH_URL,
+      });
       router.refresh();
     }
   }, [pathname, router]);
