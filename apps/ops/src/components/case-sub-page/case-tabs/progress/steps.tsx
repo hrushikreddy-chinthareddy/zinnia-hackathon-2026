64f66e77df6b72ec43ff297c76352705a7cc76a7
Brian Byers <brian.byers@zinnia.com>
2024-10-18 09:33:41 -0600

    fix(DEPU-2754, DEPU-2756, DEPU-2511): brought over Bryn's decision tag

--- apps/ops/src/components/case-sub-page/case-tabs/progress/steps.tsx
+++ apps/ops/src/components/case-sub-page/case-tabs/progress/steps.tsx
@@ -1,5 +1,6 @@
 import { TFunction, useTranslation } from 'next-i18next';
 import React, { ReactNode } from 'react';
+import { Tag, TagVariant } from '@zinnia/bloom/components';
 
 import Content, { ContentVariant } from '@deps/components/content/content';
 import NavElement, { NavElementSize, NavElementType } from '@deps/components/nav-element/nav-element';
@@ -19,6 +20,60 @@ import Exceptions from './exceptions';
 import { formatTimestamp, TransformedStep } from './progress-tab-helpers';
 import Tasks from './tasks';
 
+enum StepTagIds {
+    UnderwritingAccepted = 'underwritingEvalutaion.underwritingDecisionApproved',
+    UnderwritingAdverse = 'underwritingEvaluation.underwritingDecisionAdverse',
+    UnderwritingDeclined = 'underwritingEvalutaion.underwritingDecisionDeclined',
+    SuitabilityReview = 'suitabilityReview.suitabilityReview',
+    UserAccepted = 'userDecision.acceptedOffer',
+    UserRejected = 'userDecision.rejectedOffer',
+    UserExpired = 'userDecision.expireOffer',
+    UserAmended = 'userDecision.requestedOfferAmendment',
+}
+
+const stepResultTag = (step: TransformedStep, t: TFunction): ReactNode => {
+    let text;
+    switch (step.id) {
+        // Underwriting
+        case StepTagIds.UnderwritingAccepted:
+            text = t('caseOverview.tabs.accepted');
+            break;
+        case StepTagIds.UnderwritingAdverse:
+            text = t('caseOverview.tabs.adverse');
+            break;
+        case StepTagIds.UnderwritingDeclined:
+            text = t('caseOverview.tabs.declined');
+            break;
+        // Suitability
+        case StepTagIds.SuitabilityReview:
+            if (step.status === 'COMPLETED') {
+                text = t('caseOverview.tabs.approved');
+                break;
+            } else {
+                text = t('caseOverview.tabs.declined');
+                break;
+            }
+        // User Decision
+        case StepTagIds.UserAccepted:
+            text = t('caseOverview.tabs.accepted');
+            break;
+        case StepTagIds.UserRejected:
+            text = t('caseOverview.tabs.rejected');
+            break;
+        case StepTagIds.UserExpired:
+            text = t('caseOverview.tabs.expired');
+            break;
+        case StepTagIds.UserAmended:
+            text = t('caseOverview.tabs.amended');
+            break;
+        default:
+            break;
+    }
+    if (text) {
+        return <Tag text={text} variant={TagVariant.White} />;
+    }
+    return;
+};
 const getStepStatusIconTooltip = (step: TransformedStep, t: TFunction): ReactNode => {
     let icon = null;
     let tooltipTitle = null;
@@ -88,6 +143,7 @@ const Step = ({ step, ...rest }: { step: TransformedStep } & React.HTMLAttribute
                     <div className="flex flex-row items-center gap-2 justify-self-start">
                         {getStepStatusIconTooltip(step, t)}
                         <Content contentClassName="min-w-max" variant={ContentVariant.BodySm} details={step.name} />
+                        {stepResultTag(step, t)}
                     </div>
                     {hasSidesheet && (
                         <NavElement type={NavElementType.Button} size={NavElementSize.Small} onClick={openSidesheet}>
