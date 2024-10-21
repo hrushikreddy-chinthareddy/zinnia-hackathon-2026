3c16cb0d30abaa7b3232c938a7ae79383e87e717
Bryn Newell <bryn.newell@zinnia.com>
2024-10-21 15:16:14 -0600

    updating case search card to meet needs and moving the result table to a new file

--- apps/ops/src/pages/cases/index.tsx
+++ apps/ops/src/pages/cases/index.tsx
@@ -1,14 +1,10 @@
 import { Session, getSession, withPageAuthRequired } from '@auth0/nextjs-auth0';
-import { Table, TableBody, TableCell, TableHeader, TableHeaderCell, TableRow } from '@zinnia/bloom/components';
 import { GetServerSidePropsContext } from 'next';
 import dynamic from 'next/dynamic';
-import Image from 'next/image';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 
-import { CaseDetailField, OwnerWithOthers } from '@deps/components/card/case-search-card/case-search-card';
-import ChipStatus from '@deps/components/chip-status/chip-status';
 import { FieldSize } from '@deps/components/fields/field';
 import FilterButton from '@deps/components/filter-button/filter-button';
 import NavElement, { NavElementSize, NavElementType } from '@deps/components/nav-element/nav-element';
@@ -17,6 +13,7 @@ import { PageLoader, PageLoaderVariant } from '@deps/components/page-loader/page
 import { PageHead } from '@deps/components/page-title';
 import SearchBar from '@deps/components/search/search-bar';
 import SelectSimple from '@deps/components/select/select';
+import { CaseResultTable } from '@deps/components/table/case-result-table';
 import Typography, { TypographyVariant } from '@deps/components/typography/typography';
 import { TranslationFiles } from '@deps/config/translations';
 import { AE_FGA_ROLE } from '@deps/constants/advisors-excel';
@@ -38,11 +35,9 @@ import {
     isSearchValueObjectEmpty,
     toggleLabels,
 } from '@deps/helpers/case-management';
-import { getAgents, getPolicyOwners } from '@deps/helpers/parties';
 import { doesUserHavePagePermissions, getUserData } from '@deps/helpers/query-data.helper';
 import { ALL_LOCALES, DEFAULT_LOCALE } from '@deps/helpers/routing.helper';
 import { storage } from '@deps/helpers/sessionStorage.helper';
-import { formatDateDescriptionList, formatSSN, toTitleCase } from '@deps/helpers/string.helper';
 import { useSegmentPageTracker } from '@deps/hooks/useSegmentPageTracker';
 import { Statuses } from '@deps/models/case/case';
 import { UserPermission } from '@deps/models/user-profile';
@@ -52,11 +47,9 @@ import { CaseSearchQuery, CaseStatsQuery } from '@deps/queries/cases';
 import { FgaRelation } from '@deps/types/fga';
 import { PolicySearchKeys, SearchViewQuery } from '@deps/types/search';
 import { SegmentPageName, SegmentTrackedPageProps } from '@deps/types/segment-analytics';
-import { getCarrierLogoByClientId } from '@deps/utils/carriers';
 import nextI18nextConfig from 'next-i18next.config';
 
 import useCaseFilterQueryStore from './caseFilterQueryStore';
-import styles from './styles.module.css';
 
 // Lazy Loaded Components
 const SideSheetRefineResults = dynamic(() => import('@deps/containers/side-sheet-refine-results/side-sheet-refine-results'));
@@ -357,142 +350,10 @@ const CaseManagementDashboard = ({ authorizedCarriers, isAdvisorsExcel, user }: 
 
         return (
             <>
-                {/* {cases.map(singleCase => (
-                    <CaseSearchCard
-                        key={`case-search-card-${singleCase.id}`}
-                        {...singleCase}
-                        {...singleCase.additionalData}
-                        searchValues={caseManagementFilters.searchValue}
-                    />
-                ))} */}
-                <Table>
-                    <TableHeader>
-                        <TableRow>
-                            <TableHeaderCell>Case/ID</TableHeaderCell>
-                            <TableHeaderCell>Case Status</TableHeaderCell>
-                            <TableHeaderCell>Owner/SSN</TableHeaderCell>
-                            <TableHeaderCell>Policy</TableHeaderCell>
-                            <TableHeaderCell>Agent/SSN</TableHeaderCell>
-                            {/* to do - add onClick handleSort method */}
-                            <TableHeaderCell sortable>Created</TableHeaderCell>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {cases.map(singleCase => {
-                            const policyOwners = singleCase.parties ? getPolicyOwners(singleCase.parties) : [];
-                            const entities = policyOwners
-                                .slice(1)
-                                .map(owner => ({ name: toTitleCase(owner.fullName), ssn: formatSSN(owner.ssn) }));
-                            // to do - can i use full name here instead of this maddness?
-                            const ownerName = policyOwners.length
-                                ? (policyOwners?.[0]?.firstName || '') + ' ' + (policyOwners?.[0]?.lastName || '')
-                                : null;
-                            const ssn = policyOwners.length ? policyOwners[0].ssn : undefined;
-
-                            const ownerComponentProps = {
-                                text: toTitleCase(ownerName?.trim()),
-                                // highlights: [searchValues?.ownerFirstName, searchValues?.ownerLastName].filter(Boolean) as string[],
-                                entities,
-                                truncate: true,
-                            };
-
-                            const agents = getAgents(singleCase?.parties || []);
-                            const agentSsn = agents.length ? agents[0].ssn : undefined;
-                            const otherAgents = agents
-                                .slice(1)
-                                .map(owner => ({ name: toTitleCase(owner.fullName), ssn: formatSSN(owner.ssn) }));
-                            const agentComponentProps = {
-                                text: toTitleCase(agents?.[0]?.fullName),
-                                // to do - are highlights needed for agents?
-                                // highlights: [searchValues?.ownerFirstName, searchValues?.ownerLastName].filter(Boolean) as string[],
-                                entities: otherAgents,
-                                truncate: true,
-                            };
-
-                            const imageSrc = getCarrierLogoByClientId(singleCase.carrier);
-
-                            return (
-                                <TableRow key={`case-search-card-${singleCase.id}`}>
-                                    <TableCell>
-                                        <div className="flex flex-col">
-                                            <Typography variant={TypographyVariant.BodySm} className="block">
-                                                {singleCase.processSubType ? toTitleCase(singleCase.processSubType) : singleCase.process}
-                                            </Typography>
-                                            {/* to do - color isn't right */}
-                                            <Typography variant={TypographyVariant.BodySm} className={styles.detail}>
-                                                {singleCase.id}
-                                            </Typography>
-                                        </div>
-                                    </TableCell>
-                                    <TableCell>
-                                        <ChipStatus
-                                            status={singleCase.caseStatus}
-                                            data-testid="chip-status"
-                                            classNames="whitespace-nowrap"
-                                        />
-                                    </TableCell>
-                                    {/* to do - add showOwnerInfo logic */}
-                                    <TableCell>
-                                        <div className="flex flex-col">
-                                            {policyOwners.length > 1 ? (
-                                                <OwnerWithOthers {...ownerComponentProps} />
-                                            ) : (
-                                                <CaseDetailField pii={true} {...ownerComponentProps} />
-                                            )}
-                                            <CaseDetailField
-                                                pii={true}
-                                                text={formatSSN(ssn)}
-                                                className={styles.detail}
-                                                // highlights={searchValues?.ssn ? [searchValues?.ssn] : null}
-                                            />
-                                        </div>
-                                    </TableCell>
-                                    <TableCell>
-                                        <div className="flex items-center gap-2">
-                                            {/* to do - where does showLogo logic come from? */}
-                                            <Tooltip placement={PopoverPlacement.TopRight} body={singleCase.carrier} isTabbable={false}>
-                                                <div className="flex h-6 w-6 items-center justify-center rounded border-2 border-gray-100">
-                                                    <Image
-                                                        src={imageSrc}
-                                                        alt={`${singleCase.carrier} icon`}
-                                                        width={24}
-                                                        height={24}
-                                                        role="presentation"
-                                                        aria-hidden="true"
-                                                    />
-                                                </div>
-                                            </Tooltip>
-                                            <CaseDetailField
-                                                pii={true}
-                                                text={singleCase.policyNumber}
-                                                // highlights={searchValues?.policyNumber ? [searchValues?.policyNumber] : null}
-                                            />
-                                        </div>
-                                    </TableCell>
-                                    <TableCell>
-                                        <div className="flex flex-col">
-                                            {agents.length > 1 ? (
-                                                <OwnerWithOthers {...agentComponentProps} />
-                                            ) : (
-                                                <CaseDetailField pii={true} {...agentComponentProps} />
-                                            )}
-                                            <CaseDetailField
-                                                pii={true}
-                                                text={formatSSN(agentSsn)}
-                                                className={styles.detail}
-                                                // highlights={searchValues?.ssn ? [searchValues?.ssn] : null}
-                                            />
-                                        </div>
-                                    </TableCell>
-                                    <TableCell>{formatDateDescriptionList(new Date(singleCase.createdAt || ''))}</TableCell>
-                                </TableRow>
-                            );
-                        })}
-                    </TableBody>
-                </Table>
+                <CaseResultTable cases={cases} />
             </>
         );
-    }, [caseTableData, caseManagementFilters.searchValue]);
+    }, [caseTableData]);
 
     // Sidesheet Support
     const sideSheet = useSideSheetContext();
