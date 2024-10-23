ddb97e9ec3eecc160d6b6c23fbb4bb37fe8b9d6b
gaurisharma1811 <gauri.sharma@zinnia.com>
2024-10-23 15:35:58 +0530

    added express check to sbgc withdrawal form

--- apps/ops/src/containers/otp/withdrawal-forms/sbgc-withdrawal-form.helper.tsx
+++ apps/ops/src/containers/otp/withdrawal-forms/sbgc-withdrawal-form.helper.tsx
@@ -403,6 +403,21 @@ export default function getSbgcConfig(t: TFunction) {
             },
         },
         {
+            label: t('distributionMethod.expressCheck'),
+            value: FormDisbursementSelections.ExpressCheck,
+            fields: null,
+            getDefaultPayload: () => {
+                return DEFAULT_DISBURSEMENT_UPDATE;
+            },
+            generatePayloadFromSelection: () => {
+                return {
+                    ...getDefaultFormDisbursementValues(),
+                    paymentMethod: { text: PaymentMailType.Check },
+                    paymentMailType: { text: PaymentMailType.ExpressCheck },
+                };
+            },
+        },
+        {
             label: t('distributionMethod.brokerageAccount'),
             value: FormDisbursementSelections.Brokerage,
             fields: [
