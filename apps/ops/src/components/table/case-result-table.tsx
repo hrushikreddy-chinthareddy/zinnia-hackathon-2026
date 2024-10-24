5cd864d9ec570d37cb10b23417980383590a5a15
Bryn Newell <bryn.newell@zinnia.com>
2024-10-24 11:37:20 -0600

    adding the date logic for created at and moving the link to open the case detail

--- apps/ops/src/components/table/case-result-table.tsx
+++ apps/ops/src/components/table/case-result-table.tsx
@@ -1,4 +1,5 @@
 import { Link, Table, TableBody, TableCell, TableHeader, TableHeaderCell, TableRow } from '@zinnia/bloom/components';
+import dayjs from 'dayjs';
 import Image from 'next/image';
 import { useTranslation } from 'react-i18next';
 
@@ -9,7 +10,8 @@ import { TranslationFiles } from '@deps/config/translations';
 import { getStatusDetails } from '@deps/containers/case-redesign-sub-page';
 import { calculateDaysAgo } from '@deps/helpers/case-management';
 import { getAgents, getPolicyOwners } from '@deps/helpers/parties';
-import { formatDateDescriptionList, formatSSN, toTitleCase } from '@deps/helpers/string.helper';
+import { formatSSN, toTitleCase } from '@deps/helpers/string.helper';
+import { getTimeAgoUnitValue } from '@deps/hooks/useStatusInfo';
 import { Case } from '@deps/models/case/case';
 import { CaseDetailsTabValues, DEFAULT_ERROR_STRING } from '@deps/types/constants';
 import { SearchViewQuery } from '@deps/types/search';
@@ -61,6 +63,8 @@ export const CaseResultTable = ({ cases, searchValues }: CaseResultTableProps) =
         <Table>
             <TableHeader>
                 <TableRow>
+                    {/* This header cell is needed so the link can come first in the Table Row, without it the table body will shift right one column too far */}
+                    <TableHeaderCell className="sr-only">View Case Details</TableHeaderCell>
                     <TableHeaderCell>Case/ID</TableHeaderCell>
                     <TableHeaderCell>Case Status</TableHeaderCell>
                     <TableHeaderCell>Owner/SSN</TableHeaderCell>
@@ -116,8 +120,34 @@ export const CaseResultTable = ({ cases, searchValues }: CaseResultTableProps) =
                         caseNumber: String(singleCase.policyNumber).split('').join(' '),
                     });
 
+                    const getTimeText = () => {
+                        let text;
+                        const { unit, count } = getTimeAgoUnitValue(singleCase.createdAt) || {};
+                        const currentYear = dayjs().year();
+                        const createdYear = dayjs(singleCase.createdAt).year();
+                        // to do - i don't think i need this because dayjs will handle the translation too right?
+                        // const timeText = t('temporal.timeago', { formattedDate: '', count: count, unit: unit }).trim();
+                        if (unit === 'hour' || unit === 'minute') {
+                            text = dayjs(singleCase.createdAt).fromNow();
+                        } else if (createdYear === currentYear) {
+                            // to do - i think i'd rather use all dayjs but this is an option too
+                            // text = formatDateDescriptionList(new Date(singleCase.createdAt || ''));
+                            text = dayjs(singleCase.createdAt).format('M/D/YYYY');
+                        } else {
+                            text = dayjs(singleCase.createdAt).format('MMM D, YYYY');
+                        }
+                        return text;
+                    };
+
                     return (
                         <TableRow key={`case-search-card-${singleCase.id}`} className={styles.row}>
+                            {/* This lives as a visibly hidden link instead of as a click handler on the table row for acccessibility concerns. Nested interactive elements are not allowed */}
+                            <Link
+                                className={styles.caseLink}
+                                href={`/cases/${singleCase.id}/${CaseDetailsTabValues.progress}`}
+                                aria-label={viewCaseText}
+                                text={viewCaseText}
+                            />
                             <TableCell>
                                 <div className="flex flex-col">
                                     <Typography variant={TypographyVariant.BodySm} className="block">
@@ -179,14 +209,7 @@ export const CaseResultTable = ({ cases, searchValues }: CaseResultTableProps) =
                                     <CaseDetailField pii={true} text={formatSSN(agentSsn)} className={styles.detail} />
                                 </div>
                             </TableCell>
-                            {/* to do - change this to be the "hours ago" if it's today */}
-                            <TableCell>{formatDateDescriptionList(new Date(singleCase.createdAt || ''))}</TableCell>
-                            <Link
-                                className={styles.caseLink}
-                                href={`/cases/${singleCase.id}/${CaseDetailsTabValues.progress}`}
-                                aria-label={viewCaseText}
-                                text={viewCaseText}
-                            />
+                            <TableCell>{getTimeText()}</TableCell>
                         </TableRow>
                     );
                 })}
