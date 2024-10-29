818f302b1577f9a58baaf5760f4dd366d4c8ed11
mclarkson-zinnia <mackenzie.clarkson@zinnia.com>
2024-10-28 17:11:16 -0700

    fix for setting cookies in middleware to use in same render

--- apps/consumer-experience/src/middleware.ts
+++ apps/consumer-experience/src/middleware.ts
@@ -1,8 +1,4 @@
 import { LineOfBusiness } from '@zinnia/api-types/types/sor';
-import {
-  RequestCookies,
-  ResponseCookies,
-} from 'next/dist/server/web/spec-extension/cookies';
 import { NextResponse, type NextRequest } from 'next/server';
 
 import { RouteKey, getRedirectUrl, routeMap } from '@/route-map';
@@ -44,6 +40,7 @@ import {
   getCarrierSubdomainById,
   isValidCarrierSubdomain,
 } from './utils/carriers';
+import { applySetCookie } from './utils/cookies';
 import { lineOfBusinessUrlPath } from './utils/data';
 import { applyThemeCookies } from './utils/theme';
 import {
@@ -51,33 +48,6 @@ import {
   getSubdomain,
   prependSubdomain,
 } from './utils/url';
-
-/**
- * NextJS doesn't foward the headers to react server components.
- * This method creates a new header and copies the request headers over
- * it then sets the headers on the response so the react server components have the updated cookies and headers
- * @param req NextRequest
- * @param res NextResponse
- */
-function applySetCookie(req: NextRequest, res: NextResponse): void {
-  // parse the outgoing Set-Cookie header
-  const setCookies = new ResponseCookies(res.headers);
-  // Build a new Cookie header for the request by adding the setCookies
-  const newReqHeaders = new Headers(req.headers);
-  const newReqCookies = new RequestCookies(newReqHeaders);
-  setCookies.getAll().forEach(cookie => newReqCookies.set(cookie));
-  // set “request header overrides” on the outgoing response
-  NextResponse.next({
-    request: { headers: newReqHeaders },
-  }).headers.forEach((value, key) => {
-    if (
-      key === 'x-middleware-override-headers' ||
-      key.startsWith('x-middleware-request-')
-    ) {
-      res.headers.set(key, value);
-    }
-  });
-}
 
 const applyMockCookies = (req: NextRequest, res: NextResponse<unknown>) => {
   if (!isMockAllowed()) {
@@ -125,6 +95,7 @@ export async function middleware(req: NextRequest) {
   const resetDeliveryDateActive = true;
 
   applyThemeCookies(req, resNext);
+  applySetCookie(req, resNext);
 
   if (session) {
     const searchParmas = req.nextUrl.searchParams;
