52ccd69a12497a6f01b1ecd952e22976507079dc
mclarkson-zinnia <mackenzie.clarkson@zinnia.com>
2024-11-04 10:51:09 -0800

    Add variable for progress bar outline

--- apps/consumer-experience/src/components/progress-bar-steps/ProgressBarSteps.tsx
+++ apps/consumer-experience/src/components/progress-bar-steps/ProgressBarSteps.tsx
@@ -91,7 +91,7 @@ export const ProgressBarSteps = ({
               style={{
                 width: `${100 / stepsWithoutFinal}%`,
                 color: isStepComplete
-                  ? 'var(--color-base-border-border-secondary-color)'
+                  ? 'var(--progress-bar-step-outline)'
                   : 'var(--color-base-surface-surface-bold)',
               }}
               className={styles.stepContainer}
