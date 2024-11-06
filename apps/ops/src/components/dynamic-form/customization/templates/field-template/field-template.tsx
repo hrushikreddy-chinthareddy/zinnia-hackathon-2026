27bc4be073d2d86b2b42c3a9c235f3e455137392
PANKAJ AGARWAL <a.pankaj1982@gmail.com>
2024-11-06 18:31:00 +0530

    add text messages

--- apps/ops/src/components/dynamic-form/customization/templates/field-template/field-template.tsx
+++ apps/ops/src/components/dynamic-form/customization/templates/field-template/field-template.tsx
@@ -1,4 +1,4 @@
-import { FieldTemplateProps } from '@rjsf/utils';
+import { FieldTemplateProps, getUiOptions } from '@rjsf/utils';
 import { Divider, Label, Tooltip } from '@zinnia/bloom/components';
 
 import { ReactComponent as CircleInfoIcon } from '@deps/styles/elements/icons/circles/circle-info.svg';
@@ -6,47 +6,26 @@ import { ReactComponent as CircleInfoIcon } from '@deps/styles/elements/icons/ci
 import styles from './field-template.module.css';
 export function FieldTemplate(props: FieldTemplateProps) {
     const { id, label, displayLabel, required, description, errors, children, readonly, formData, classNames, uiSchema } = props;
+    const uiOptions = getUiOptions(uiSchema);
+    const helpText = uiOptions.help;
 
-    const helpText = uiSchema && uiSchema['ui:options']?.['help'] ? uiSchema['ui:options']?.['help'] : '';
-
-    const helpInformation = helpText ? (
+    const helpInformation = helpText && (
         <Tooltip trigger={<CircleInfoIcon height={'16px'} width={'16px'} className="text-primary" />}>{helpText}</Tooltip>
-    ) : (
-        ''
     );
-
     const fieldLabel = label ? `${label} ${required ? '*' : ''}` : '';
-
-    if (readonly) {
-        return (
-            <>
-                <div>{classNames?.indexOf('divider') !== -1 && <Divider direction="horizontal" />}</div>
-                <div className={styles.children}>
-                    {typeof formData === 'string' && (
-                        <div>
-                            <Label labelFor={id}>{fieldLabel}</Label>
-                            {formData}
-                        </div>
-                    )}
-                    {typeof formData !== 'string' && <>{children}</>}
-                </div>
-            </>
-        );
-    } else {
-        return (
-            <>
-                <div>{classNames?.indexOf('divider') !== -1 && <Divider direction="horizontal" />}</div>
-                <div className={styles.children}>
-                    {displayLabel && (
-                        <Label labelFor={id} interactiveElements={[helpInformation]}>
-                            {fieldLabel}
-                        </Label>
-                    )}
-                    {description}
-                    {children}
-                    {errors}
-                </div>
-            </>
-        );
-    }
+    return (
+        <>
+            <div>{classNames?.indexOf('divider') !== -1 && <Divider direction="horizontal" color="subtle" />}</div>
+            <div className={styles.children}>
+                {displayLabel && (
+                    <Label labelFor={id} interactiveElements={[helpInformation]}>
+                        {fieldLabel}
+                    </Label>
+                )}
+                {description}
+                {readonly && typeof formData === 'string' ? formData : children}
+                {errors}
+            </div>
+        </>
+    );
 }
