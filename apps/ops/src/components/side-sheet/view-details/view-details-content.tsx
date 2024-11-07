c7cc2f5445b56e13b3f681af37813d3399a9aab2
aditis26 <aditi.sharma@zinnia.com>
2024-11-07 13:46:15 +0530

    date formatted

--- apps/ops/src/components/side-sheet/view-details/view-details-content.tsx
+++ apps/ops/src/components/side-sheet/view-details/view-details-content.tsx
@@ -11,7 +11,7 @@ function formatDate(dateString: string) {
     const day = date.getDate().toString().padStart(2, '0');
     const month = (date.getMonth() + 1).toString().padStart(2, '0');
     const year = date.getFullYear().toString();
-    return `${month}-${day}-${year}`;
+    return `${month}/${day}/${year}`;
 }
 export function ViewDetailsContent({ qualificationType, contractValue, policyDate }: ViewDetailsContentProps) {
     const formattedPolicyDate = formatDate(policyDate);
