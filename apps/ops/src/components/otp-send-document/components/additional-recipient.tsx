ba45cbdac595fa0ea1753754865c98b862e8b49d
Vijayalaxmi Sambhane <vijayalaxmi.sambhane@zinnia.com>
2024-11-11 16:33:56 +0530

    CMW-24619 fixes

--- apps/ops/src/components/otp-send-document/components/additional-recipient.tsx
+++ apps/ops/src/components/otp-send-document/components/additional-recipient.tsx
@@ -1,11 +1,13 @@
-import { ChipX } from '@zinnia/bloom/components';
+import { ChipX, PopoverPlacement } from '@zinnia/bloom/components';
 import { useTranslation } from 'next-i18next';
 import { useState } from 'react';
 import xss from 'xss';
 
 import Field, { FieldSize, FieldType } from '@deps/components/fields/field';
+import Popover from '@deps/components/popover/popover';
 import Typography, { TypographyVariant } from '@deps/components/typography/typography';
 import { FormValidationErrors } from '@deps/models/case/withdrawal/case';
+import { ReactComponent as CircleInfoIcon } from '@deps/styles/elements/icons/circles/circle-info.svg';
 
 import { validateEmail } from '../correspondence';
 
@@ -50,16 +52,34 @@ const AdditionalRecipient = ({ classNames, emails, setEmails, setError }: Additi
         <div className={classNames}>
             <Typography variant={TypographyVariant.Label} className="w-full">
                 {t(`correspondence.email`)}
+                <Popover
+                    placement={PopoverPlacement.TopRight}
+                    title={t('correspondence.additionalRecipientTooltipTitle') as string}
+                    body={t('correspondence.additionalRecipientTooltipBody') as string}
+                >
+                    <CircleInfoIcon height={'16px'} width={'16px'} className="text-primary" />
+                </Popover>
             </Typography>
+
             <div className={`border-2 border-gray-200 px-2 pt-1 rounded-lg`}>
                 {emails.map(email => (
                     <ChipX label={email as string} key={email} onDelete={() => deleteEmail(email)} className="my-1" />
                 ))}
                 <Field
                     onChange={e => {
-                        setEmail(xss(e?.target?.value));
+                        setEmail(xss(e?.target?.value.trim()));
                     }}
                     handleEnterKey={() => {
+                        addEmail(email);
+                    }}
+                    onKeyPress={e => {
+                        if (e.key === ',' || e.key === ';') {
+                            e.preventDefault();
+                            addEmail(email);
+                        }
+                    }}
+                    onBlur={e => {
+                        e.preventDefault();
                         addEmail(email);
                     }}
                     value={email as string}
