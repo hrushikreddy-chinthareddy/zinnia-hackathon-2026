c5b65445f9e890d75688c862471084aec6da89a3
Rocco Sangellino <60282108+RoccoSangellino@users.noreply.github.com>
2024-11-13 09:58:57 -0700

    removed old spec transformer for dashboard (#558)

--- apps/ops/src/models/case/case.ts
+++ apps/ops/src/models/case/case.ts
@@ -156,39 +156,6 @@ export interface DashboardStatsElementResponse {
     values?: DashboardStatsElementResponse[];
 }
 
-/**
- * The response object returned by the {@link getCaseDashboardStats} endpoint.
- */
-export interface CaseDashboardStatsResponseOld {
-    /**
-     * Array of elements that contain the stats, example:
-     * see {@link DashboardSearchRequest} for more details.
-     *
-     */
-    element: DashboardStatsElementResponseOld[];
-}
-
-export interface DashboardStatsElementResponseOld {
-    /**
-     * The count of occurences that match the filter
-     */
-    count: number;
-    /**
-     * The @see {@link GroupByOptions} passed from the request
-     */
-    key: string;
-    /**
-     * The name of the label that matches the value of the GroupBy parameter
-     * @see {@link Case} for field names
-     */
-    name: string;
-    /**
-     * Array of elements that contain the stats, example:
-     * see {@link DashboardStatsElementResponse} for more details.
-     */
-    values?: CaseDashboardStatsResponseOld;
-}
-
 export interface CaseDashboardStatsErrorResponse {
     data: {
         err: string;
