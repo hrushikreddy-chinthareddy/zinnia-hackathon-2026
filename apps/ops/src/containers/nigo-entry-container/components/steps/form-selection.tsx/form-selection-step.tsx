4f505f2200c82ba02cb931cff50cd396dcbb6804
Leena Bodhe <103044931+leenabodhe@users.noreply.github.com>
2024-11-13 17:12:40 +0530

    Added helper for client code check

--- apps/ops/src/containers/nigo-entry-container/components/steps/form-selection.tsx/form-selection-step.tsx
+++ apps/ops/src/containers/nigo-entry-container/components/steps/form-selection.tsx/form-selection-step.tsx
@@ -17,10 +17,10 @@ import { ApiVersion } from '@deps/models/case/enums';
 import { TaskApiVersionMapper } from '@deps/models/case/helpers';
 import { AvailableFormsTransaction, SendDocumentFormParts } from '@deps/models/case/send-document';
 import { TaskStatus } from '@deps/models/case/task-instance';
-import { Carrier } from '@deps/models/case/withdrawal/case';
 import { Policy } from '@deps/models/policy/sor-policy';
 import { updateTask } from '@deps/queries/api/v2/task';
 
+import { getFormSelectionConfig } from './form-selection.helper';
 import TransactionDocumentSelection from './transaction-document-selection';
 import { useNigoEntry } from '../../nigo-entry-provider';
 
@@ -33,6 +33,7 @@ type FormSelectionProps = {
 };
 
 function FormSelectionStep({ availableFormsTransactions, policy, documentData, clientCode, ctiCallNumber = '' }: FormSelectionProps) {
+    const { isFormIdRequired } = getFormSelectionConfig(clientCode);
     const { t } = useTranslation(TranslationFiles.COMMON, { keyPrefix: 'nigoEntry.formSelection' });
     const { goToNext } = useWorkflow();
     const [error, setError] = useState<string>('');
@@ -93,8 +94,8 @@ function FormSelectionStep({ availableFormsTransactions, policy, documentData, c
 
 
     useEffect(() => {
-       if ((clientCode !== Carrier.MASS && !isNullEmptyOrUndefined(document?.selected?.formId))
-            ||(clientCode === Carrier.MASS && !isNullEmptyOrUndefined(transactionType.selected) && !isNullEmptyOrUndefined(transactionSubType.selected))) {
+       if ((isFormIdRequired && !isNullEmptyOrUndefined(document?.selected?.formId))
+            || (!isFormIdRequired && !isNullEmptyOrUndefined(transactionType.selected) && !isNullEmptyOrUndefined(transactionSubType.selected))) {
             setFormProgram(prevFormProgram => {
                 return {
                     ...prevFormProgram,
@@ -119,18 +120,18 @@ function FormSelectionStep({ availableFormsTransactions, policy, documentData, c
                 };
             });
         }
-    }, [clientCode, document?.selected, setFormProgram, transactionSubType?.selected, transactionType?.selected]);
+    }, [clientCode, document?.selected, isFormIdRequired, setFormProgram, transactionSubType.selected, transactionType.selected]);
 
     const handleStepContinue = useCallback(async() => {
-        if (clientCode !== Carrier.MASS && isNullEmptyOrUndefined(document?.selected?.formId)) {
+        if (isFormIdRequired  == true && isNullEmptyOrUndefined(document?.selected?.formId)) {
             return setError(t('errors.selectForm') as string);
-        } else if (clientCode === Carrier.MASS && (isNullEmptyOrUndefined(transactionType.selected) || isNullEmptyOrUndefined(transactionSubType.selected))) {
+        } else if (isFormIdRequired === false && (isNullEmptyOrUndefined(transactionType.selected) || isNullEmptyOrUndefined(transactionSubType.selected))) {
             return setError(t('errors.selectOptions') as string);
         } else {
             await submit();
             goToNext();
         }
-    }, [clientCode, document?.selected?.formId, goToNext, submit, t, transactionSubType.selected, transactionType.selected]);
+    }, [document?.selected?.formId, goToNext, isFormIdRequired, submit, t, transactionSubType.selected, transactionType.selected]);
 
     return (
         <WorkflowCard
