9c46a944d0a127c13b259ce9ca405d74020c5951
saurav-0083 <saurav.rath@zinnia.com>
2024-11-19 17:34:36 +0530

    Email logic fix SSW Edit

--- apps/ops/src/components/ssw-edit/ssw-edit-helper.ts
+++ apps/ops/src/components/ssw-edit/ssw-edit-helper.ts
@@ -29,13 +29,13 @@ export type UpdatedProgram = {
 
 export const getDocumentSource = (documentId: string): ChannelType => {
     if (documentId) {
-        if (documentId.includes('-O-') || documentId.includes('-MAN-')) {
-            return ChannelType.Phone;
-        } else {
+        if (documentId.includes('-EM-')) {
             return ChannelType.Email;
+        } else {
+            return ChannelType.Phone;
         }
     } else {
-        return ChannelType.Email;
+        return ChannelType.Phone;
     }
 };
 
