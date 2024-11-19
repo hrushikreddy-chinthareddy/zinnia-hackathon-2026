14f8d6be267b8ecf8bf5fcff160442cd636448f9
Anssam <anssam@breathelife.com>
2024-11-19 09:26:25 -0500

    feat: add pom directory + pom_experience feature flag

--- apps/ops/src/utils/optimizely/flags.ts
+++ apps/ops/src/utils/optimizely/flags.ts
@@ -63,4 +63,5 @@ export enum FEATURE_FLAGS {
     CASE_STATS_DASHBOARD = 'case-management-case_stats_dashboard',
     CASE_INSIGHTS = 'case_insights',
     POLICY_FREE_LOOK_CANCELLATION = 'policy-management_feature_free-look-cancellation',
+    POM_EXPERIENCE = 'pom-experience',
 }
