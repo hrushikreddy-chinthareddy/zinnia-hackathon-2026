86ce88b356305a20b56b2c748a78a083fbfba97f
saurav-0083 <saurav.rath@zinnia.com>
2024-11-22 21:33:37 +0530

    Phone signaute section

--- apps/ops/src/components/ssw-edit/ssw-update/ssw-operations.tsx
+++ apps/ops/src/components/ssw-edit/ssw-update/ssw-operations.tsx
@@ -11,7 +11,7 @@ import { FormSignature } from '@deps/models/case/withdrawal/case';
 
 import EditProgram from './edit-program';
 import { signaturesConfig } from '../bank-update/bank-update.helper';
-import { SswUpdateType } from '../ssw-edit-helper';
+import { getDocumentSource, SswUpdateType } from '../ssw-edit-helper';
 
 type SswOperationsProps = {
     document: DocumentData;
@@ -24,6 +24,7 @@ type SswOperationsProps = {
 const SswOperations = ({ document, programs, setSswUpdateView, programType, onProgramUpdate }: SswOperationsProps) => {
     const { t } = useTranslation(TranslationFiles.COMMON);
     const { formSignature } = useContext(FormDataContext);
+    const source = getDocumentSource(document.documentNumber);
 
     return (
         <div className="p-8">
@@ -31,7 +32,7 @@ const SswOperations = ({ document, programs, setSswUpdateView, programType, onPr
             {programs?.map((item: Program, index: number) => (
                 <EditProgram key={index} program={item} onTerminate={onProgramUpdate} onEdit={setSswUpdateView} document={document} />
             ))}
-            {document.source !== ChannelType.Phone && formSignature && (
+            {source !== ChannelType.Phone && formSignature && (
                 <SignatureValidations isFormStateReadOnly={false} config={signaturesConfig} />
             )}
         </div>
