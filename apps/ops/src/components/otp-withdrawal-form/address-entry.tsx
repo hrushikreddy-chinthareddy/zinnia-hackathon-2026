f6a435237014d7de47d726061bb91fd89223e53f
aditis26 <aditi.sharma@zinnia.com>
2024-11-25 13:27:40 +0530

    comment resolve

--- apps/ops/src/components/otp-withdrawal-form/address-entry.tsx
+++ apps/ops/src/components/otp-withdrawal-form/address-entry.tsx
@@ -82,7 +82,7 @@ export default function AddressEntry({
                     <Field
                         label={t(`mailingAddressLine2`) as string}
                         message={errors.addressLine2}
-                        onChange={e => setAddressLine2(xss(e.target.value.toUpperCase()))}
+                        onChange={e => setAddressLine2(xss(e.target.value?.toUpperCase()))}
                         size={FieldSize.Small}
                         type={FieldType.BaseActive}
                         value={addressLine2}
@@ -111,7 +111,7 @@ export default function AddressEntry({
                 <Field
                     label={t(`city`) as string}
                     message={errors.city}
-                    onChange={e => setCity(xss(e.target.value.toUpperCase()))}
+                    onChange={e => setCity(xss(e.target.value?.toUpperCase()))}
                     size={FieldSize.Small}
                     type={FieldType.BaseActive}
                     value={city}
