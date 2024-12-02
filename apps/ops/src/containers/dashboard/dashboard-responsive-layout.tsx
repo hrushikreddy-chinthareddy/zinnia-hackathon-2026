433d0f983eddca39cf79e36920f73a9830c33aeb
Ximena Girasol <ximena.girasol@zinnia.com>
2024-12-02 21:17:55 +0000

    make gap in info-card smaller

--- apps/ops/src/containers/dashboard/dashboard-responsive-layout.tsx
+++ apps/ops/src/containers/dashboard/dashboard-responsive-layout.tsx
@@ -20,7 +20,7 @@ export const DashboardResponsiveLayout = ({ children }: PropsWithChildren) => {
         <div className="fixed inset-0 z-50 grid h-full w-full place-items-center ">
             <CardInfo
                 icon={<ErrorIcon className="text-semantic-warning" height={50} width={50} />}
-                className="rounded border-2 border-dashed border-semantic-warning bg-white shadow-sm p-8 m-8 min-h-[300px] flex flex-col  gap-8"
+                className="rounded border-2 border-dashed border-semantic-warning bg-white shadow-sm p-8 m-8 min-h-[300px] flex flex-col  gap-2"
                 title="Screen width not supported"
                 subtitle="We are sorry, but the current screen width is not supported. Please open on a larger screen"
             />
