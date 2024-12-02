e680ecc0379cb7663aeb28654db05617117b1ebf
Ximena Girasol <ximena.girasol@zinnia.com>
2024-12-02 21:26:33 +0000

    remove double exception summary

--- apps/ops/src/pages/dashboard/issued-business/index.tsx
+++ apps/ops/src/pages/dashboard/issued-business/index.tsx
@@ -354,13 +354,6 @@ export const IssuedBusinessPage = ({ authorizedCarriers, brokerDealersSSR, compl
                             selectedSubprocess={selectedSubprocess}
                         />
                     </div>
-                    <div className="bg-white p-8 flex flex-col gap-8">
-                        <ExceptionSummary
-                            timeframe={timeframe}
-                            carrierOrBrokerDealer={carrierOrBrokerDealer}
-                            startDate={createdDateStart}
-                        />
-                    </div>
                 </CardContainer>
             </NoNavLayout>
         </DashboardResponsiveLayout>
