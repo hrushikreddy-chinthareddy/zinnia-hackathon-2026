89e9c45f98ee23e9830b317608c5206f036b38d8
Vijayalaxmi Sambhane <vijayalaxmi.sambhane@zinnia.com>
2024-12-02 20:41:00 +0530

    Added default date as fallback

--- apps/ops/src/components/otp-send-document/statement-selection.tsx
+++ apps/ops/src/components/otp-send-document/statement-selection.tsx
@@ -7,7 +7,7 @@ import 'react-pdf/dist/Page/TextLayer.css';
 import { Loader } from '@deps/components/page-loader';
 import { useWorkflow } from '@deps/contexts/WorkflowContainerContext';
 import { DocumentDisplayCode, PolicyDocument, PolicyDocuments } from '@deps/models/case/document';
-import { StatementTypes } from '@deps/models/case/send-statement';
+import { StatementStartYear, StatementTypes } from '@deps/models/case/send-statement';
 import { FormValidationErrors } from '@deps/models/case/withdrawal/case';
 import { Policy } from '@deps/models/policy/sor-policy';
 import { getCorrespondenceDocs } from '@deps/queries/api/documents';
@@ -113,7 +113,7 @@ function StatementSelection({ policy, applicableStatement, statements, setStatem
     const { t } = useTranslation(undefined, { keyPrefix: 'contactCenter' });
     const { goToNext } = useWorkflow();
 
-    const statementStartYear = policy.policyDates?.issueDate || '';
+    const statementStartYear = policy.policyDates?.issueDate || StatementStartYear;
     const [selectedStatementType, setSelectedStatementType] = useState<StatementTypes[]>(applicableStatement);
     const currentYear = dayjs().year().toString();
     const currentQuarter = `${currentYear}-Q${getQuarter(dayjs())}`;
--- apps/ops/src/models/case/send-statement.ts
+++ apps/ops/src/models/case/send-statement.ts
@@ -12,6 +12,8 @@ export enum StatementTypes {
     QuarterlyStatement = 'SOA',
 }
 
+export const StatementStartYear = '2022-Q1';
+
 export type StatementTypesResponse = {
     applicableStatement: StatementTypes[];
 };
