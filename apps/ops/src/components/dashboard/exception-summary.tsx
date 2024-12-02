c4f55a53872ce12ae8a846c4ad85a6c48bcb7e5d
Rocco Sangellino <rocco.sangellino@zinnia.com>
2024-12-02 13:40:43 -0700

    added AI stats to exception summary

--- apps/ops/src/components/dashboard/exception-summary.tsx
+++ apps/ops/src/components/dashboard/exception-summary.tsx
@@ -3,16 +3,20 @@ import * as Highcharts from 'highcharts';
 import HC_ACCESSIBILITY from 'highcharts/modules/accessibility';
 import HighchartsExporting from 'highcharts/modules/exporting';
 import HighchartsReact from 'highcharts-react-official';
-import { useMemo, useRef } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 
 import PageLoader from '@deps/components/page-loader/page-loader';
 import Typography, { TypographyVariant } from '@deps/components/typography/typography';
 import CardContainer from '@deps/containers/card-container/card-container';
+import { dashboardChartTitleFormat } from '@deps/helpers/dashboard/dashboard-helpers';
 import { wholeNumberFormatify } from '@deps/helpers/numbers.helper';
 import { convertToQueryString } from '@deps/helpers/routing.helper';
+import useCaseInsightsPermission from '@deps/hooks/useCaseInsights';
 import { DashboardStatsElementResponse } from '@deps/models/case/case';
 import { GroupByOptions } from '@deps/models/case/enums';
+import { getCaseInsights } from '@deps/queries/api/openai';
 import { ReactComponent as ChartBarsIcon } from '@deps/styles/elements/icons/illustrations/chart-bars.svg';
+import { ReactComponent as LightBulbIcon } from '@deps/styles/elements/icons/illustrations/light-bulb.svg';
 
 import useExceptionData from '../../pages/dashboard/issued-business/useExceptionData';
 import NavElement, { NavElementSize, NavElementType } from '../nav-element/nav-element';
@@ -159,6 +163,8 @@ export const ExceptionSummary = ({
     carrierOrBrokerDealer?: GroupByOptions.Carrier | GroupByOptions.BrokerDealerName;
     selectedSubprocess: string;
 }) => {
+    const [aiSummary, setAiSummary] = useState<string | null>(null);
+    const shouldShowCaseInsights = useCaseInsightsPermission();
     const [loading, exceptionData, statsResponse, filter] = useExceptionData({
         startDate,
         carrierOrBrokerDealer,
@@ -333,6 +339,42 @@ export const ExceptionSummary = ({
         };
     }, [statsResponse, sortedMonthly, processedData.weeklyCategories, processedData.monthlyCategories, processedData.weekly]);
 
+    const getOpenAiSummary = async (caseStats: DashboardStatsElementResponse[], processSubType: string) => {
+        try {
+            const summary = await getCaseInsights({
+                content: JSON.stringify(caseStats),
+                prompt: `You are an expert in all things new business application data. Your job is to summarize the data for business and executive users. They want simple and insightful information about the data provided to you. The data provided to you here are completed ${dashboardChartTitleFormat(
+                    processSubType,
+                    false
+                )} applications, but the ${dashboardChartTitleFormat(
+                    processSubType,
+                    false
+                )} applications encountered exceptions along their path to completion. The data is grouped by Carrier and then by Exception Category and the values represent an exception that occurred for a ${dashboardChartTitleFormat(
+                    processSubType,
+                    false
+                )} application. Avoid using phrases such as "the data". Your responses should be insightful and will be displayed on a UI as a summary for a module related to a distribution chart. Use percentages and real data where it makes sense. Keep it concise and to the point. Format number values to U.S. inclding commas where appropriate.`,
+            });
+            return summary;
+        } catch (error) {
+            return '';
+        }
+    };
+
+    useEffect(() => {
+        if (!shouldShowCaseInsights) {
+            return;
+        }
+        if (statsResponse?.length) {
+            getOpenAiSummary(statsResponse, selectedSubprocess).then(summary => {
+                if (summary) {
+                    setAiSummary(summary);
+                }
+            });
+        } else {
+            setAiSummary(`No exceptions for ${dashboardChartTitleFormat(selectedSubprocess)}.`);
+        }
+    }, [statsResponse, selectedSubprocess, shouldShowCaseInsights]);
+
     return (
         <CardContainer containerClassNames="rounded" classNames="!p-0" fullWidth={true}>
             <div className="flex flex-col xl:flex-row justify-between gap-4 w-full">
@@ -340,10 +382,27 @@ export const ExceptionSummary = ({
                     <Typography className="mb-1" variant={TypographyVariant.H2}>
                         {'Exception Summary'}
                     </Typography>
-                    <div className="flex-1 border-r-1 xl:border-r-0 xl:border-t-1 border-[#EDEDED]">
-                        <Typography className="mb-4 xl:mt-1" variant={TypographyVariant.BodySmBold}>
+                    <div className="flex-1 border-r-1 xl:border-r-0 xl:border-t-1 border-[#EDEDED] flex flex-col gap-4">
+                        <Typography className="xl:mt-1" variant={TypographyVariant.BodySmBold}>
                             Avg Monthly Exceptions / Total Cases
                         </Typography>
+                        {loading ? (
+                            <div className="grid gap-4 h-full mb-4 w-full place-content-center bg-[--color-base-surface-surface-tertiary]">
+                                <PageLoader />
+                            </div>
+                        ) : (
+                            <>
+                                {!!aiSummary?.length && (
+                                    <>
+                                        <div className="flex flow-col items-center align-middle gap-2">
+                                            <LightBulbIcon height={'24px'} width={'24px'} />
+                                            <Typography variant={TypographyVariant.LabelLg}>Insight</Typography>
+                                        </div>
+                                        <Typography variant={TypographyVariant.BodySm}>{aiSummary}</Typography>
+                                    </>
+                                )}
+                            </>
+                        )}
                         <ol className="flex flex-col gap-2 pr-4">
                             {sortedMonthly.map((stat, index) => (
                                 <li key={`stat-${index}-${stat.name}`}>
