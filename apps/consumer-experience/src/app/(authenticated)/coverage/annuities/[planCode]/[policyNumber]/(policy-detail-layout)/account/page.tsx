92f36e8dd60c74f7854b8e7e133dc0c9668fd99a
mclarkson-zinnia <mackenzie.clarkson@zinnia.com>
2024-12-06 13:27:27 -0800

    fix account value null handling and extra cards

--- apps/consumer-experience/src/app/(authenticated)/coverage/annuities/[planCode]/[policyNumber]/(policy-detail-layout)/account/page.tsx
+++ apps/consumer-experience/src/app/(authenticated)/coverage/annuities/[planCode]/[policyNumber]/(policy-detail-layout)/account/page.tsx
@@ -71,7 +71,7 @@ export default async function AccountValuePage({
 
   //If withdrawal amount is greater than 0 and also if we're past the withdrawal start date
   const canShowWithdrawal =
-    freeWithdrawalAmount &&
+    freeWithdrawalAmount != null &&
     freeWithdrawalAmount > 0 &&
     withdrawalAllowedStartDate !== null &&
     dayjs().isAfter(dayjs(withdrawalAllowedStartDate));
@@ -163,24 +163,6 @@ export default async function AccountValuePage({
             }
           />
         </ClickableCardContainer>
-        <ClickableCardContainer>
-          <ClickableCardContainer.LinkContent
-            linkTo={{
-              url: `/coverage/${LineOfBusinessPath.ANNUITIES}/${planCode}/${policyNumber}/withdrawals`,
-              label: 'go to free withdrawal page',
-            }}
-          >
-            <ValueWithPopover
-              value={freeWithdrawalAmount}
-              label="Free withdrawal"
-              popoverElement={
-                <FreeWithdrawalValuePopover
-                  percentValue={accountWithdrawalPercentage}
-                />
-              }
-            />
-          </ClickableCardContainer.LinkContent>
-        </ClickableCardContainer>
         {canShowWithdrawal && (
           <ClickableCardContainer>
             <ClickableCardContainer.LinkContent
@@ -202,34 +184,6 @@ export default async function AccountValuePage({
             </ClickableCardContainer.LinkContent>
           </ClickableCardContainer>
         )}
-
-        <ClickableCardContainer>
-          <ValueWithPopover
-            value={formatUSDollars(cumulativeGrossDeathBenefitAmount)}
-            label="Death benefit"
-            emphasizeValue
-            popoverElement={
-              <LabelPopover title={'Death Benefit'}>
-                This is the amount, inclusive of any additional riders or
-                features, that will be available to your beneficiaries should
-                you pass away.
-              </LabelPopover>
-            }
-          />
-        </ClickableCardContainer>
-        <ClickableCardContainer>
-          <ValueWithPopover
-            value={formatUSDollars(totalYearToDatePremiumAmount)}
-            label="Total premium"
-            emphasizeValue
-            popoverElement={
-              <LabelPopover title={'Total Premium'}>
-                This is the amount of money you've contributed to your account
-                value to date.
-              </LabelPopover>
-            }
-          />
-        </ClickableCardContainer>
         <AdditionalAccountValueLinks
           planCode={planCode}
           policyNumber={policyNumber}
