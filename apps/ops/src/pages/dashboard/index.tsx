5684b9a3d04a71c45c23be780f97306bc2f7aad3
Ed Sztukowski <edsztukowski@gmail.com>
2024-12-06 14:51:14 -0700

    adding new filter header using zustand

--- apps/ops/src/pages/dashboard/index.tsx
+++ apps/ops/src/pages/dashboard/index.tsx
@@ -1,22 +1,15 @@
 import { getAccessToken, withPageAuthRequired } from '@auth0/nextjs-auth0';
 import { TabContent } from '@zinnia/bloom/components';
 import { FgaRoles } from '@zinnia/utils';
-import clsx from 'clsx';
 import { GetServerSidePropsContext } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { useCallback, useMemo, useRef, useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import { useCallback, useRef, useState } from 'react';
 
-import { MultiselectOption } from '@deps/components/autocomplete/autocomplete.types';
 import { ActiveApplications } from '@deps/components/dashboard/active-applications/active-applications';
-import { BrokerDealerFilter } from '@deps/components/dashboard/broker-dealer-filter/broker-dealer-filter';
 import { DashboardTabNav } from '@deps/components/dashboard/dashboard-nav-links';
 import IssuedBusiness from '@deps/components/dashboard/issued-business/issued-business';
-import { FieldSize } from '@deps/components/fields/field';
 import NoNavLayout from '@deps/components/no-nav-layout';
 import { PageHead } from '@deps/components/page-title';
-import Select from '@deps/components/select/select';
-import Typography, { TypographyVariant } from '@deps/components/typography/typography';
 import { TranslationFiles } from '@deps/config/translations';
 import { DashboardResponsiveLayout } from '@deps/containers/dashboard/dashboard-responsive-layout';
 import { serverSidePropsLogout } from '@deps/helpers/logout.helpers';
@@ -27,12 +20,11 @@ import { UserPermission } from '@deps/models/user-profile';
 import { DashboardResponseData, fetchAgentsSSR } from '@deps/queries/api/dashboard';
 import { checkTupleSsr, getCarrierListServerSSR } from '@deps/queries/api/fga';
 import { FgaRelation } from '@deps/types/fga';
-import { getCarrierListItem, getCarrierNameByClientId, getClientIdsByCarrierName } from '@deps/utils/carriers';
 import { FeatureFlags, optimizelyService } from '@deps/utils/optimizely/optimizely';
 import { logWarn, parseErrorInformation } from '@deps/utils/server-logging';
 import nextI18nextConfig from 'next-i18next.config';
 
-import styles from './Dashboard.module.css';
+import FiltersHeaderZustand from '@deps/components/dashboard/filters-header/filters-header-zustand';
 
 export interface CarrierListItem {
     [key: string]: string;
@@ -55,137 +47,36 @@ const DashboardPage = ({
         rootMargin: `${-64}px 0px -100% 0px`,
     });
 
-    const { t } = useTranslation(TranslationFiles.COMMON);
-
     const [loading, setLoading] = useState<boolean>(false);
-    const [brokerDealers, setBrokerDealers] = useState<DashboardResponseData[]>(brokerDealersSSR || []);
-
-    const carrierFilterItems = useMemo(
-        () =>
-            authorizedCarriers.map((carrierCode: string) => {
-                const valueAndDisplay = getCarrierNameByClientId(carrierCode) || carrierCode.toUpperCase();
-
-                return {
-                    value: getClientIdsByCarrierName(authorizedCarriers, valueAndDisplay) || carrierCode.toUpperCase(),
-                    displayText: valueAndDisplay,
-                    label: getCarrierListItem(carrierCode),
-                };
-            }),
-        [authorizedCarriers]
-    );
-
-    const [selectedCarriers, setSelectedCarriers] = useState<CarrierListItem>(
-        carrierFilterItems.length === 1 ? { [carrierFilterItems[0].value]: carrierFilterItems[0].displayText } : {}
-    );
-    const [placeholderSelectedCarriers, setPlaceholderSelectedCarriers] = useState<CarrierListItem>(
-        carrierFilterItems.length === 1 ? { [carrierFilterItems[0].value]: carrierFilterItems[0].displayText } : {}
-    );
-    const [selectedBrokerDealers, setSelectedBrokerDealers] = useState<CarrierListItem>(
-        brokerDealers?.length === 1 ? { [brokerDealers[0].key]: brokerDealers[0].name } : {}
-    );
 
     const handleSetLoading = useCallback((bool: boolean) => {
         setLoading(bool);
     }, []);
 
-    const getUniqueCarrierFilterItems = (): MultiselectOption[] => {
-        const carrierLabels = new Set();
-
-        const uniqueCarrierFilterItems = (
-            carrierFilterItems.filter(item => {
-                if (carrierLabels.has(item.displayText)) {
-                    return false;
-                }
-
-                carrierLabels.add(item.displayText);
-                return true;
-            }) as typeof carrierFilterItems
-        ).sort((item1, item2) => item1.displayText.localeCompare(item2.displayText));
-        return uniqueCarrierFilterItems;
-    };
-    const updateCarrierFilters = (value: string, displayText: string) => {
-        setPlaceholderSelectedCarriers(prevSelectedCarriers => {
-            if (prevSelectedCarriers[value]) {
-                delete prevSelectedCarriers[value];
-                return { ...prevSelectedCarriers };
-            } else {
-                return { ...prevSelectedCarriers, [value]: displayText };
-            }
-        });
-    };
-
-    const updateBrokerDealerFilters = (value: string, displayText: string) => {
-        setSelectedBrokerDealers(prevSelectedAgents => {
-            if (prevSelectedAgents[value]) {
-                delete prevSelectedAgents[value];
-                return { ...prevSelectedAgents };
-            } else {
-                return { ...prevSelectedAgents, [value]: displayText };
-            }
-        });
-    };
-
-    const handleOnOpenChange = (open: boolean) => {
-        if (!open) {
-            setSelectedCarriers(placeholderSelectedCarriers);
-        }
-    };
     return (
         <DashboardResponsiveLayout>
             <PageHead titleKey="dashboard" />
             <NoNavLayout fullHeight={true} displayTopNavBar={true} size="large">
-                <div
-                    id="carrier-header"
+                <FiltersHeaderZustand
+                    carrierHeaderIsIntersecting={carrierHeaderIsIntersecting}
+                    carrierHeaderEntry={carrierHeaderEntry}
+                    authorizedCarriers={authorizedCarriers}
+                    brokerDealersSSR={brokerDealersSSR}
                     ref={carrierHeaderRef}
-                    className={clsx('flex-wrap', styles.filtersHeader, {
-                        [styles.pinned as string]:
-                            carrierHeaderIsIntersecting || Number(carrierHeaderEntry?.boundingClientRect.bottom) < 64,
-                    })}
-                >
-                    <Typography className="flex items-center" variant={TypographyVariant.H1} data-testid="header-text">
-                        {t('caseStatsDashboardTitle')}
-                    </Typography>
-                    <div className="flex justify-between items-center">
-                        <div className="flex nowrap gap-4">
-                            <div className="w-52">
-                                <Select
-                                    isMultiselect
-                                    options={getUniqueCarrierFilterItems()}
-                                    value={placeholderSelectedCarriers}
-                                    onChange={updateCarrierFilters}
-                                    size={FieldSize.Small}
-                                    placeholder={t('allCarriers') || ''}
-                                    disabled={loading || carrierFilterItems.length === 1}
-                                    name="carrier-dropdown-btn"
-                                    onOpenChange={handleOnOpenChange}
-                                />
-                            </div>
-                            <div className="w-52">
-                                <BrokerDealerFilter
-                                    brokerDealers={brokerDealers}
-                                    selectedBrokerDealers={selectedBrokerDealers}
-                                    selectedCarriers={Object.keys(placeholderSelectedCarriers)}
-                                    setSelectedBrokerDealers={setBrokerDealers}
-                                    updateBrokerDealerFilters={updateBrokerDealerFilters}
-                                    disabled={loading}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    loading={loading}
+                />
+
                 <DashboardTabNav>
                     <TabContent className="w-full" value={'active-applications'}>
                         <ActiveApplications
                             sankeyChartRef={sankeyChartRef}
-                            selectedBrokerDealers={selectedBrokerDealers}
-                            selectedCarriers={selectedCarriers}
                             handleSetLoading={handleSetLoading}
                             loading={loading}
                             carrierHeaderRef={carrierHeaderRef}
                         />
                     </TabContent>
                     <TabContent className="w-full" value={'issued-business'}>
-                        <IssuedBusiness selectedBrokerDealers={selectedBrokerDealers} selectedCarriers={selectedCarriers} />
+                        <IssuedBusiness />
                     </TabContent>
                 </DashboardTabNav>
             </NoNavLayout>
