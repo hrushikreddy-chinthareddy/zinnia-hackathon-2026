5cc1c9b614a4df47b3835187b7acdbab6e097c0a
Dhruvi <dhruvi.shah@zinnia.com>
2024-12-07 13:14:44 -0500

    search updates

--- apps/ops/e2e/cypress/pages/policy/policy/policy-details.page.ts
+++ apps/ops/e2e/cypress/pages/policy/policy/policy-details.page.ts
@@ -1,30 +1,64 @@
 import Page from '../../page';
 
 class PolicyDetailsPage extends Page {
-  url = '/policy/policy-details';
+    url = '/policy/policy-details';
 
-  get policyNumberTextBox() {
-    return cy.get('[data-testid="field-input-test-id"]');
+    get policyNumberTextBox() {
+      return cy.get('[data-testid="field-input-test-id"]');
+    }
+
+    get ['Policy link']() {
+      return cy.get('a[data-testid="Policy"]');
+    }
+
+    get ['Premiums card']() {
+    return cy.get('[data-testid="bank-details-container"]').first();
+    }
+
+    get clickPolicySearchDropdown() {
+      return cy.get('button[id="radix-:r2h:"]');
+    }
+
+    get firstNameTextBox() {
+      return cy.get('[placeholder="First name"]');
+    }
+
+    get lastNameTextBox() {
+      return cy.get('[placeholder="Last name"]');
+    }
+
+    getPolicySidebarLink(policySubLink: string) {
+      return cy.get('[data-testid="drawer-details-drawer-test-id').contains('span', policySubLink);
+    }
+
+    // getclickPolicySearchDropdown() {
+    //   return cy.get('[data-testid="button-group-label-test-id-Policy Number"]');
+    // }
+
+    getPolicyNumber(policyNumber) {
+    this.policyNumberTextBox.type(policyNumber);
+    }
+
+    getClickPolicySearchDropdown() {
+    this.clickPolicySearchDropdown.click();
+    }
+
+  getPolicySearchBy(searchBy) {
+    //return cy.get(`[data-testid="button-group-label-test-id-${searchBy}"]`);
+    return cy.get('p').contains(searchBy);
+    }
+
+  getSearchText(searchText) {
+    return cy.get(`[placeholder="Policy number"]`).type(searchText);
+    }
+
+  getSearchTextByFirstName(firstName){
+    this.firstNameTextBox.type(firstName);
   }
 
-  get ['Policy link']() {
-    return cy.get('a[data-testid="Policy"]');
-  }
-
-  get ['Premiums card']() {
-   return cy.get('[data-testid="bank-details-container"]').first(); 
-  }
-
-  getPolicySidebarLink(policySubLink: string) {
-    return cy.get('[data-testid="drawer-details-drawer-test-id').contains('span', policySubLink);
-  }
-
-// ------------------------------------------------------------------------
-
-  getPolicyNumber(policyNumber) {
-   this.policyNumberTextBox.type(policyNumber);
+  getSeachTextByLastName(lastName){
+    this.lastNameTextBox.type(lastName);
   }
 
 }
-
 export default new PolicyDetailsPage();
