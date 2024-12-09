b73039b818237cf0dfcbd71ee1ce67d47dcff98d
PANKAJ AGARWAL <a.pankaj1982@gmail.com>
2024-12-09 14:24:35 +0530

    combine tasks with same id

--- apps/ops/src/components/case-sub-page/case-tabs/progress/exceptions.tsx
+++ apps/ops/src/components/case-sub-page/case-tabs/progress/exceptions.tsx
@@ -2,10 +2,22 @@ import { useTranslation } from 'next-i18next';
 
 import Content, { ContentVariant } from '@deps/components/content/content';
 import { ExceptionStatuses } from '@deps/models/case/exception-instance';
+import { TaskType } from '@deps/models/case/task';
 
 import { formatTimestamp } from './progress-tab-helpers';
 import { ExceptionView } from './progress-tab-types';
 import Tasks from './tasks';
+
+const TaskTypeMap: Record<TaskType, string> = {
+    [TaskType.SuitabilityReview]: 'suitability review',
+    [TaskType.Withdrawal]: '',
+    [TaskType.OFT]: '',
+    [TaskType.RMD]: '',
+    [TaskType.SSW]: '',
+    [TaskType.RENEWAL]: '',
+    [TaskType.REG60]: '',
+    [TaskType.Suitability]: '',
+};
 
 export default function Exceptions({ exceptions, unmapped = false }: { exceptions: ExceptionView[]; unmapped?: boolean }) {
     const { t } = useTranslation();
@@ -13,29 +25,57 @@ export default function Exceptions({ exceptions, unmapped = false }: { exception
         return null;
     }
 
+    const areGroupedExceptions = exceptions.every(
+        exception => exception.tasks.length > 0 && exception.tasks.every(task => task.id === exceptions[0].tasks[0].id)
+    );
+
     return (
         <ul>
-            {exceptions.map(exception => (
-                <li className="flex w-full flex-col" key={exception.id}>
+            {!areGroupedExceptions &&
+                exceptions.map(exception => (
+                    <li className="flex w-full flex-col" key={exception.id}>
+                        <div className="flex w-full flex-col justify-between lg:flex-row">
+                            <Content
+                                className={exception.status === ExceptionStatuses.New ? 'text-semantic-error' : 'text-semantic-success'}
+                                contentClassName="mt-1"
+                                variant={ContentVariant.BodySm}
+                                details={exception.description}
+                            />
+                            {unmapped && (
+                                <Content
+                                    className="text-gray-600"
+                                    contentClassName="mt-1"
+                                    variant={ContentVariant.BodySm}
+                                    details={t('caseOverview.tabs.since', { date: formatTimestamp(exception.updatedAt) }) as string}
+                                />
+                            )}
+                        </div>
+                        <Tasks tasks={exception.tasks} />
+                    </li>
+                ))}
+            {areGroupedExceptions && (
+                <li className="flex w-full flex-col">
                     <div className="flex w-full flex-col justify-between lg:flex-row">
                         <Content
-                            className={exception.status === ExceptionStatuses.New ? 'text-semantic-error' : 'text-semantic-success'}
+                            className="text-semantic-error"
                             contentClassName="mt-1"
                             variant={ContentVariant.BodySm}
-                            details={exception.description}
+                            details={
+                                t(`One or more ${TaskTypeMap[exceptions[0].tasks[0].description as TaskType]} exceptions found`) as string
+                            }
                         />
                         {unmapped && (
                             <Content
                                 className="text-gray-600"
                                 contentClassName="mt-1"
                                 variant={ContentVariant.BodySm}
-                                details={t('caseOverview.tabs.since', { date: formatTimestamp(exception.updatedAt) }) as string}
+                                details={t('caseOverview.tabs.since', { date: formatTimestamp(exceptions[0].updatedAt) }) as string}
                             />
                         )}
                     </div>
-                    <Tasks tasks={exception.tasks} />
+                    <Tasks tasks={exceptions[0].tasks} />
                 </li>
-            ))}
+            )}
         </ul>
     );
 }
