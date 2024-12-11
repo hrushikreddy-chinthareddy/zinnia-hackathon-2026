2d001e6547ce1ec641ccd309e769ea8d606e54d6
shubhamzin1 <190578939+shubhamzin1@users.noreply.github.com>
2024-12-11 23:27:43 +0530

    revoked changes in create-case-form

--- apps/ops/src/components/create-case-form/create-case-form.tsx
+++ apps/ops/src/components/create-case-form/create-case-form.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from 'next-i18next';
 import xss from 'xss';
+
 import Button, { ButtonSize, ButtonType } from '@deps/components/button/button';
 import Field, { FieldSize, FieldType, FieldVariant } from '@deps/components/fields/field';
 import SelectSimple from '@deps/components/select/select';
@@ -27,11 +28,6 @@ interface CreateCaseViewProps {
     setSearchByOption: React.Dispatch<React.SetStateAction<SearchKeys>>;
 }
 
-type Option = {
-    label: string;
-    value: string;
-}
-
 const CreateCaseForm = ({
     clientIds,
     caseType,
@@ -48,7 +44,6 @@ const CreateCaseForm = ({
     searchByOption,
     setSearchByOption,
 }: CreateCaseViewProps) => {
-
     const { t } = useTranslation(TranslationFiles.COMMON);
     const isNewLayout = caseType === CaseType.AddressChange || caseType === CaseType.ReReg;
 
@@ -91,9 +86,9 @@ const CreateCaseForm = ({
                             onChange={onClientChange}
                             className="md:min-w-[250px]"
                             label={t('caseRenewal.caseCreate.client') as string}
-                            options={clientIds.map((cId : string) => {
+                            options={clientIds.map(cId => {
                                 return { label: `${getCarrierNameByClientId(cId) || cId}`, value: cId.toLowerCase() };
-                            }).sort((a:Option,b:Option) => a.label.localeCompare(b.label))}
+                            })}
                             placeholder={t('caseRenewal.caseCreate.selectAClient') as string}
                             size={FieldSize.Small}
                             value={clientId}
