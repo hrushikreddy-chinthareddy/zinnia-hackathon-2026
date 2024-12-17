8e0e0f8401edd6902c684d7c1995456d76c56246
Bryn Newell <bryn.newell@zinnia.com>
2024-12-17 11:37:24 -0700

    DEPU-3525 updating the copy for the tooltip

--- apps/ops/src/containers/subpages/documents-sub-page/documents-results-table.tsx
+++ apps/ops/src/containers/subpages/documents-sub-page/documents-results-table.tsx
@@ -106,7 +106,7 @@ export default function DocumentsResultsTable({
                             {documentType !== DocumentTypeView.Correspondence && (
                                 <Popover
                                     body={t('documentIdentifierTooltip')}
-                                    title={t('documentIdentifier') as string}
+                                    title={t('document') as string}
                                     placement={PopoverPlacement.TopRight}
                                 >
                                     <Icon type={IconType.CIRCLE_INFO} color="var(--color-primary-color-primary)" height={16} width={16} />
