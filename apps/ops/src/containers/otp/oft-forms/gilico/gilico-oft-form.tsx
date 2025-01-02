2b0d1e359c9a57fd7fdbbd9df706f855151248ce
aditis26 <aditi.sharma@zinnia.com>
2025-01-02 12:09:12 +0530

    gilico ui

---
+++ apps/ops/src/containers/otp/oft-forms/gilico/gilico-oft-form.tsx
@@ -0,0 +1,87 @@
+import { useTranslation } from 'next-i18next';
+import { useContext, useEffect } from 'react';
+
+import CedingCompanyDistribution from '@deps/components/otp-withdrawal-form/ceding-company-distribution';
+import FormDisbursement from '@deps/components/otp-withdrawal-form/form-disbursement/form-disbursement';
+import FormParties from '@deps/components/otp-withdrawal-form/form-party/form-party';
+import FormProgramPartialWithdrawal from '@deps/components/otp-withdrawal-form/form-program/form-program-partial-withdrawal';
+import SignatureValidations from '@deps/components/otp-withdrawal-form/signature-validation/signature-validations';
+import DiaryNotesWarning from '@deps/components/side-sheet/diary-notes/diary-notes-alert';
+import { FormDataContext } from '@deps/contexts/OtpWithdrawalFormContext';
+import { getOwnerStateOfResidence } from '@deps/helpers/otp-withdrawal.helper';
+import { Carrier } from '@deps/models/case/withdrawal/case';
+
+import getFlicOftConfig from './gilico-oft-form.helper';
+
+export default function GilicoOftWithdrawalForm() {
+  const { t } = useTranslation(undefined, { keyPrefix: 'caseWithdrawal.request' });
+  const {
+    signaturesConfig,
+    formPartyConfigs,
+    formValidation,
+    disbursementOptions,
+    surrenderingInstructionsOptions,
+    identifySelectedFormProgramOption,
+    selectOneOptions,
+    defaultValues,
+    qualificationOptions
+  } = getFlicOftConfig(t);
+
+  const {
+    formParty,
+    setFormData,
+    formData,
+    initialForm,
+    setFormValidator,
+    ownerStateOfResidence,
+    isFormStateReadOnly,
+    setOwnerStateOfResidence,
+  } = useContext(FormDataContext);
+
+  useEffect(() => {
+    setFormValidator(() => formValidation);
+  }, []);
+
+  useEffect(() => {
+    setFormData({
+      ...formData,
+      formExtName: `${initialForm?.carrier || Carrier.GLCO}_OFT_DIGITAL_FORM`,
+      metaData: {
+        formType: `${initialForm?.carrier || Carrier.GLCO}_OFT_DIGITAL_FORM`,
+        formId: null,
+        formNumber: '',
+      },
+    });
+  }, [initialForm]);
+
+  useEffect(() => {
+    const newOwnerStateOfResidence = getOwnerStateOfResidence(formParty);
+    if (newOwnerStateOfResidence !== ownerStateOfResidence) {
+      setOwnerStateOfResidence(newOwnerStateOfResidence);
+    }
+  }, [formParty, ownerStateOfResidence]);
+  return (
+    <>
+      {!isFormStateReadOnly && <DiaryNotesWarning />}
+      <FormParties isFormStateReadOnly={isFormStateReadOnly} configs={formPartyConfigs} />
+      <FormProgramPartialWithdrawal
+        isFormStateReadOnly={isFormStateReadOnly}
+        options={surrenderingInstructionsOptions}
+        title={t('amountDetails.surrenderingInstructions.title') as string}
+        selectionIdentifier={identifySelectedFormProgramOption}
+        selectOneOptions={selectOneOptions}
+      />
+      <SignatureValidations isFormStateReadOnly={isFormStateReadOnly} config={signaturesConfig} />
+      <CedingCompanyDistribution
+        qualificationOptions={qualificationOptions}
+        isFormStateReadOnly={isFormStateReadOnly}
+      />
+      <FormDisbursement
+        options={disbursementOptions}
+        isFormStateReadOnly={isFormStateReadOnly}
+        title={t('distributionMethod.cedingCompanyDistribution') as string}
+        defaultValue={defaultValues.disbursementOption}
+      />
+    </>
+  );
+}
