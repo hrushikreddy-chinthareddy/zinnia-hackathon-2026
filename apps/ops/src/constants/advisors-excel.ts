afe4bb7a1d00fc5c8b1c74cc707c0244d2122d53
Rocco Sangellino <rocco.sangellino@zinnia.com>
2025-01-06 14:46:03 -0700

    ZC-769 adding GLCO for AE users

--- apps/ops/src/constants/advisors-excel.ts
+++ apps/ops/src/constants/advisors-excel.ts
@@ -1,10 +1,11 @@
 // Temporary work around for Excel Advisors
 // Hopefully these are temporary as well
 const AE_CARRIER_SBGC = 'SBGC';
+const AE_CARRIER_GLCO = 'GLCO';
 
 const AE_BROKER_DEALER_NAME_PROD = 'ADVISORS EXCEL LLC';
 const AE_BROKER_DEALER_NAME_QA = 'ADVISORS EXCEL, LLC';
 
 const AE_FGA_ROLE = 'role:advisors_excel_imo_support';
 
-export { AE_CARRIER_SBGC, AE_BROKER_DEALER_NAME_PROD, AE_BROKER_DEALER_NAME_QA, AE_FGA_ROLE };
+export { AE_CARRIER_SBGC, AE_BROKER_DEALER_NAME_PROD, AE_BROKER_DEALER_NAME_QA, AE_FGA_ROLE, AE_CARRIER_GLCO };
