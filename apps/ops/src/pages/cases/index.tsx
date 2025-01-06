bfa5b8a4ec56e52537b0acae7c31a85cf1aae594
Bryn Newell <bryn.newell@zinnia.com>
2025-01-06 12:13:38 -0700

    DEPU-3775 adding the presidential mourning banners to both landing pages

--- apps/ops/src/pages/cases/index.tsx
+++ apps/ops/src/pages/cases/index.tsx
@@ -1,4 +1,7 @@
 import { Session, getSession, withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { BannerAlert, BannerVariant } from '@zinnia/bloom/components';
+import dayjs from 'dayjs';
+import isBetween from 'dayjs/plugin/isBetween';
 import { GetServerSidePropsContext } from 'next';
 import dynamic from 'next/dynamic';
 import { useTranslation } from 'next-i18next';
@@ -355,11 +358,24 @@ const CaseManagementDashboard = ({ authorizedCarriers, isAdvisorsExcel, user }: 
         sideSheet.handleOpen(true);
     };
 
+    dayjs.extend(isBetween);
+    const showPresidentialMourningBanner = () => {
+        const today = dayjs();
+        return today.isBetween('2025-01-08', '2025-01-10', 'day', '[]');
+    };
+
     // JSX
     return (
         <CaseManagementFiltersContext.Provider value={[caseManagementFilters, setCaseManagementFilters]}>
             <PageHead titleKey="caseManagement" />
             <NoNavLayout fullHeight={true}>
+                {showPresidentialMourningBanner() && (
+                    <BannerAlert
+                        bodyText={`In recognition of the National Day of Mourning following the death of former President Jimmy Carter, the stock market will be closed on January 9, 2025.  As a result, contract values are as of close of business January 8, 2025.  Any trades or other financial transactions submitted on January 9, 2025 will be processed when the stock market reopens on January 10, 2025.`}
+                        variant={BannerVariant.Warning}
+                        className="mb-8"
+                    />
+                )}
                 <Typography variant={TypographyVariant.H1} className="md:mb-8 mb-4">
                     {t('caseManagementDashboard.h1')}
                 </Typography>
--- apps/ops/src/pages/policies/index.tsx
+++ apps/ops/src/pages/policies/index.tsx
@@ -1,4 +1,7 @@
 import { getAccessToken, withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { BannerAlert, BannerVariant } from '@zinnia/bloom/components';
+import dayjs from 'dayjs';
+import isBetween from 'dayjs/plugin/isBetween';
 import { GetServerSidePropsContext } from 'next';
 import router from 'next/router';
 import { TFunction, useTranslation } from 'next-i18next';
@@ -223,11 +226,24 @@ const PolicyManagementDashboard = ({ user }: PolicyManagementDashboardProps) => 
         }
     }, [loadSearchResults, policySearchFilters.searchValue, fetchPolicies]);
 
+    dayjs.extend(isBetween);
+    const showPresidentialMourningBanner = () => {
+        const today = dayjs();
+        return today.isBetween('2025-01-08', '2025-01-10', 'day', '[]');
+    };
+
     return (
         <DashboardContext.Provider value={{ searchValue: policySearchFilters.searchValue }}>
             <PageHead titleKey="policySearch" />
             <NoNavLayout displayTopNavBar={false}>
                 <div className="flex flex-col items-center xl:items-start">
+                    {showPresidentialMourningBanner() && (
+                        <BannerAlert
+                            bodyText={`In recognition of the National Day of Mourning following the death of former President Jimmy Carter, the stock market will be closed on January 9, 2025.  As a result, contract values are as of close of business January 8, 2025.  Any trades or other financial transactions submitted on January 9, 2025 will be processed when the stock market reopens on January 10, 2025.`}
+                            variant={BannerVariant.Warning}
+                            className="mb-8"
+                        />
+                    )}
                     <Typography variant={TypographyVariant.H1} className="md:mb-8 mb-4">
                         {t('dashboard.h1')}
                     </Typography>
