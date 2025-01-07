8b109d7d112b8e00c0cc49926991dcae4c20d0e5
mclarkson-zinnia <mackenzie.clarkson@zinnia.com>
2025-01-07 15:18:25 -0800

    more mock dating

--- apps/consumer-experience/src/components/mourning-banner/MourningBanner.test.tsx
+++ apps/consumer-experience/src/components/mourning-banner/MourningBanner.test.tsx
@@ -1,11 +1,18 @@
 import { render, screen } from '@testing-library/react';
-import MockDate from 'mockdate';
 
 import { MourningBanner } from './MourningBanner';
 
 describe('MourningBanner component', () => {
+  beforeEach(() => {
+    jest.useFakeTimers(); // Enable modern fake timers
+  });
+
+  afterEach(() => {
+    jest.useRealTimers(); // Restore real timers after each test
+  });
+
   it('renders the banner when the date is between 2025-01-08 and 2025-01-10', () => {
-    MockDate.set('2025-01-08');
+    jest.setSystemTime(new Date('2025-01-08T00:00:00'));
     render(<MourningBanner />);
     expect(
       screen.getByText(/In recognition of the National Day of Mourning/)
@@ -13,7 +20,7 @@ describe('MourningBanner component', () => {
   });
 
   it('does not render the banner when the date is not between 2025-01-08 and 2025-01-10', () => {
-    MockDate.set('2025-01-01');
+    jest.setSystemTime(new Date('2025-01-01T00:00:00'));
     render(<MourningBanner />);
     expect(
       screen.queryByText(/In recognition of the National Day of Mourning/)
