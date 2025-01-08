9d80c43cd76a4b616132230339b70697721a6e95
Anssam <anssam.ghezala@zinnia.com>
2025-01-08 22:27:41 +0400

    feat: add some styling + split components

--- apps/ops/src/pages/pom/index.tsx
+++ apps/ops/src/pages/pom/index.tsx
@@ -1,4 +1,3 @@
-import { withPageAuthRequired } from '@auth0/nextjs-auth0/client';
 import { clsx } from 'clsx';
 
 import NoNavLayout from '@deps/components/no-nav-layout';
@@ -7,7 +6,8 @@ import Typography, { TypographyVariant } from '@deps/components/typography/typog
 import usePomExperience from '@deps/hooks/usePomExperience';
 
 import styles from './index.module.css';
-import { Divider, Icon, IconType, TabContent, TabGroup, TabList, TabTrigger } from '@zinnia/bloom/components';
+import { Divider, Icon, IconType, Label, TabContent, TabGroup, TabList, TabTrigger } from '@zinnia/bloom/components';
+import { withPageAuthRequired } from '@auth0/nextjs-auth0/client';
 
 function POM() {
     // const { t } = useTranslation(TranslationFiles.COMMON);
@@ -17,66 +17,82 @@ function POM() {
         return null;
     }
 
+    const tabs = [
+        { label: 'Entity Information', icon: IconType.DOCUMENT_TEXT, value: 'personalInfo', content: personalInfoSection() },
+        { label: 'Licenses and Appointments', icon: IconType.DOCUMENT_TEXT, value: 'licenses', content: <div>Section 2</div> },
+        { label: 'Training and Education', icon: IconType.DOCUMENT_TEXT, value: 'training', content: <div>Section 3</div> },
+        { label: 'Hierarchies', icon: IconType.DOCUMENT_TEXT, value: 'hierarchies', content: <div>Section 4</div> },
+    ];
+
+    function personalInfoSection() {
+        return (
+            <div>
+                <div className={clsx(styles.cardSection)}>
+                    <Typography variant={TypographyVariant.H2} className={clsx(styles.personalInfoSectionHeader)}>
+                        Identification
+                    </Typography>
+                    <div className={clsx(styles.personalInfoSection)}>
+                        <div>
+                            <Label>Type of corporation</Label>
+                            <Typography variant={TypographyVariant.BodySm}>Third party marketer</Typography>
+                        </div>
+                        <div>Section 2</div>
+                        <div>Section 3</div>
+                    </div>
+                </div>
+                <Divider direction="horizontal" />
+                <div className={clsx(styles.cardSection)}>
+                    <Typography variant={TypographyVariant.H2} className={clsx(styles.caption)}>
+                        Contact Info
+                    </Typography>
+                </div>
+            </div>
+        );
+    }
+
     return (
         <>
             <PageHead titleKey="pom" />
             <NoNavLayout displayTopNavBar>
                 <div className={clsx(styles.cardContainer)}>
-                    <div className={clsx(styles.cardHeader)}>
-                        <Icon type={IconType.USER} className={clsx(styles.icon)} />
-                        <div>
-                            <Typography variant={TypographyVariant.H1} className={clsx(styles.header)}>
-                                Acme Corporation
-                            </Typography>
-                            <Typography variant={TypographyVariant.Caption} className={clsx(styles.caption)}>
-                                Tax identification number: 669-45-6789
-                            </Typography>
-                        </div>
-                    </div>
-                    <TabGroup defaultValue="personalInfo" activationMode="manual" onValueChange={console.log}>
-                        <TabList>
-                            <TabTrigger value="personalInfo">
-                                <Icon type={IconType.DOCUMENT_TEXT} />
-                                Entity Information
-                            </TabTrigger>
-                            <TabTrigger value="licenses">
-                                <Icon type={IconType.DOCUMENT_TEXT} />
-                                Licenses and Appointments
-                            </TabTrigger>
-                            <TabTrigger value="training">
-                                <Icon type={IconType.DOCUMENT_TEXT} />
-                                Training and Education
-                            </TabTrigger>
-                            <TabTrigger value="hierarchies">
-                                <Icon type={IconType.DOCUMENT_TEXT} />
-                                Hierarchies
-                            </TabTrigger>
+                    <CardHeader />
+                    <TabGroup
+                        defaultValue={tabs[0].value}
+                        activationMode="manual"
+                        onValueChange={console.log}
+                        className={clsx(styles.tabs)}
+                    >
+                        <TabList className={clsx(styles.tabList)}>
+                            {tabs.map(({ label, icon, value }) => (
+                                <TabTrigger value={value}>
+                                    <Icon type={icon} />
+                                    {label}
+                                </TabTrigger>
+                            ))}
                         </TabList>
-                        <TabContent value="personalInfo">
-                            <div>
-                                <Typography variant={TypographyVariant.H2} className={clsx(styles.caption)}>
-                                    Identification
-                                </Typography>
-                                <div className={clsx(styles.personInfoSection)}>
-                                    <div>Section 1</div>
-                                    <div>Section 2</div>
-                                    <div>Section 3</div>
-                                </div>
-                            </div>
-                            <Divider direction="horizontal" />
-                            <div>
-                                <Typography variant={TypographyVariant.H2} className={clsx(styles.caption)}>
-                                    Contact Info
-                                </Typography>
-                            </div>
-                        </TabContent>
-                        <TabContent value="licenses">Content for tab 2</TabContent>
-                        <TabContent value="training">Content for tab 3</TabContent>
-                        <TabContent value="hierarchies">Content for tab 4</TabContent>
+                        {tabs.map(({ value, content }: { value: string; content: any }) => (
+                            <TabContent value={value}>{content}</TabContent>
+                        ))}
                     </TabGroup>
                 </div>
             </NoNavLayout>
         </>
+    );
+}
+
+function CardHeader() {
+    return (
+        <div className={clsx(styles.cardHeader)}>
+            <Icon type={IconType.USER} className={clsx(styles.icon)} />
+            <div>
+                <Typography variant={TypographyVariant.H1} className={clsx(styles.header)}>
+                    Acme Corporation
+                </Typography>
+                <Typography variant={TypographyVariant.Caption} className={clsx(styles.caption)}>
+                    Tax identification number: 669-45-6789
+                </Typography>
+            </div>
+        </div>
     );
 }
 
