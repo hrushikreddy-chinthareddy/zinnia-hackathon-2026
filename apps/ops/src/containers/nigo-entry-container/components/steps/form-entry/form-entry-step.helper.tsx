60ea1e9ec9daf6ae462f684c85cb51fb3450d7c8
Leena Bodhe <103044931+leenabodhe@users.noreply.github.com>
2025-01-10 20:14:59 +0530

    Added nigo etry support for GLCO redemption

--- apps/ops/src/containers/nigo-entry-container/components/steps/form-entry/form-entry-step.helper.tsx
+++ apps/ops/src/containers/nigo-entry-container/components/steps/form-entry/form-entry-step.helper.tsx
@@ -13,6 +13,7 @@ import { SbgcSSWForm } from '@deps/containers/otp/ssw-forms/sbgc/sbgc-ssw-form';
 import DlicWithdrawalForm from '@deps/containers/otp/withdrawal-forms/dlic/dlic-withdrawal-form';
 import FlicWithdrawalForm from '@deps/containers/otp/withdrawal-forms/flic-withdrawal-form';
 import GdmnWithdrawalForm from '@deps/containers/otp/withdrawal-forms/gdmn/gdmn-withdrawal-form';
+import GilicoWithdrawalForm from '@deps/containers/otp/withdrawal-forms/gilico/gilico-withdrawal-form';
 import MassWithdrawalForm from '@deps/containers/otp/withdrawal-forms/mass/mass-withdrawal-form';
 import NasuWithdrawalForm from '@deps/containers/otp/withdrawal-forms/nasu/nasu-withdrawal-form';
 import RslnWithdrawalForm from '@deps/containers/otp/withdrawal-forms/rsln/rsln-withdrawal-form';
@@ -41,6 +42,7 @@ export const getWithdrawalFormComponentMap = (qualType: QualTypes | ''): Record<
     [Carrier.NASU]: <NasuWithdrawalForm />,
     [Carrier.GDMN]: <GdmnWithdrawalForm />,
     [Carrier.RSLN]: <RslnWithdrawalForm />,
+    [Carrier.GLCO]: <GilicoWithdrawalForm />,
 });
 
 export const getOFTFormComponentMap = (planCode: string | '', qualType: QualTypes | ''): Record<string, React.ReactNode> => ({
--- apps/ops/src/utils/optimizely/flags.ts
+++ apps/ops/src/utils/optimizely/flags.ts
@@ -55,6 +55,7 @@ export enum FEATURE_FLAGS {
     NIGO_ENTRY_MASS_WITHDRAWAL = 'nigo-entry_mass_withdrawal',
     NIGO_ENTRY_RSLN_WITHDRAWAL = 'nigo-entry_rsln_withdrawal',
     NIGO_ENTRY_GDMN_WITHDRAWAL = 'nigo-entry_gdmn_withdrawal',
+    NIGO_ENTRY_GLCO_WITHDRAWAL = 'nigo-entry_glco_withdrawal',
     NIGO_ENTRY_SBGC_OFT = 'nigo-entry_sbgc_oft',
     NIGO_ENTRY_FLIC_OFT = 'nigo-entry_flic_oft',
     NIGO_ENTRY_DLIC_OFT = 'nigo-entry_dlic_oft',
