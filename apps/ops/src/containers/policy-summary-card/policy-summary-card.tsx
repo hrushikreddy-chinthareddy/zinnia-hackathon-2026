6d811795566fa5e26c32ba30991ba3f8f3730ee2
Bryn Newell <bryn.newell@zinnia.com>
2025-01-13 13:37:18 -0700

    DEPU-3425 adding the activity dropdown menu to policy summary card

--- apps/ops/src/containers/policy-summary-card/policy-summary-card.tsx
+++ apps/ops/src/containers/policy-summary-card/policy-summary-card.tsx
@@ -92,7 +92,8 @@ const quickLinks = (t: TFunction, policy: PolicyDetails): QuickLinksProps['links
         },
         {
             href: t('site.navLinks.history.link', { id: policyNumber, planCode }),
-            name: t('site.navLinks.history.text'),
+            name: t('pageHeader.activity.headerText'),
+            dropdown: true,
         },
         {
             href: t('site.navLinks.documents.link', { id: policyNumber, planCode }),
