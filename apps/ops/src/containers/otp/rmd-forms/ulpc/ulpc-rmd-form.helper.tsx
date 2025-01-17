93b1dbf1a4334093804212f6f971ffb810abe4b4
Leena Bodhe <103044931+leenabodhe@users.noreply.github.com>
2025-01-17 12:57:15 +0530

    Added change for preload ssn under signature

--- apps/ops/src/containers/otp/rmd-forms/ulpc/ulpc-rmd-form.helper.tsx
+++ apps/ops/src/containers/otp/rmd-forms/ulpc/ulpc-rmd-form.helper.tsx
@@ -241,6 +241,7 @@ export default function getUlpcRmdConfig(t: TFunction) {
                 },
             ],
             signatureType: SignatureValidationTypeWithdrawal.Owner,
+            partyRole: PartyRoles.OWNER,
         },
     ];
 
