cbbef50e45420fed12c8b80a69b6e3bbdd806dad
Leena Bodhe <103044931+leenabodhe@users.noreply.github.com>
2025-01-17 12:59:53 +0530

    Added change for preload ssn under signature

--- apps/ops/src/containers/otp/rmd-forms/glco-rmd-form.helper.tsx
+++ apps/ops/src/containers/otp/rmd-forms/glco-rmd-form.helper.tsx
@@ -214,6 +214,7 @@ export default function getGlcoRmdConfig(t: TFunction) {
                 },
             ],
             signatureType: SignatureValidationTypeWithdrawal.Owner,
+            partyRole: PartyRoles.OWNER,
         },
         {
             key: `sig-val-beneficiary`,
