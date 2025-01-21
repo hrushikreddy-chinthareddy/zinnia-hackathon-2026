19cfdc454ac51cbd90105c9538056a413422d04e
Vijayalaxmi Sambhane <vijayalaxmi.sambhane@zinnia.com>
2025-01-21 17:39:39 +0530

    Updated metadata for the completed task

--- apps/ops/src/utils/tasks/task-metadata-helper.ts
+++ apps/ops/src/utils/tasks/task-metadata-helper.ts
@@ -16,7 +16,7 @@ export const TaskMetadataHelper = (task: ManagementTask, tasksMetadata: any[]) =
                         return option;
                     });
 
-                    const potentialMatchesOptions = generatePotentialMatchesOptions(task.data.potentialMatches);
+                    const potentialMatchesOptions = generatePotentialMatchesOptions(task.data.potentialMatches) || [];
 
                     uiSchema.potentialMatches['ui:options'].customOptions = [...potentialMatchesOptions, ...existingOptions];
                 }
@@ -32,8 +32,8 @@ export const TaskMetadataHelper = (task: ManagementTask, tasksMetadata: any[]) =
 
 const generatePotentialMatchesOptions = (potentialMatches: PotentialMatches[]): any[] => {
     return potentialMatches
-        .filter(item => item.correlationid && item.correlationid !== '')
-        .map((item: PotentialMatches) => {
+        ?.filter(item => item.correlationid && item.correlationid !== '')
+        ?.map((item: PotentialMatches) => {
             const id = uuidv4();
             const subElement = {
                 label: MatchingCaseTypes[item.entityType as keyof typeof MatchingCaseTypes] ?? MatchingCaseTypes.NB_APPLICATION_DATA,
