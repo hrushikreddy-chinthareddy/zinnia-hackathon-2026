87d6a7fc55947a256794d4e42d484eb9c93c96d0
Vijayalaxmi Sambhane <vijayalaxmi.sambhane@zinnia.com>
2025-01-22 18:09:28 +0530

    Updated keys

--- apps/ops/src/containers/task-container/steps-healper/match-payment-document-review.tsx
+++ apps/ops/src/containers/task-container/steps-healper/match-payment-document-review.tsx
@@ -1,5 +1,3 @@
-import { v4 as uuidv4 } from 'uuid';
-
 import { MatchingCase } from '@deps/models/case/task/doc-matching-payment';
 
 import { GetStepsProps } from './types';
@@ -8,22 +6,7 @@ import ConfirmStep from '../components/steps/confirm/confirm-step';
 import { MemoizedTaskFormStep as TaskFormStep } from '../components/steps/task-form/task-form-step';
 
 export const getMatchDocumentPaymentReviewSteps = ({ taskType, taskInfoLink, t, taskMetadata, task }: GetStepsProps) => {
-    const docMatchKey = uuidv4();
-    const paymentMatchKey = uuidv4();
-
     const isSubmit = task.data.potentialMatches === MatchingCase.REINDEX;
-
-    // todo:vijaya: commenting this for demo
-    // const dynamicSteps = taskMetadata.map((item: any, index: number) => {
-    //     const isStepVisible = (isSubmit: boolean, index: number, length: number): boolean => {
-    //         if (isSubmit && index === length - 1) {
-    //             return false;
-    //         }
-    //         if (index === length - 2) {
-    //             return true;
-    //         }
-    //         return true;
-    //     };
 
     const dynamicSteps = [
         {
@@ -34,7 +17,7 @@ export const getMatchDocumentPaymentReviewSteps = ({ taskType, taskInfoLink, t, 
                     taskInfoLink={taskInfoLink}
                     isSubmit={isSubmit}
                     taskMetadata={taskMetadata[0]}
-                    key={docMatchKey}
+                    key={'docMatchKey'}
                 ></TaskFormStep>
             ),
             text: taskMetadata[0]?.title || '',
@@ -50,7 +33,7 @@ export const getMatchDocumentPaymentReviewSteps = ({ taskType, taskInfoLink, t, 
                     taskInfoLink={taskInfoLink}
                     isSubmit={true}
                     taskMetadata={taskMetadata[1]}
-                    key={paymentMatchKey}
+                    key={'paymentMatchKey'}
                 ></TaskFormStep>
             ),
             text: taskMetadata[1]?.title || '',
