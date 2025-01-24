1b25681ad1f4cbc5a5ed11447bf567fabb6171e3
Vijayalaxmi Sambhane <vijayalaxmi.sambhane@zinnia.com>
2025-01-24 18:56:59 +0530

    Updated stages & steps mapping

--- apps/ops/src/components/case-sub-page/case-tabs/progress/tasks.tsx
+++ apps/ops/src/components/case-sub-page/case-tabs/progress/tasks.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from 'next-i18next';
 
 import Content, { ContentVariant } from '@deps/components/content/content';
-import NewTaskSideSheet from '@deps/containers/case-overview/tasks-table/sidesheet/new-task-sidesheet-content';
+import GlobalTaskSideSheet from '@deps/containers/case-overview/tasks-table/sidesheet/global-task-sidesheet-content';
 import TaskSideSheet from '@deps/containers/case-overview/tasks-table/sidesheet/task-sidesheet-content';
 import { useSideSheetContext } from '@deps/contexts/SideSheetContext';
 import { convertKebabedDateString } from '@deps/helpers/string.helper';
@@ -9,11 +9,15 @@ import { Statuses } from '@deps/models/case/case';
 import { ExceptionStatuses } from '@deps/models/case/exception-instance';
 import { TaskType } from '@deps/models/case/task';
 import { ReactComponent as ChevronDown } from '@deps/styles/elements/icons/arrow/chevron-down.svg';
-import GlobalTaskSideSheet from '@deps/containers/case-overview/tasks-table/sidesheet/global-task-sidesheet-content';
 
 import { TaskView } from './progress-tab-types';
 
-const SupportedTaskMap = [TaskType.SuitabilityReview, TaskType.SuitabilityDataEntry, TaskType.PURCHASE_DOCUMENT_MATCHING];
+const SupportedTaskMap = [
+    TaskType.SuitabilityReview,
+    TaskType.SuitabilityDataEntry,
+    TaskType.PURCHASE_DOCUMENT_MATCHING,
+    TaskType.Agent_Nigo,
+];
 
 export function Task({ task }: { task: TaskView }) {
     const { t } = useTranslation();
@@ -22,6 +26,7 @@ export function Task({ task }: { task: TaskView }) {
     const TaskTitle: Record<string, string> = {
         [TaskType.SuitabilityReview]: t('caseOverview.tabs.suitabilityReviewIssues'),
         [TaskType.PURCHASE_DOCUMENT_MATCHING]: t('caseOverview.tabs.purchaseDocumentMatchingIssues'),
+        [TaskType.Agent_Nigo]: t('caseOverview.tabs.agentNigo'),
     };
 
     const TaskTypeMap: Record<string, string> = {
