24a95178f1c37769bb0c67761541b0f9510190dd
saurav-0083 <saurav.rath@zinnia.com>
2025-01-29 19:41:11 +0530

    enabling data entry

--- apps/ops/src/containers/nigo-entry-container/components/steps/form-entry/form-entry-step.helper.tsx
+++ apps/ops/src/containers/nigo-entry-container/components/steps/form-entry/form-entry-step.helper.tsx
@@ -5,8 +5,9 @@ import MassOftWithdrawalForm from '@deps/containers/otp/oft-forms/mass/mass-oft-
 import NasuOftWithdrawalForm from '@deps/containers/otp/oft-forms/nasu/nasu-oft-form';
 import RSLNOftWithdrawalForm from '@deps/containers/otp/oft-forms/rsln/rsln-oft-form';
 import SbgcOftWithdrawalForm from '@deps/containers/otp/oft-forms/sbgc/sbgc-oft-form';
-import UsaaOftWithdrawalForm from '@deps/containers/otp/oft-forms/usaa/usaa-oft-form';
 import UlpcOftWithdrawalForm from '@deps/containers/otp/oft-forms/ulpc/ulpc-oft-form';
+import UsaaOftWithdrawalForm from '@deps/containers/otp/oft-forms/usaa/usaa-oft-form';
+import DlicRmdWithdrawalForm from '@deps/containers/otp/rmd-forms/dlic/dlic-rmd-form';
 import FlicRmdWithdrawalForm from '@deps/containers/otp/rmd-forms/flic-rmd-form';
 import GlcoRmdWithdrawalForm from '@deps/containers/otp/rmd-forms/glco-rmd-form';
 import MassMutualRmdWithdrawalForm from '@deps/containers/otp/rmd-forms/mm-rmd-form';
@@ -71,7 +72,8 @@ export const getRMDFormComponentMap = (qualType: QualTypes | ''): Record<string,
     [Carrier.MASS]: <MassMutualRmdWithdrawalForm qualType={qualType} />,
     [Carrier.SBGC]: <SbgcRmdWithdrawalForm />,
     [Carrier.GLCO]: <GlcoRmdWithdrawalForm />,
-    [Carrier.ULPC]: <UlpcRmdWithdrawalForm />
+    [Carrier.ULPC]: <UlpcRmdWithdrawalForm />,
+    [Carrier.DLIC]: <DlicRmdWithdrawalForm />,
 });
 
 const getSSWFormComponentMap = (qualType: QualTypes | '', planCode?: string): Record<string, React.ReactNode> => ({
