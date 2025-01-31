e49032f0ec52fd3396472c48531f154a45a91b00
Vijayalaxmi Sambhane <vijayalaxmi.sambhane@zinnia.com>
2025-01-31 17:58:58 +0530

    reverted task type

--- apps/ops/src/components/case-sub-page/case-tabs/progress/progress-tab-helpers.ts
+++ apps/ops/src/components/case-sub-page/case-tabs/progress/progress-tab-helpers.ts
@@ -385,7 +385,7 @@ export class TransformedCase {
         }
         return {
             createdAt: foundTask.createdAt,
-            description: foundTask.label ?? '',
+            description: foundTask.label || foundTask.taskType,
             id: foundTask.id,
             hasParentException: isFromException,
             parentExceptionStatus: status,
