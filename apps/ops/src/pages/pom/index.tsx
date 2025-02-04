88505f93d55447a99685450fd13632b881cff292
mclarkson-zinnia <mackenzie.clarkson@zinnia.com>
2025-02-04 11:32:26 -0800

    remove unnecessary fragment

--- apps/ops/src/pages/pom/index.tsx
+++ apps/ops/src/pages/pom/index.tsx
@@ -10,11 +10,7 @@ import nextI18nextConfig from 'next-i18next.config';
 
 function POM() {
     const { t } = useTranslation('pom');
-    return (
-        <>
-            <Pom translations={t} />
-        </>
-    );
+    return <Pom translations={t} />;
 }
 
 export default POM;
