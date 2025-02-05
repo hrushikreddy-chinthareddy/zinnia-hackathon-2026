e2f6e89f7a8ff130c6650081eb3229a4e66b73f6
Brian Byers <brian.byers@zinnia.com>
2025-02-05 13:43:24 -0700

    fix(DEPU-4068): remove test file. import fix

--- apps/ops/src/components/document-viewer/tax-form-previewer.tsx
+++ apps/ops/src/components/document-viewer/tax-form-previewer.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'next-i18next';
 
-import NavElement, { NavElementSize, NavElementType, NavElementVariant } from '../nav-element/nav-element';
+import NavElement, { NavElementSize, NavElementType, NavElementVariant } from '@deps/components/nav-element/nav-element';
 import { TaxformResponse } from '@zinnia/api-types/types/documents-v3';
 import { TaxForm } from '@deps/models/case/send-tax-forms';
 
