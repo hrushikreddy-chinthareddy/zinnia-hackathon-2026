fa50f74b3afbedf0fac9a5a6c97749480d01b3b5
shubhamzin1 <shubham.bansal@zinnia.com>
2025-02-13 16:38:41 +0530

    fixed issues not showing case progress

--- apps/ops/src/components/case-sub-page/case-tabs/progress/exceptions.tsx
+++ apps/ops/src/components/case-sub-page/case-tabs/progress/exceptions.tsx
@@ -2,43 +2,30 @@ import { TFunction, useTranslation } from 'next-i18next';
 
 import Content, { ContentVariant } from '@deps/components/content/content';
 import { ExceptionStatuses } from '@deps/models/case/exception-instance';
-import { TaskType } from '@deps/models/case/task';
 
 import { formatTimestamp } from './progress-tab-helpers';
 import { ExceptionView } from './progress-tab-types';
 import Tasks from './tasks';
 
-const TaskTypeMap: Record<string, string> = {
-    ['SUITABILITY_REVIEW']: 'suitability review',
-};
-
 const renderException = (exception: ExceptionView, isSingleTask: boolean, t: TFunction, unmapped?: boolean) => {
     return (
         <>
-            {exception.tasks.every(task => task.description !== TaskType.SuitabilityReview) && (
-                <div className="flex w-full flex-col justify-between lg:flex-row">
+            <div className="flex w-full flex-col justify-between lg:flex-row">
+                <Content
+                    className={exception.status === ExceptionStatuses.Resolved ? 'text-semantic-success' : 'text-semantic-error'}
+                    contentClassName="mt-1"
+                    variant={ContentVariant.BodySm}
+                    details={exception.description}
+                />
+                {unmapped && (
                     <Content
-                        className={exception.status === ExceptionStatuses.Resolved ? 'text-semantic-success' : 'text-semantic-error'}
+                        className="text-gray-600"
                         contentClassName="mt-1"
                         variant={ContentVariant.BodySm}
-                        details={
-                            !isSingleTask
-                                ? exception.description
-                                : (t('caseOverview.tabs.commonTaskIssues', {
-                                      taskType: TaskTypeMap[exception.tasks[0].description],
-                                  }) as string)
-                        }
+                        details={t('caseOverview.tabs.since', { date: formatTimestamp(exception.updatedAt) }) as string}
                     />
-                    {unmapped && (
-                        <Content
-                            className="text-gray-600"
-                            contentClassName="mt-1"
-                            variant={ContentVariant.BodySm}
-                            details={t('caseOverview.tabs.since', { date: formatTimestamp(exception.updatedAt) }) as string}
-                        />
-                    )}
-                </div>
-            )}
+                )}
+            </div>
         </>
     );
 };
@@ -63,10 +50,14 @@ export default function Exceptions({ exceptions, unmapped = false }: { exception
                     </li>
                 ))}
             {isSingleTask && (
-                <li className="flex w-full flex-col">
-                    {renderException(exceptions[0], isSingleTask, t, unmapped)}
+                <>
+                    {exceptions.map(exception => (
+                        <li className="flex w-full flex-col" key={exception.id}>
+                            {renderException(exception, isSingleTask, t, unmapped)}
+                        </li>
+                    ))}
                     <Tasks tasks={exceptions[0].tasks} />
-                </li>
+                </>
             )}
         </ul>
     );
