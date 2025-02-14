d39e50c7b800cda971fe72e5bbef0924a7291a22
saurav-0083 <saurav.rath@zinnia.com>
2025-02-14 12:28:14 +0530

    Refactor SSW forms to remove TPA authorization from PrdnSSWForm and add it to RslnSSWForm

--- apps/ops/src/containers/otp/ssw-forms/prdn/prdn-ssw-form.tsx
+++ apps/ops/src/containers/otp/ssw-forms/prdn/prdn-ssw-form.tsx
@@ -15,7 +15,6 @@ import { Carrier } from '@deps/models/case/withdrawal/case';
 import SswEditSelection from '../ssw-edit-selection';
 import getPrdnConfig from './prdn-ssw-from-helper';
 import DistributionReason from '@deps/components/otp-withdrawal-form/form-restriction/distribution-reason';
-import EmployerTpaAuthorization from '@deps/components/otp-withdrawal-form/employer-tpa-authorization';
 
 export function PrdnSSWForm() {
     const { t } = useTranslation(undefined, { keyPrefix: 'caseWithdrawal.request' });
@@ -31,7 +30,6 @@ export function PrdnSSWForm() {
         isFormStateReadOnly,
         ownerStateOfResidence,
         setOwnerStateOfResidence,
-        formTpaAuthorization,
     } = useContext(FormDataContext);
 
     useEffect(() => {
@@ -55,8 +53,6 @@ export function PrdnSSWForm() {
         }
     }, [formParty]);
 
-    const hasTpaAuthorization = formTpaAuthorization && !Object.values(formTpaAuthorization).every(val => val === null);
-
     return (
         <>
             {!isFormStateReadOnly && <DiaryNotesWarning />}
@@ -68,7 +64,6 @@ export function PrdnSSWForm() {
             <TaxWithholdings isFormStateReadOnly={isFormStateReadOnly} ownerStateOfResidence={ownerStateOfResidence} />
             <FormDisbursement isFormStateReadOnly={isFormStateReadOnly} options={disbursementOptions} />
             <SignatureValidations isFormStateReadOnly={isFormStateReadOnly} config={signaturesConfig} />
-            {hasTpaAuthorization && <EmployerTpaAuthorization isFormStateReadOnly={isFormStateReadOnly} />}
         </>
     );
 }
