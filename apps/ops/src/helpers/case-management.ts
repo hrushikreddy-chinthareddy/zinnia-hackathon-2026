f574f863fb4f3b3d3815ed6156d947f323074a91
shubhamzin1 <shubham.bansal@zinnia.com>
2025-02-17 14:18:53 +0530

    allowed adding space for firmName

--- apps/ops/src/helpers/case-management.ts
+++ apps/ops/src/helpers/case-management.ts
@@ -44,7 +44,7 @@ export const getSearchValueObject = (
                 ...(agentLastName ? { agentLastName } : {}),
             };
         case 'firmName':
-            return firmName ? { brokerDealerName: firmName } : {};
+            return firmName ? { brokerDealerName: firmName.trim() } : {};
         default:
             return {};
     }
