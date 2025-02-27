c7f1c1b898ed99445cae4982438625a082a6a2c3
aditis26 <aditi.sharma@zinnia.com>
2025-02-27 17:59:09 +0530

    task helper conflict resolved

--- apps/ops/src/containers/task-container/task.healpers.ts
+++ apps/ops/src/containers/task-container/task.healpers.ts
@@ -1,9 +1,10 @@
 import { ManagementTask, TaskStatus } from '@deps/models/case/task-instance';
 import { updateCaseTask } from '@deps/operations/tasks/task-operations';
-import { buildTaskPayload } from '@deps/utils/tasks/task-payload-helper';
 
 export const updateTask = async (task: ManagementTask, correlationId: string, taskStatus?: TaskStatus): Promise<boolean> => {
-    const processedTask = buildTaskPayload(task, task);
-    const taskResponse = await updateCaseTask(processedTask, taskStatus ?? TaskStatus.Completed);
-    return !!taskResponse;
+    const taskResponse = await updateCaseTask(task, taskStatus);
+    if (!taskResponse) {
+        return false;
+    }
+    return true;
 };
