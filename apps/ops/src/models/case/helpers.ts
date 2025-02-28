591abafdd5dc487e2f40338811954c488507e4ed
aditis26 <aditi.sharma@zinnia.com>
2025-02-28 16:50:18 +0530

    error resolved

--- apps/ops/src/models/case/helpers.ts
+++ apps/ops/src/models/case/helpers.ts
@@ -59,4 +59,5 @@ export const TaskApiVersionMapper: Record<TaskType, ApiVersion> = {
     [TaskType.PremiumNigo]: ApiVersion.v2,
     [TaskType.Application_Nigo]: ApiVersion.v2,
     [TaskType.Agent_Review]: ApiVersion.v2,
+    [TaskType.NB_APP_DATA_ENTRY]: ApiVersion.v2,
 };
