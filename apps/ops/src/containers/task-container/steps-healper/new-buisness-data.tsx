6ea83cbeb375014de4eab3d317d6394fedc48104
aditis26 <aditi.sharma@zinnia.com>
2025-02-28 15:51:43 +0530

    new buisness initial setup

---
+++ apps/ops/src/containers/task-container/steps-healper/new-buisness-data.tsx
@@ -0,0 +1,38 @@
+import { GetStepsProps } from './types';
+import { Step } from '../../progress-bar-steps/progress-bar-steps-item/progress-bar-steps-item';
+import ConfirmStep from '../components/steps/confirm/confirm-step';
+import { TaskReviewStep } from '../components/steps/task-review/task-review-step';
+
+export const getNewBusinessDataSteps = ({
+  carrierId,
+  caseId,
+  taskInfoLink,
+  taskType,
+  isReadyForDataEntry,
+  t,
+  nigoExceptions,
+  nigoSubExceptions,
+  taskMetadata,
+}: GetStepsProps) => {
+  const steps: Step[] = [
+    {
+      ariaLabel: taskMetadata[0]?.title || '',
+      isVisible: () => true,
+      component: <TaskReviewStep caseId={caseId} clientCode={carrierId} taskInfoLink={taskInfoLink} taskType={taskType} />,
+      text: t('tabs.matchDocument'),
+      index: 0,
+      isCompleted: true,
+      screenReaderLabel: taskMetadata[0]?.title || '',
+    },
+
+    {
+      ariaLabel: t('confirm'),
+      isVisible: () => true,
+      component: <ConfirmStep taskType={taskType} taskInfoLink={taskInfoLink}></ConfirmStep>,
+      text: t('confirm'),
+      index: 3,
+      screenReaderLabel: t('confirm'),
+    },
+  ];
+  return steps;
+};
--- apps/ops/src/models/case/task.ts
+++ apps/ops/src/models/case/task.ts
@@ -19,6 +19,7 @@ export enum TaskType {
     PremiumNigo = 'NB_PAYMENT_PROCESSING_NIGO',
     Application_Nigo = 'NB_APPLICATION_NIGO',
     Agent_Review = 'NB_AGENT_REVIEW',
+    NB_APP_DATA_ENTRY = 'NB_APP_DATA_ENTRY',
 }
 
 export enum EarlyTaskType {
