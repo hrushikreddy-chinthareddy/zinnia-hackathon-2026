eae7834e1253a6e2b16779aafd997325ddb10c7c
Zinnia_India <vijayalaxmi.sambhane@zinnia.com>
2025-03-04 17:18:13 +0530

    code refactor

--- apps/ops/src/pages/task/[taskId]/index.tsx
+++ apps/ops/src/pages/task/[taskId]/index.tsx
@@ -122,7 +122,7 @@ export const getServerSideProps = withPageAuthRequired({
                     },
                 };
             }
-            if (!((taskUserOverride === true && !isProd()) || (taskTypeOverride && !isProd()))) {
+            if (!(!isProd() && (taskUserOverride || taskTypeOverride))) {
                 if (
                     !(
                         user.email &&
