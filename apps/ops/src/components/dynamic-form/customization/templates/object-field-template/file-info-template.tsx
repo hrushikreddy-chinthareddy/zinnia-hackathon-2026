8a2ab7d1aed68b75c6267db251824c203540dfcd
shubhamzin1 <shubham.bansal@zinnia.com>
2025-03-05 17:42:45 +0530

    uplod-doc-qa-issues

--- apps/ops/src/components/dynamic-form/customization/templates/object-field-template/file-info-template.tsx
+++ apps/ops/src/components/dynamic-form/customization/templates/object-field-template/file-info-template.tsx
@@ -16,12 +16,12 @@ function FileInfoTemplate(props: ArrayFieldTemplateProps) {
         <div className={clsx('flex ')}>
             <ul className="file-info">
                 {formData.map((fileInfo: any, index: number) => {
-                    const { documentId } = fileInfo;
+                    const { documentId, documentName } = fileInfo;
                     return (
                         <li key={index} className="p-2 border-1 border-gray-100 my-4 max-w-sm">
                             <div className="typography-content-body-sm-bold flex gap-2">
                                 <UploadIcon height={25} width={25} />
-                                <div>{documentId ?? ''}</div>
+                                <div>{documentName ?? documentId ?? ''}</div>
                             </div>
                         </li>
                     );
