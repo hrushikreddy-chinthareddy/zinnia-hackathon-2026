4f9c8ef7dfad765cd5b43c3350eca9ed32fd151a
Vikram Prasad <vikram.prasad@zinnia.com>
2025-03-06 20:33:58 +0530

    feat: claimTask/unAssign/claimNextTask new apis implemented

---
+++ apps/ops/src/pages/api/case/v1/tasks/[taskId]/assignments.ts
@@ -0,0 +1,54 @@
+import { getAccessToken } from '@auth0/nextjs-auth0';
+
+import { apiServerBaseUrl } from '@deps/queries/api-config';
+import { logTrace, logWarn, parseErrorInformation, withAuthAndLogging } from '@deps/utils/server-logging';
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { serverApi } from '@deps/queries/api-utils/serverApiClient';
+
+export default withAuthAndLogging(
+    async (req: NextApiRequest, res: NextApiResponse<any>, logCtx) => {
+        const now = performance.now();
+        const method = req.method;
+        const { taskId } = req.query;
+        const accessToken = (await getAccessToken(req, res)).accessToken;
+        const baseUrl = `${apiServerBaseUrl}/case/v1/tasks/${taskId}/assignments`;
+
+        const loggingContext = { ...logCtx, baseUrl, method };
+        logTrace(`assignments::${method}::start`, loggingContext);
+
+        const config = {
+            headers: {
+                authorization: `Bearer ${accessToken}`,
+                'Content-type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            },
+        };
+
+        try {
+            let response;
+            if (method === 'PUT') {
+                response = await serverApi.put(baseUrl, {}, config);
+            } else if (method === 'DELETE') {
+                response = await serverApi.delete(baseUrl, config);
+            } else {
+                return res.status(405).json({ error: `Method ${method} not allowed` });
+            }
+
+            logTrace(`serverApiClient::${method}::success`, {
+                ...loggingContext,
+                duration: performance.now() - now,
+            });
+
+            return res.status(200).json(response.data);
+        } catch (error) {
+            logWarn(`serverApiClient::${method}::error`, {
+                ...parseErrorInformation(error),
+                ...loggingContext,
+                duration: performance.now() - now,
+            });
+            return res.status(500).json({ error: 'Internal Server Error' });
+        }
+    },
+    { file: 'case/v1/tasks/[taskId]/assignments', function: 'routeHandler' }
+);
--- apps/ops/src/pages/api/case/v1/tasks/assignments.ts
+++ apps/ops/src/pages/api/case/v1/tasks/assignments.ts
@@ -1,21 +1,22 @@
 import { getAccessToken } from '@auth0/nextjs-auth0';
 
-import { AssignmentTask } from '@deps/queries/api/v1/claim-task';
+import { ClaimNextTask } from '@deps/queries/api/v1/claim-task';
 import { apiServerBaseUrl } from '@deps/queries/api-config';
 import { serverApi } from '@deps/queries/api-utils/serverApiClient';
 import { logTrace, logWarn, parseErrorInformation, withAuthAndLogging } from '@deps/utils/server-logging';
 
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { AxiosResponse } from 'axios';
 
 export default withAuthAndLogging(
-    async (req: NextApiRequest, res: NextApiResponse<AssignmentTask | null>, logCtx) => {
+    async (req: NextApiRequest, res: NextApiResponse<ClaimNextTask | null>, logCtx) => {
         const now = performance.now();
         const accessToken = (await getAccessToken(req, res)).accessToken;
 
         const baseUrl = `${apiServerBaseUrl}/case/v1/tasks/assignments`;
 
         const loggingContext = { ...logCtx, baseUrl };
-        logTrace('assignTask::start', loggingContext);
+        logTrace('claimNextTask::start', loggingContext);
 
         const config = {
             headers: {
@@ -26,11 +27,10 @@ export default withAuthAndLogging(
         };
 
         try {
-            const data = await serverApi.post(baseUrl, config);
-            logTrace('assignTask::post::success::task claimed successful', { ...loggingContext, duration: performance.now() - now });
-            return res.status(200).send(await data.json());
+            const data = await serverApi.post<unknown, AxiosResponse<ClaimNextTask>>(baseUrl, {}, config);
+            return res.status(200).json(data.data);
         } catch (error) {
-            logWarn('assignTask::post::error::something went wrong while claiming task', {
+            logWarn('claimNextTask::post::error::something went wrong while claiming task', {
                 ...parseErrorInformation(error),
                 ...loggingContext,
                 duration: performance.now() - now,
