7b965c6833c595b2fba264b01a54ef00ec813619
Ed Sztukowski <edsztukowski@gmail.com>
2025-03-24 13:04:34 -0600

    update prepush command to not need an upstream branch (#1367)

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-changed=$(git diff --name-only @{u}..HEAD -- apps/*)
+# Navigate to the root directory of the repository
+cd "$(git rev-parse --show-toplevel)"
 
+# Get list of changed files between the previous commit and the current commit
+changed=$(git diff --name-only HEAD^1 -- apps/*)
 if [ -n "$changed" ]; then
   processed_apps=()
   for app in $changed; do
-    package_name=$(basename $(dirname $app))
+  package_name=$(echo $app | cut -d'/' -f2)
+
     if [[ ! " ${processed_apps[@]} " =~ " ${package_name} " ]]; then
       turbo run pre-push --filter=$package_name
       processed_apps+=($package_name)
