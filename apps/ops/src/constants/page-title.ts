dcc078e9cd2222deb051bd63c459aac30eb21f9b
brian-byers-zinnia <143027957+brian-byers-zinnia@users.noreply.github.com>
2025-04-29 09:53:02 -0600

    [DEPU-4643]: renames Zinnia Open Insurance to Zinnia Live (#1618)

--- apps/ops/src/constants/page-title.ts
+++ apps/ops/src/constants/page-title.ts
@@ -1,6 +1,6 @@
-export const DEFAULT_PAGE_TITLE = 'Zinnia Open Insurance'
+export const DEFAULT_PAGE_TITLE = 'Zinnia Live';
 
 export const CarrierToCarrierTitleMap: Record<string, string> = {
-    'NASU': 'NASSAU',
-    'FLIC': 'Forethought'
-}
+    NASU: 'NASSAU',
+    FLIC: 'Forethought',
+};
