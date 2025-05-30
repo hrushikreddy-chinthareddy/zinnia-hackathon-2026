6e1aedc94fb7080057fde99fe911d6a7fbeb495d
vijaya-zinnia <153061769+vijaya-zinnia@users.noreply.github.com>
2025-05-30 18:34:07 +0530

    DEPU-4433 Updates (#1814)

--- apps/ops/src/containers/task-container/task-handlers/tasks/nb-application-review.ts
+++ apps/ops/src/containers/task-container/task-handlers/tasks/nb-application-review.ts
@@ -20,10 +20,7 @@ const applicationReviewHandler: TaskHandler<ReviewPayload, NigoExceptionResponse
             metadata[0].formSchema.definitions.declineReason = { enum: reasonList.map(reason => JSON.stringify(reason)) };
         }
 
-        metadata[0].uiSchema.declineReason['ui:options'].enumOptions = reasonList.map(reason => ({
-            label: reason.detailedReason,
-            value: JSON.stringify(reason),
-        }));
+        metadata[0].uiSchema.declineReason['ui:options'] = { enumNames: reasonList.map(reason => reason.detailedReason) };
     },
 };
 
