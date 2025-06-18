44205ff920d409ecaf210abf8e5d57e3c5830694
Anssam <84331560+Anssam@users.noreply.github.com>
2025-06-18 11:09:28 -0400

    [CUI-850] Update document logic to use themeCookie instead of getSubdomain function (#1960)

--- apps/consumer-experience/src/utils/documents.ts
+++ apps/consumer-experience/src/utils/documents.ts
@@ -1,8 +1,6 @@
-import { headers } from 'next/headers';
-
 import { Subdomains } from '@/types/carriers';
 
-import { getSubdomain } from './url';
+import { getThemeCookies } from './theme';
 
 // TODO: decided to do this based on subdomain, but i could also update this to take
 // in policyNumber and planCode, make a get documents call and do it on carrierId
@@ -12,8 +10,9 @@ import { getSubdomain } from './url';
 // is that legacy carriers are on v2 and any new carriers from wellabe forward are on
 // v3. If this logic is still being used when onboarding a carrier, be sure to verify
 // which documents service they are using!
-export const retrieveDocumentsFromV2 = (): boolean => {
-  const carrierId = getSubdomain(headers());
+export const retrieveDocumentsFromV2 = async (): Promise<boolean> => {
+  const carrierId = await getThemeCookies();
+
   switch (carrierId) {
     case Subdomains.EVERLY:
       return true;
