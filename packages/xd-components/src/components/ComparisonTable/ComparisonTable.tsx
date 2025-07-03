6d19126bcade168ff24bea1e4518e63ea0a09114
Anssam <84331560+Anssam@users.noreply.github.com>
2025-07-03 15:09:12 -0400

    [CUI-824] Make Summary Page Comparison Component (#2110)
    Co-authored-by: Ximena Girasol <37059904+GirasolXimena@users.noreply.github.com>
    Co-authored-by: Ximena Girasol <ximena.girasol@zinnia.com>

---
+++ packages/xd-components/src/components/ComparisonTable/ComparisonTable.tsx
@@ -0,0 +1,56 @@
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderCell,
+  TableRow,
+  TableCell,
+} from '@zinnia/bloom/components';
+import styles from './ComparisonTable.module.css';
+import { ComparisonTableProps } from './types';
+import clsx from 'clsx';
+import { ComparisonField } from './ComparisonField';
+
+export const ComparisonTable = ({
+  newValueHeader,
+  currentValueHeader,
+  data,
+}: ComparisonTableProps) => {
+  const renderHeaders = () =>
+    [newValueHeader, currentValueHeader].map((header, index) => (
+      <TableHeaderCell key={index}>{header}</TableHeaderCell>
+    ));
+
+  const renderRows = () =>
+    Object.values(data).map(
+      ({ label, newValue, currentValue, onEdit, isNew }, idx) => (
+        <TableRow key={idx}>
+          <TableCell>
+            <ComparisonField header={label} onEdit={onEdit} />
+          </TableCell>
+          <TableCell>
+            <ComparisonField subtext={newValue} isNew={isNew} />
+          </TableCell>
+          {currentValue && (
+            <TableCell>
+              <ComparisonField subtext={currentValue} />
+            </TableCell>
+          )}
+        </TableRow>
+      )
+    );
+
+  return (
+    <div className={styles.tableContainer}>
+      <Table className={clsx(styles.tableWrapper, styles.desktopView)}>
+        <TableHeader>
+          <TableRow>
+            <TableHeaderCell />
+            {renderHeaders()}
+          </TableRow>
+        </TableHeader>
+        <TableBody>{renderRows()}</TableBody>
+      </Table>
+    </div>
+  );
+};
