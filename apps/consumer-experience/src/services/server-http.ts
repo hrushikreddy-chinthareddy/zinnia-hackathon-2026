866c82cae7ab3fddbde57f775b97c621e2066529
mclarkson-zinnia <155466927+mclarkson-zinnia@users.noreply.github.com>
2025-07-23 12:59:52 -0700

    [CUI-989] Add logic for using welb auth0 tokens (#2385)

--- apps/consumer-experience/src/services/server-http.ts
+++ apps/consumer-experience/src/services/server-http.ts
@@ -6,6 +6,7 @@ import {
   MfaAssociateInputs,
   MfaSendChallengeInputs,
 } from '@/types/auth';
+import { Subdomains } from '@/types/carriers';
 import { getAccessToken, getSession } from '@/utils/auth';
 import { logTrace } from '@/utils/logging/log-fns';
 import {
@@ -14,8 +15,11 @@ import {
   LoggingModule,
   LoggingStage,
 } from '@/utils/logging/server-logging';
+import { FEATURE_FLAGS } from '@/utils/optimizely/flags';
 import { AUTH0_SCOPE } from '@/utils/serverClientUtils';
+import { getSubdomain } from '@/utils/url';
 
+import { getFeatureFlags } from './feature-flags';
 import { HttpRequest } from './http';
 
 function getIp() {
@@ -76,6 +80,25 @@ class ServerHttpRequest extends HttpRequest {
   };
 
   sendVerificationCode = async (email: string) => {
+    let clientId = process.env.AUTH0_CLIENT_ID;
+    let clientSecret = process.env.AUTH0_CLIENT_SECRET;
+
+    const featureFlagDecisions = await getFeatureFlags();
+
+    // client_id and client_secret here are using wellabe specific variables so that the email with the code
+    // has the correct wellabe logo. This is a TEMPORARY change because it's the only way for us to know what
+    // subdomain the user is on before sending the email since we do not have access to organizations through
+    // this custom auth0 flow. These variables are defined on a separate auth0 application that is a duplicate of
+    // the mypolicyview application
+    if (featureFlagDecisions?.[FEATURE_FLAGS.WELLABE_AUTH_TOKENS]) {
+      const subDomain = getSubdomain(headers());
+
+      if (subDomain === Subdomains.WELLABE) {
+        clientId = process.env.AUTH0_CLIENT_ID_WELB;
+        clientSecret = process.env.AUTH0_CLIENT_SECRET_WELB;
+      }
+    }
+
     return fetch(`${process.env.AUTH0_ISSUER_BASE_URL}/passwordless/start`, {
       method: 'POST',
       headers: {
@@ -83,8 +106,8 @@ class ServerHttpRequest extends HttpRequest {
       },
       cache: 'no-store',
       body: JSON.stringify({
-        client_id: process.env.AUTH0_CLIENT_ID,
-        client_secret: process.env.AUTH0_CLIENT_SECRET,
+        client_id: clientId,
+        client_secret: clientSecret,
         connection: 'email',
         email,
         send: 'code',
