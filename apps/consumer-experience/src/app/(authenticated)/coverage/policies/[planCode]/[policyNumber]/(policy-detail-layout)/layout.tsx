b5062466851fe875461efc8f36a50e7f9ce6a26a
Anssam <84331560+Anssam@users.noreply.github.com>
2025-08-06 15:01:04 -0400

    [CUI-1023] Improve logging - add data validation (#2471)

--- apps/consumer-experience/src/app/(authenticated)/coverage/policies/[planCode]/[policyNumber]/(policy-detail-layout)/layout.tsx
+++ apps/consumer-experience/src/app/(authenticated)/coverage/policies/[planCode]/[policyNumber]/(policy-detail-layout)/layout.tsx
@@ -5,7 +5,12 @@ import { Footer } from '@/components/footer/Footer';
 import { PolicyDetailPageHeader } from '@/components/policy-detail-page-header/PolicyDetailPageHeader';
 import { PolicyStatusAlertBanner } from '@/components/policy-status-alert-banner/PolicyStatusAlertBanner';
 import { getPolicyStatusDetails } from '@/services';
-import { buildCommonLogContext } from '@/utils/logging/server-logging';
+import { logTrace } from '@/utils/logging/log-fns';
+import {
+  buildCommonLogContext,
+  LoggingModule,
+  LoggingStage,
+} from '@/utils/logging/server-logging';
 
 // disable because NextJS needs this to be exported from this file
 // eslint-disable-next-line react-refresh/only-export-components
@@ -28,6 +33,26 @@ export default async function AuthenticatedLayout({
 }) {
   const { planCode, policyNumber } = params;
   const loggingContext = await buildCommonLogContext();
+
+  if (
+    !planCode ||
+    planCode === 'undefined' ||
+    !policyNumber ||
+    policyNumber === 'undefined'
+  ) {
+    logTrace(
+      `${LoggingModule.PAGE}::AuthenticatedLayout::Policy::${LoggingStage.ERROR}`,
+      {
+        file: 'app/(authenticated)/coverage/policies/[planCode]/[policyNumber]/(policy-detail-layout)/layout.tsx',
+        function: 'AuthenticatedLayout',
+        error: `Missing planCode or policyNumber in params - planCode: ${planCode}, policyNumber: ${policyNumber}`,
+        planCode,
+        policyNumber,
+        ...loggingContext,
+      }
+    );
+  }
+
   const { data } = await getPolicyStatusDetails(
     { planCode, policyNumber },
     loggingContext
