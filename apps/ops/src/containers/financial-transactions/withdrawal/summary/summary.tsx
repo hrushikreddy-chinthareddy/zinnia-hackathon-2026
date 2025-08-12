680a090f0b554b86b6661fcdd7354d255587a480
Ashish Suhane <ashish.suhane@zinnia.com>
2025-08-12 17:57:10 +0530

    feat-DEPU-6449 fixed lint issues

--- apps/ops/src/containers/financial-transactions/withdrawal/summary/summary.tsx
+++ apps/ops/src/containers/financial-transactions/withdrawal/summary/summary.tsx
@@ -288,11 +288,9 @@ const Summary = ({ policy }: SummaryProps) => {
                         ) as DisbursementPaymentForm
                     }
                     ownerTaxState={ownerTaxState}
-                    requestedAmountDollarAmount={
-                        numberFormatify(
-                            policy.withdrawalValues?.maximumWithdrawalAmount
-                        ) || 0
-                    }
+                    requestedAmountDollarAmount={numberFormatify(
+                        policy.withdrawalValues?.maximumWithdrawalAmount || 0
+                    )}
                     // TODO MG: confirm this comment is valid
                     // Hardcoded for MVP - get back null usually (should that be 100?)
                     totalAllocationAmount={totalPayment}
