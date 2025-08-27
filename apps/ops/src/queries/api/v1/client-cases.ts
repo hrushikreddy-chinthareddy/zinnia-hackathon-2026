7d2b7b24bdcf24acdd3a47118b9f2403b25977e6
Ed Sztukowski <edsztukowski@gmail.com>
2025-08-27 11:25:44 -0600

    add PATCH endpoint

--- apps/ops/src/queries/api/v1/client-cases.ts
+++ apps/ops/src/queries/api/v1/client-cases.ts
@@ -117,6 +117,45 @@ export const saveIllustrationToClientCase = async (
     }
 };
 
+export const editIllustrationToClientCase = async (
+    clientCaseId: string,
+    oldIllustrationId: string,
+    title: string,
+    productType: string,
+    productId: string,
+    inputs: string
+) => {
+    try {
+        const request = client.patch(
+            `${BASE_URL}/${clientCaseId}/illustrations/${oldIllustrationId}`,
+            { id: oldIllustrationId, title, productType, productId, inputs }
+        );
+        const response = await request;
+        if (response.status === StatusCode.Okay) {
+            return { data: response.data, error: null };
+        } else {
+            const error = new Error(response?.statusText);
+            return {
+                data: null,
+                error: { ...error, status: response?.status || 500 },
+            };
+        }
+    } catch (e) {
+        return {
+            data: null,
+            error: {
+                ...(e instanceof Error
+                    ? e
+                    : new Error(
+                          (e as Error)?.message ||
+                              'an error occurred while editing illustration on client case'
+                      )),
+                status: 500,
+            },
+        };
+    }
+};
+
 export const selectIllustrationForApplication = async (
     clientCaseId: string,
     illustrationId: string
