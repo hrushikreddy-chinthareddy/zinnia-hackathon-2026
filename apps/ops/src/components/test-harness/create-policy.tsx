b1f54f8419e64dc3592195869b55c0b2243a62ec
Ed Sztukowski <edsztukowski@gmail.com>
2025-08-28 11:34:49 -0600

    add more farmers agents

--- apps/ops/src/components/test-harness/create-policy.tsx
+++ apps/ops/src/components/test-harness/create-policy.tsx
@@ -96,29 +96,12 @@ const policyStateOptions = [
     },
 ];
 
-const agentSelectOptions = [
-    ...(isEvglDemo()
-        ? [
-              {
-                  textValue: 'KEELY.FOUX+EVERGLADESAGENT@ZINNIA.COM',
-                  value: 'Agent1',
-              },
-              {
-                  textValue: 'ALI.KHATAMI+EVERGLADESAGENT@ZINNIA.COM',
-                  value: 'Agent2',
-              },
-          ]
-        : []),
-
-    ...(isFarmersTraining()
-        ? [
-              {
-                  textValue: 'fitz953@farmersoktauser.com',
-                  value: 'Agent3',
-              },
-          ]
-        : []),
-];
+function generateFarmersAgentsArray(agents: Record<string, Agent>) {
+    return Object.entries(agents).map(([key, agent]) => ({
+        textValue: `${key} ${agent.firstName} ${agent.lastName}`,
+        value: key,
+    }));
+}
 
 const evergladesAgents = {
     Agent1: {
@@ -137,19 +120,333 @@ const evergladesAgents = {
     },
 };
 
-const farmersAgents = {
-    Agent3: {
+interface Agent {
+    firstName: string;
+    lastName: string;
+    producerKey: string;
+    licenseNumber: string;
+    companyProducerID: string;
+}
+
+const farmersAgents: Record<string, Agent> = {
+    '1020977': {
         firstName: 'Johnny',
         lastName: 'Fitzmaurice',
-        producerKey: '97447600037877',
+        producerKey: '99112J1020977',
         licenseNumber: '',
-        companyProducerID: '97447600037877',
+        companyProducerID: '99112J1020977',
+    },
+    '23901': {
+        firstName: '',
+        lastName: '',
+        producerKey: '8410123901',
+        licenseNumber: '',
+        companyProducerID: '',
+    },
+    '26998': {
+        firstName: 'Josephine',
+        lastName: 'Hanan',
+        producerKey: '14887326998',
+        licenseNumber: '',
+        companyProducerID: '14887326998',
+    },
+    '761139': {
+        firstName: 'Brenda',
+        lastName: 'Hickman',
+        producerKey: '180401761139',
+        licenseNumber: '',
+        companyProducerID: '180401761139',
+    },
+    '885771': {
+        firstName: 'Michael',
+        lastName: 'Barclay',
+        producerKey: '298201885771',
+        licenseNumber: '',
+        companyProducerID: '298201885771',
+    },
+    '972915': {
+        firstName: 'William',
+        lastName: 'Brown',
+        producerKey: '357401972915',
+        licenseNumber: '',
+        companyProducerID: '357401972915',
+    },
+    '1029126': {
+        firstName: 'JEFFREY',
+        lastName: 'GUTHALS',
+        producerKey: '3574741029126',
+        licenseNumber: '',
+        companyProducerID: '3574741029126',
+    },
+    '747676': {
+        firstName: 'Dolores',
+        lastName: 'Rosales Valenzuela',
+        producerKey: '357474747676',
+        licenseNumber: '',
+        companyProducerID: '357474747676',
+    },
+    '21051': {
+        firstName: 'Giovanna',
+        lastName: 'Ramirez',
+        producerKey: '71734021051',
+        licenseNumber: '',
+        companyProducerID: '71734021051',
+    },
+    '1002799': {
+        firstName: 'Jay',
+        lastName: 'Everidge',
+        producerKey: '7907561002799',
+        licenseNumber: '',
+        companyProducerID: '7907561002799',
+    },
+    '989296': {
+        firstName: 'Michael',
+        lastName: 'Brady',
+        producerKey: '889931989296',
+        licenseNumber: '',
+        companyProducerID: '889931989296',
+    },
+    '98844': {
+        firstName: 'Denitra',
+        lastName: 'Lockhart',
+        producerKey: '95170198844',
+        licenseNumber: '',
+        companyProducerID: '95170198844',
+    },
+    '994133': {
+        firstName: 'Rodrigo',
+        lastName: 'salas',
+        producerKey: '951749994133',
+        licenseNumber: '',
+        companyProducerID: '951749994133',
+    },
+    '54077': {
+        firstName: 'Colin',
+        lastName: 'Lovett',
+        producerKey: '95174954077',
+        licenseNumber: '',
+        companyProducerID: '95174954077',
+    },
+    '1541079': {
+        firstName: 'Stephanie',
+        lastName: 'Patino',
+        producerKey: '9744761541079',
+        licenseNumber: '',
+        companyProducerID: '9744761541079',
+    },
+    '37877': {
+        firstName: 'Kerri',
+        lastName: 'Watercutter',
+        producerKey: '97447637877',
+        licenseNumber: '',
+        companyProducerID: '97447637877',
+    },
+    '884728': {
+        firstName: 'Donald',
+        lastName: 'Wagner',
+        producerKey: '991101884728',
+        licenseNumber: '',
+        companyProducerID: '991101884728',
+    },
+    '1026121': {
+        firstName: 'JOHN',
+        lastName: 'HENNESSEY',
+        producerKey: '06093M1026121',
+        licenseNumber: '',
+        companyProducerID: '06093M1026121',
+    },
+    '1001192': {
+        firstName: 'Sylvia',
+        lastName: 'Willson',
+        producerKey: '0609J31001192',
+        licenseNumber: '',
+        companyProducerID: '0609J31001192',
+    },
+    '1013967': {
+        firstName: 'Denise',
+        lastName: 'Scott',
+        producerKey: '0715AF1013967',
+        licenseNumber: '',
+        companyProducerID: '0715AF1013967',
+    },
+    '1134661': {
+        firstName: 'Danielle',
+        lastName: 'Neble',
+        producerKey: '08081V1134661',
+        licenseNumber: '',
+        companyProducerID: '08081V1134661',
+    },
+    '1022333': {
+        firstName: 'Ryan',
+        lastName: 'Jolly',
+        producerKey: '18048V1022333',
+        licenseNumber: '',
+        companyProducerID: '18048V1022333',
+    },
+    '10001': {
+        firstName: 'Jay',
+        lastName: 'Strickland',
+        producerKey: '19257A10001',
+        licenseNumber: '',
+        companyProducerID: '19257A10001',
+    },
+    '15062': {
+        firstName: 'Chase',
+        lastName: 'Barney',
+        producerKey: '19677T15062',
+        licenseNumber: '',
+        companyProducerID: '19677T15062',
+    },
+    '1490826': {
+        firstName: 'Mathew',
+        lastName: 'Knight',
+        producerKey: '2223EF1490826',
+        licenseNumber: '',
+        companyProducerID: '2223EF1490826',
+    },
+    '1028816': {
+        firstName: 'Gary',
+        lastName: 'Fischer',
+        producerKey: '35205F1028816',
+        licenseNumber: '',
+        companyProducerID: '35205F1028816',
+    },
+    '886050': {
+        firstName: 'LUAN',
+        lastName: 'HO',
+        producerKey: '35206M886050',
+        licenseNumber: '',
+        companyProducerID: '35206M886050',
+    },
+    '108748': {
+        firstName: 'Jeffrey',
+        lastName: 'Coppola',
+        producerKey: '35766M108748',
+        licenseNumber: '',
+        companyProducerID: '35766M108748',
+    },
+    '1024455': {
+        firstName: 'DONNA',
+        lastName: 'KRAMER',
+        producerKey: '3660CX1024455',
+        licenseNumber: '',
+        companyProducerID: '3660CX1024455',
+    },
+    '1236874': {
+        firstName: 'Tihomir',
+        lastName: 'Vujatov',
+        producerKey: '3660CX1236874',
+        licenseNumber: '',
+        companyProducerID: '3660CX1236874',
+    },
+    '1464470': {
+        firstName: 'Tyler',
+        lastName: 'Barnes',
+        producerKey: '6722CF1464470',
+        licenseNumber: '',
+        companyProducerID: '6722CF1464470',
+    },
+    '1029420': {
+        firstName: 'John',
+        lastName: 'Reed',
+        producerKey: '71159C1029420',
+        licenseNumber: '',
+        companyProducerID: '71159C1029420',
+    },
+    '16194': {
+        firstName: 'meng',
+        lastName: 'chang',
+        producerKey: '71159C16194',
+        licenseNumber: '',
+        companyProducerID: '71159C16194',
+    },
+    '1028000': {
+        firstName: 'Renne',
+        lastName: 'Lehmann',
+        producerKey: '7173F61028000',
+        licenseNumber: '',
+        companyProducerID: '7173F61028000',
+    },
+    '1267610': {
+        firstName: 'Kevin',
+        lastName: 'Anderson',
+        producerKey: '7309AK1267610',
+        licenseNumber: '',
+        companyProducerID: '7309AK1267610',
+    },
+    '1110079': {
+        firstName: 'Michael',
+        lastName: 'Monju',
+        producerKey: '7309KH1110079',
+        licenseNumber: '',
+        companyProducerID: '7309KH1110079',
+    },
+    '105082': {
+        firstName: 'Craig',
+        lastName: 'Smith',
+        producerKey: '88115V105082',
+        licenseNumber: '',
+        companyProducerID: '88115V105082',
+    },
+    '82412': {
+        firstName: 'Laura',
+        lastName: 'Servin',
+        producerKey: '88116K82412',
+        licenseNumber: '',
+        companyProducerID: '88116K82412',
+    },
+    '1483185': {
+        firstName: 'George',
+        lastName: 'Mandas',
+        producerKey: '88116K1483185',
+        licenseNumber: '',
+        companyProducerID: '88116K1483185',
+    },
+    '1011456': {
+        firstName: 'Anm',
+        lastName: 'Badruddoza',
+        producerKey: '95312V1011456',
+        licenseNumber: '',
+        companyProducerID: '95312V1011456',
+    },
+    '33060': {
+        firstName: 'Diane',
+        lastName: 'Shopteau',
+        producerKey: '96659C33060',
+        licenseNumber: '',
+        companyProducerID: '96659C33060',
+    },
+    '1214899': {
+        firstName: 'Robert',
+        lastName: 'Carolla',
+        producerKey: '9665A61214899',
+        licenseNumber: '',
+        companyProducerID: '9665A61214899',
     },
 };
 
+const agentSelectOptions = [
+    ...(isEvglDemo()
+        ? [
+              {
+                  textValue: 'KEELY.FOUX+EVERGLADESAGENT@ZINNIA.COM',
+                  value: 'Agent1',
+              },
+              {
+                  textValue: 'ALI.KHATAMI+EVERGLADESAGENT@ZINNIA.COM',
+                  value: 'Agent2',
+              },
+          ]
+        : []),
+
+    ...(isFarmersTraining()
+        ? [...generateFarmersAgentsArray(farmersAgents)]
+        : []),
+];
+
 const defaultAgent = isEvglDemo()
     ? { agent: 'Agent1', ...evergladesAgents.Agent1 }
-    : { agent: 'Agent3', ...farmersAgents.Agent3 };
+    : { agent: '1020977', ...farmersAgents['1020977'] };
 
 const agentValues = {
     ...(isEvglDemo() ? evergladesAgents : {}),
