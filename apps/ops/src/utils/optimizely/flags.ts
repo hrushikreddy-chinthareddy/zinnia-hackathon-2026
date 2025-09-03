4d65c204f90828ec3d26523e4ade6963bfb1c843
Saurav Rath <160708675+saurav-zinnia@users.noreply.github.com>
2025-09-03 17:45:02 +0530

    [CMW-31478]-NASU RMD - Unexpected Nigo for DOB mismatch is getting generated when DOB is matching with LifeCad. (#2816)

--- apps/ops/src/utils/optimizely/flags.ts
+++ apps/ops/src/utils/optimizely/flags.ts
@@ -137,7 +137,7 @@ export enum FEATURE_FLAGS {
     SYSTEMATIC_RMD_TRANSACTION = 'systematic_rmd_transaction',
     FAST_WITHDRAWAL = 'fast-withdrawal',
     FAST_OFT = 'fast-oft',
-    FAST_RMD = 'fast-rmd',
+    // FAST_RMD = 'fast-rmd',
     FAST_SSW = 'fast-ssw',
     CALL_AUDIO_FEATURE = 'call-audio-feature',
     NEW_DEATH_CLAIM = 'new_death_claim',
