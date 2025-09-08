f206190209f8e165340e7dd65fd61b1d6d119db7
Saurav Rath <160708675+saurav-zinnia@users.noreply.github.com>
2025-09-08 12:53:29 +0530

    DEPU-5119--Show the case dropped out for manual processing in the UI  (#2731)

--- apps/ops/src/containers/nigo-entry-container/components/steps/service-form-review/service-form-review-step.tsx
+++ apps/ops/src/containers/nigo-entry-container/components/steps/service-form-review/service-form-review-step.tsx
@@ -3,38 +3,19 @@ import {
     AssistiveTextVariant,
     Loader,
 } from '@zinnia/bloom/components';
-import dayjs from 'dayjs';
 import { useTranslation } from 'next-i18next';
-import { useCallback, useContext, useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import TransactionNavigationButtons, {
     ParentPage,
 } from '@deps/components/transaction-navigation-buttons/transaction-navigation-buttons';
 import WorkflowCard from '@deps/components/workflows/workflow-card/workflow-card';
 import { TranslationFiles } from '@deps/config/translations';
-import { buildFormV2 } from '@deps/containers/otp/withdrawal-forms/utils/withdrawal-form-helpers';
 import { NigoSubException } from '@deps/containers/task-container/components/steps/nigo-details/nigo-details.types';
-import { FormDataContext } from '@deps/contexts/OtpWithdrawalFormContext';
-import { useWorkflow } from '@deps/contexts/WorkflowContainerContext';
-import { isEmptyObject } from '@deps/helpers/objects.helpers';
-import { isNullEmptyOrUndefined } from '@deps/helpers/string.helpers';
+import { useServiceFormReview } from '@deps/hooks/useServiceFormReview';
 import { DocumentData } from '@deps/models/case/document';
-import { ApiVersion } from '@deps/models/case/enums';
-import { TaskApiVersionMapper } from '@deps/models/case/helpers';
-import { TaskStatus } from '@deps/models/case/task-instance';
-import {
-    FormValidationErrors,
-    NigoMessages,
-} from '@deps/models/case/withdrawal/case';
-import { updateTask } from '@deps/queries/api/v2/task';
-import { ZAHARA_API_DATE_FORMAT } from '@deps/types/constants';
-import { browserLogInfo } from '@deps/utils/browser-logging';
 
-import { SuggestedDocType } from './document-indexing-info';
-import { SelOptionType, ServiceFormReview } from './service-form-review';
-import { getFormData } from './service-form-review.helpers';
-import { useNigoEntry } from '../../nigo-entry-provider';
-import { getCaseType } from '../form-entry/form-entry-step.helpers';
+import { ServiceFormReview } from './service-form-review';
 import { NigoException } from '../nigo-details/nigo-details.types';
 
 interface ServiceFormReviewStepProps {
@@ -60,241 +41,22 @@ export const ServiceFormReviewStep = ({
     const { t } = useTranslation(TranslationFiles.COMMON, {
         keyPrefix: 'nigoEntry.serviceFormReview',
     });
-    const { goToNext } = useWorkflow();
-
-    const {
-        sectionOption,
-        documentIndexingInfo,
-        formErrors,
-        setFormErrors,
-        setSubmitFailed,
-        setMessages,
-        messages,
-    } = useNigoEntry();
-    const formState = useContext(FormDataContext);
-
-    const {
-        formSource,
-        setFormSource,
-        setFormData,
-        formSubtype,
-        setFormReindexingData,
-        setFormNigos,
-        formComment,
-        setFormComment,
-    } = formState;
-    const caseType = getCaseType(docType as string);
-    const carrier = clientCode?.toUpperCase();
     const [isLoading, setIsLoading] = useState(false);
-    const [timer] = useState(performance.now());
-    const filteredNigoException = nigoExceptions?.find(
-        (nigoException) =>
-            nigoException.label === 'Case routed for manual processing'
-    );
-    const NIGO_EXCEPTION = filteredNigoException?.value;
-    const subExceptions = nigoSubExceptions?.find(
-        (subItem: NigoSubException) => subItem.nmId === NIGO_EXCEPTION
-    )?.subExceptions;
-    const notesSubException = subExceptions?.find((item: any) => {
-        return item.label === 'Validation failed due to reason not listed.';
-    })?.value;
 
-    const submit = useCallback(async () => {
-        setIsLoading(true);
-
-        if (
-            TaskApiVersionMapper[formState.initialForm.taskType] ===
-                ApiVersion.v2 &&
-            formState.initialForm.status !== TaskStatus.Completed
-        ) {
-            const successfulCaseUpdate = await updateTask(
-                formState.initialForm.caseId,
-                formState.initialForm.taskId,
-                buildFormV2(TaskStatus.Completed, document, formState),
-                timer
-            );
-
-            if (successfulCaseUpdate && successfulCaseUpdate.id) {
-                browserLogInfo(
-                    'ServiceFormReviewStep::submit::Successfully updated task',
-                    {
-                        caseId: formState.initialForm.caseId,
-                        taskId: formState.initialForm.taskId,
-                        id: successfulCaseUpdate.id,
-                    }
-                );
-                setSubmitFailed(false);
-            } else {
-                browserLogInfo(
-                    'ServiceFormReviewStep::submit::Failed to update task',
-                    {
-                        caseId: formState.initialForm.caseId,
-                        taskId: formState.initialForm.taskId,
-                        id: successfulCaseUpdate.id,
-                    }
-                );
-                setSubmitFailed(true);
-            }
-        } else {
-            setSubmitFailed(false);
-        }
-
-        setIsLoading(false);
-    }, [document, formState, setSubmitFailed, timer]);
-
-    const handleStepContinue = useCallback(async () => {
-        const errors = {} as FormValidationErrors;
-        setFormErrors({});
-
-        if (sectionOption === SelOptionType.DOC_INDEXING) {
-            if (
-                isNullEmptyOrUndefined(documentIndexingInfo?.docTypeToReindex)
-            ) {
-                errors['noDocTypeToReindex'] = t(
-                    'formErrors.formValidation.noDocTypeToReindex'
-                );
-            }
-            if (
-                !isNullEmptyOrUndefined(
-                    documentIndexingInfo?.docTypeToReindex
-                ) &&
-                documentIndexingInfo?.docTypeToReindex ===
-                    SuggestedDocType.OTHER &&
-                isNullEmptyOrUndefined(documentIndexingInfo?.notes)
-            ) {
-                errors['noNotes'] = t('formErrors.formValidation.noNotes');
-            }
-        }
-
-        if (sectionOption === NIGO_EXCEPTION) {
-            if (
-                messages[NIGO_EXCEPTION] === undefined ||
-                isEmptyObject(messages[NIGO_EXCEPTION])
-            ) {
-                errors['noCategoryDetailsSelected'] = t(
-                    'formErrors.formValidation.noCategoryDetailsSelected'
-                );
-            } else {
-                const selectedMessage = messages[NIGO_EXCEPTION]
-                    ? Object.keys(messages[NIGO_EXCEPTION])
-                    : [];
-                if (selectedMessage.includes(notesSubException as string)) {
-                    if (isNullEmptyOrUndefined(formComment?.comment)) {
-                        errors['noComment'] = t(
-                            'formErrors.formValidation.noComment'
-                        );
-                    }
-                }
-            }
-        }
-
-        setFormErrors(errors);
-        if (Object.keys(errors).length === 0) {
-            if (
-                [SelOptionType.DOC_INDEXING, NIGO_EXCEPTION].includes(
-                    sectionOption
-                )
-            ) {
-                await submit();
-                goToNext();
-            } else {
-                goToNext();
-            }
-        }
-    }, [
-        documentIndexingInfo,
-        goToNext,
-        sectionOption,
-        setFormErrors,
-        submit,
-        messages,
-        formComment,
+    const {
+        formErrors,
+        handleStepContinue,
+        filteredNigoException,
+        isRenewals,
+    } = useServiceFormReview({
         t,
-    ]);
-
-    useEffect(() => {
-        if (
-            sectionOption === NIGO_EXCEPTION &&
-            Object.keys(formErrors).length === 0
-        ) {
-            const nigos: NigoMessages[] = [];
-            const obj = {
-                exceptionId: NIGO_EXCEPTION,
-                messages: !isEmptyObject(messages)
-                    ? Object.keys(messages[NIGO_EXCEPTION])
-                    : [],
-            };
-            nigos.push(obj);
-            setFormNigos({ nigos: nigos });
-        } else {
-            setFormNigos(null);
-        }
-    }, [formErrors, messages, setFormNigos]);
-
-    useEffect(() => {
-        setFormSource({
-            ...formSource,
-            channel: {
-                text: document.source,
-            },
-            businessKey: document.documentNumber,
-            receivedDate: dayjs(
-                document.dateReceived,
-                'M/D/YYYY hh:mm:ss A'
-            ).format(ZAHARA_API_DATE_FORMAT),
-            receivedDateTime: dayjs(
-                document.dateReceived,
-                'M/D/YYYY hh:mm:ss A'
-            ).format('YYYY-MM-DDTHH:mm:ss:Z'),
-            sourceSysId: 'ONBASE',
-        });
-    }, [document]);
-
-    useEffect(() => {
-        if (carrier && caseType) {
-            const data = getFormData(caseType, carrier, formSubtype);
-            if (data) {
-                setFormData((prevFormData) => {
-                    return {
-                        ...prevFormData,
-                        ...data,
-                    };
-                });
-            }
-        }
-    }, [carrier, caseType, formSubtype]);
-
-    useEffect(() => {
-        if (sectionOption === SelOptionType.DOC_INDEXING) {
-            setFormReindexingData((prevState: any) => ({
-                ...prevState,
-                lob: document.lob,
-                docHandle: document.sysDocumentHandle,
-                docTypeToReindex: documentIndexingInfo?.docTypeToReindex,
-                notes:
-                    documentIndexingInfo?.docTypeToReindex ===
-                    SuggestedDocType.OTHER
-                        ? documentIndexingInfo?.notes
-                        : null,
-            }));
-            setFormNigos(null);
-            setMessages([]);
-            setFormComment({ comment: '' });
-        } else if (sectionOption === SelOptionType.NIGO_ENTRY) {
-            setFormNigos(null);
-            setFormReindexingData(null);
-            setMessages([]);
-            setFormComment({ comment: '' });
-        } else if (sectionOption === NIGO_EXCEPTION) {
-            setFormReindexingData(null);
-            setFormNigos(null);
-        } else {
-            setFormReindexingData(null);
-            setFormNigos(null);
-            setMessages([]);
-            setFormComment({ comment: '' });
-        }
-    }, [sectionOption, document, documentIndexingInfo, NIGO_EXCEPTION]);
+        docType,
+        clientCode,
+        document,
+        nigoExceptions,
+        nigoSubExceptions,
+        setIsLoading,
+    });
 
     return (
         <WorkflowCard
@@ -323,6 +85,7 @@ export const ServiceFormReviewStep = ({
                         docType={docType}
                         nigoExpection={filteredNigoException}
                         nigoSubExceptions={nigoSubExceptions}
+                        isRenewals={isRenewals}
                     />
                 </div>
                 <div className="flex flex-col gap-1">
---
+++ apps/ops/src/hooks/useServiceFormReview.ts
@@ -0,0 +1,436 @@
+import dayjs from 'dayjs';
+import { TFunction } from 'next-i18next';
+import { useCallback, useContext, useEffect, useMemo } from 'react';
+
+import { useNigoEntry } from '@deps/containers/nigo-entry-container/components/nigo-entry-provider';
+import { getCaseType } from '@deps/containers/nigo-entry-container/components/steps/form-entry/form-entry-step.helpers';
+import { SuggestedDocType } from '@deps/containers/nigo-entry-container/components/steps/service-form-review/document-indexing-info';
+import { SelOptionType } from '@deps/containers/nigo-entry-container/components/steps/service-form-review/service-form-review';
+import {
+    getFormData,
+    NIGO_EXCEPTIONS_LABEL,
+} from '@deps/containers/nigo-entry-container/components/steps/service-form-review/service-form-review.helpers';
+import { OWNER_TYPES } from '@deps/containers/otp/renewal-forms/components/renewal-form-helpers';
+import { buildFormV2 } from '@deps/containers/otp/withdrawal-forms/utils/withdrawal-form-helpers';
+import { RenewalFormDataContext } from '@deps/contexts/OtpRenewalFormContext';
+import { FormDataContext } from '@deps/contexts/OtpWithdrawalFormContext';
+import { useWorkflow } from '@deps/contexts/WorkflowContainerContext';
+import { isEmptyObject } from '@deps/helpers/objects.helpers';
+import { isNullEmptyOrUndefined } from '@deps/helpers/string.helpers';
+import { DocumentData } from '@deps/models/case/document';
+import { ApiVersion } from '@deps/models/case/enums';
+import { TaskApiVersionMapper } from '@deps/models/case/helpers';
+import { Channel } from '@deps/models/case/renewal/case-renewal';
+import {
+    TaskType,
+    TaskSource,
+    CreateTaskBody,
+    RenewalsFormData,
+} from '@deps/models/case/task';
+import { TaskStatus } from '@deps/models/case/task-instance';
+import { updateTask } from '@deps/queries/api/v2/task';
+import {
+    DEFAULT_DATE_FORMAT,
+    DEFAULT_EXTENDED_DATE_FORMAT,
+    DEFAULT_EXTENDED_DAY_DATE_FORMAT,
+    DEFAULT_EXTENDED_MONTH_DATE_FORMAT,
+    ZAHARA_API_DATE_FORMAT,
+} from '@deps/types/constants';
+import { browserLogInfo } from '@deps/utils/browser-logging';
+
+export interface UseServiceFormReviewLogicProps {
+    t: TFunction;
+    docType: string;
+    clientCode: string;
+    document: DocumentData;
+    nigoExceptions: Array<{ label: string; value: string }>;
+    nigoSubExceptions: Array<{
+        nmId: string;
+        subExceptions: Array<{ label: string; value: string }>;
+    }>;
+    setIsLoading: (loading: boolean) => void;
+}
+
+const SUPPORT_TOOL = 'SupportTool';
+const RENEWAL_TRANSFER = 'RenewalTransfer';
+
+export function useServiceFormReview({
+    t,
+    docType,
+    clientCode,
+    document,
+    nigoExceptions,
+    nigoSubExceptions,
+    setIsLoading,
+}: UseServiceFormReviewLogicProps) {
+    const nigoEntry = useNigoEntry();
+    const { goToNext } = useWorkflow();
+    const formState = useContext(FormDataContext);
+    const renewalFormState = useContext(RenewalFormDataContext);
+
+    const {
+        sectionOption,
+        documentIndexingInfo,
+        formErrors,
+        setFormErrors,
+        setSubmitFailed,
+        setMessages,
+        messages,
+        formComment,
+        setFormComment,
+    } = nigoEntry;
+
+    const {
+        formSource,
+        setFormSource,
+        setFormData,
+        formSubtype,
+        setFormNigos,
+        setFormReindexingData,
+    } = formState;
+
+    const {
+        initialForm,
+        channel,
+        contractValue,
+        renewalRequestSignDate,
+        subsequentTargetFunds,
+        transOption,
+        ownerInformation,
+        upfrontNIGO,
+    } = renewalFormState;
+
+    const caseType = useMemo(() => getCaseType(docType as string), [docType]);
+    const carrier = useMemo(() => clientCode?.toUpperCase(), [clientCode]);
+    const timer = useMemo(() => performance.now(), []);
+    const isRenewals = initialForm.taskType === TaskType.RENEWAL_TASK;
+
+    const filteredNigoException = useMemo(
+        () =>
+            nigoExceptions?.find(
+                (nigoException) =>
+                    nigoException.label ===
+                    NIGO_EXCEPTIONS_LABEL.CASE_ROUTED_MANUAL
+            ),
+        [nigoExceptions]
+    );
+    const NIGO_EXCEPTION = filteredNigoException?.value;
+
+    const subExceptions = useMemo(
+        () =>
+            nigoSubExceptions?.find(
+                (subItem) => subItem.nmId === NIGO_EXCEPTION
+            )?.subExceptions,
+        [nigoSubExceptions, NIGO_EXCEPTION]
+    );
+    const notesSubException = useMemo(
+        () =>
+            subExceptions?.find(
+                (item) =>
+                    item.label ===
+                    NIGO_EXCEPTIONS_LABEL.VALIDATION_FAILED_REASON_NOT_LISTED
+            )?.value,
+        [subExceptions]
+    );
+
+    const getRenewalFormDataPayload = useCallback(() => {
+        const updatedOwnerInformation = ownerInformation?.map((owner) => {
+            if (OWNER_TYPES.includes(owner.type)) {
+                return {
+                    ...owner,
+                    signature: {
+                        ...owner?.signature,
+                        ...(channel === Channel.Phone && {
+                            signDate: renewalRequestSignDate,
+                        }),
+                    },
+                };
+            }
+            return owner;
+        });
+
+        const funds = subsequentTargetFunds?.map((item) => ({
+            fundName: item.fundName,
+            value: item.value,
+            fundCode: item?.fundCode,
+            divisionCode: item?.divisionCode,
+        }));
+
+        return {
+            channel,
+            clientCode: initialForm.carrier,
+            contractNum: document?.contract,
+            documentNumber: document?.documentNumber,
+            contractValue:
+                typeof contractValue === 'string' ? null : contractValue,
+            documentReceivedDate: dayjs(document?.documentDate, [
+                DEFAULT_DATE_FORMAT,
+                DEFAULT_EXTENDED_DAY_DATE_FORMAT,
+                DEFAULT_EXTENDED_MONTH_DATE_FORMAT,
+                DEFAULT_EXTENDED_DATE_FORMAT,
+            ]).format(ZAHARA_API_DATE_FORMAT),
+            goodOrderDate: dayjs().format(ZAHARA_API_DATE_FORMAT),
+            lob: document?.lob,
+            onbaseCaseId: document?.caseId,
+            obPendTaskId: initialForm?.data?.obPendTaskId || null,
+            ownerInformation: updatedOwnerInformation,
+            productName: document?.productName,
+            renewalRequestSignDate,
+            source: SUPPORT_TOOL,
+            subsequentGuaranteePeriod: null,
+            subsequentTargetFunds: funds || null,
+            taskType: RENEWAL_TRANSFER,
+            transOption,
+            userId: initialForm.data.userId,
+            upfrontNIGO: upfrontNIGO,
+        };
+    }, [
+        channel,
+        initialForm,
+        contractValue,
+        document,
+        ownerInformation,
+        renewalRequestSignDate,
+        subsequentTargetFunds,
+        transOption,
+        upfrontNIGO,
+    ]);
+
+    const buildRenewalFormV2 = useCallback(
+        (status: TaskStatus): CreateTaskBody<TaskStatus, RenewalsFormData> => ({
+            source: TaskSource.ZinniaTaskManagement,
+            taskType: initialForm.taskType,
+            status,
+            data: getRenewalFormDataPayload(),
+        }),
+        [initialForm, getRenewalFormDataPayload]
+    );
+
+    const submit = useCallback(async () => {
+        setIsLoading(true);
+        let successfulCaseUpdate;
+
+        if (
+            TaskApiVersionMapper[formState.initialForm.taskType] ===
+                ApiVersion.v2 &&
+            formState.initialForm.status !== TaskStatus.Completed &&
+            !isRenewals
+        ) {
+            successfulCaseUpdate = await updateTask(
+                formState.initialForm.caseId,
+                formState.initialForm.taskId,
+                buildFormV2(TaskStatus.Completed, document, formState),
+                timer
+            );
+            browserLogInfo(
+                successfulCaseUpdate && successfulCaseUpdate.id
+                    ? 'ServiceFormReviewStep::submit::Successfully updated task'
+                    : 'ServiceFormReviewStep::submit::Failed to update task',
+                {
+                    caseId: formState.initialForm.caseId,
+                    taskId: formState.initialForm.taskId,
+                    id: successfulCaseUpdate?.id,
+                }
+            );
+            setSubmitFailed(!(successfulCaseUpdate && successfulCaseUpdate.id));
+        } else if (isRenewals) {
+            successfulCaseUpdate = await updateTask(
+                initialForm.caseId,
+                initialForm.taskId,
+                buildRenewalFormV2(TaskStatus.Completed),
+                timer
+            );
+            browserLogInfo(
+                successfulCaseUpdate && successfulCaseUpdate.id
+                    ? 'ServiceFormReviewStep::submit::Successfully updated task'
+                    : 'ServiceFormReviewStep::submit::Failed to update task',
+                {
+                    caseId: initialForm.caseId,
+                    taskId: initialForm.taskId,
+                    id: successfulCaseUpdate?.id,
+                }
+            );
+            setSubmitFailed(!(successfulCaseUpdate && successfulCaseUpdate.id));
+        } else {
+            setSubmitFailed(false);
+        }
+        setIsLoading(false);
+    }, [
+        document,
+        formState,
+        setSubmitFailed,
+        timer,
+        isRenewals,
+        initialForm,
+        buildRenewalFormV2,
+        setIsLoading,
+    ]);
+
+    const validateForm = useCallback(() => {
+        const errors: any = {};
+        if (sectionOption === SelOptionType.DOC_INDEXING) {
+            if (
+                isNullEmptyOrUndefined(documentIndexingInfo?.docTypeToReindex)
+            ) {
+                errors['noDocTypeToReindex'] = t(
+                    'formErrors.formValidation.noDocTypeToReindex'
+                );
+            }
+            if (
+                !isNullEmptyOrUndefined(
+                    documentIndexingInfo?.docTypeToReindex
+                ) &&
+                documentIndexingInfo?.docTypeToReindex ===
+                    SuggestedDocType.OTHER &&
+                isNullEmptyOrUndefined(documentIndexingInfo?.notes)
+            ) {
+                errors['noNotes'] = t('formErrors.formValidation.noNotes');
+            }
+        }
+        if (sectionOption === NIGO_EXCEPTION) {
+            if (
+                messages[NIGO_EXCEPTION] === undefined ||
+                isEmptyObject(messages[NIGO_EXCEPTION])
+            ) {
+                errors['noCategoryDetailsSelected'] = t(
+                    'formErrors.formValidation.noCategoryDetailsSelected'
+                );
+            } else {
+                const selectedMessage = messages[NIGO_EXCEPTION]
+                    ? Object.keys(messages[NIGO_EXCEPTION])
+                    : [];
+                if (selectedMessage.includes(notesSubException as string)) {
+                    if (isNullEmptyOrUndefined(formComment?.comment)) {
+                        errors['noComment'] = t(
+                            'formErrors.formValidation.noComment'
+                        );
+                    }
+                }
+            }
+        }
+        return errors;
+    }, [
+        sectionOption,
+        documentIndexingInfo,
+        messages,
+        NIGO_EXCEPTION,
+        notesSubException,
+        formComment,
+        t,
+    ]);
+
+    const handleStepContinue = useCallback(async () => {
+        setFormErrors({});
+        const errors = validateForm();
+        setFormErrors(errors);
+
+        if (Object.keys(errors).length === 0) {
+            if (
+                [SelOptionType.DOC_INDEXING, NIGO_EXCEPTION].includes(
+                    sectionOption
+                )
+            ) {
+                await submit();
+                goToNext();
+            } else {
+                goToNext();
+            }
+        }
+    }, [
+        sectionOption,
+        validateForm,
+        submit,
+        goToNext,
+        setFormErrors,
+        NIGO_EXCEPTION,
+    ]);
+
+    useEffect(() => {
+        if (
+            sectionOption === NIGO_EXCEPTION &&
+            Object.keys(formErrors).length === 0
+        ) {
+            const nigos = [
+                {
+                    exceptionId: NIGO_EXCEPTION,
+                    messages: !isEmptyObject(messages)
+                        ? Object.keys(messages[NIGO_EXCEPTION])
+                        : [],
+                },
+            ];
+            setFormNigos({ nigos });
+        } else {
+            setFormNigos(null);
+        }
+    }, [formErrors, messages, sectionOption, NIGO_EXCEPTION]);
+
+    useEffect(() => {
+        setFormSource({
+            ...formSource,
+            channel: { text: document.source },
+            businessKey: document.documentNumber,
+            receivedDate: dayjs(
+                document.dateReceived,
+                'M/D/YYYY hh:mm:ss A'
+            ).format(ZAHARA_API_DATE_FORMAT),
+            receivedDateTime: dayjs(
+                document.dateReceived,
+                'M/D/YYYY hh:mm:ss A'
+            ).format('YYYY-MM-DDTHH:mm:ss:Z'),
+            sourceSysId: 'ONBASE',
+        });
+    }, [document]);
+
+    useEffect(() => {
+        if (carrier && caseType) {
+            const data = getFormData(caseType, carrier, formSubtype);
+            if (data) {
+                setFormData((prevFormData) => {
+                    return {
+                        ...prevFormData,
+                        ...data,
+                    };
+                });
+            }
+        }
+    }, [carrier, caseType, formSubtype]);
+
+    useEffect(() => {
+        if (sectionOption === SelOptionType.DOC_INDEXING) {
+            setFormReindexingData((prevState: any) => ({
+                ...prevState,
+                lob: document.lob,
+                docHandle: document.sysDocumentHandle,
+                docTypeToReindex: documentIndexingInfo?.docTypeToReindex,
+                notes:
+                    documentIndexingInfo?.docTypeToReindex ===
+                    SuggestedDocType.OTHER
+                        ? documentIndexingInfo?.notes
+                        : null,
+            }));
+            setFormNigos(null);
+            setMessages([]);
+            setFormComment({ comment: '' });
+        } else if (sectionOption === SelOptionType.NIGO_ENTRY) {
+            setFormNigos(null);
+            setFormReindexingData(null);
+            setMessages([]);
+            setFormComment({ comment: '' });
+        } else if (sectionOption === NIGO_EXCEPTION) {
+            setFormReindexingData(null);
+            setFormNigos(null);
+        } else {
+            setFormReindexingData(null);
+            setFormNigos(null);
+            setMessages([]);
+            setFormComment({ comment: '' });
+        }
+    }, [sectionOption, document, documentIndexingInfo, NIGO_EXCEPTION]);
+
+    return {
+        formErrors,
+        handleStepContinue,
+        filteredNigoException,
+        isRenewals,
+    };
+}
