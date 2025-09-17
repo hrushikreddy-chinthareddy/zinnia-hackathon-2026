37d1cad8e5827ddf654a27ff7c0e707cd9743745
Vidushi Aggarwal <vidushi.aggarwal@zinnia.com>
2025-09-17 17:04:48 +0530

    feat/DEPU-5011--update-custom-accordion

--- apps/ops/src/components/dynamic-form/customization/templates/address-field-template/address-field-template.tsx
+++ apps/ops/src/components/dynamic-form/customization/templates/address-field-template/address-field-template.tsx
@@ -24,11 +24,16 @@ export default function AddressFieldTemplate({
     uiSchema = {},
     title,
 }: ObjectFieldTemplateProps): JSX.Element {
-    const { backgroundColor, helpText, classes } = getUiOptions(uiSchema);
+    const { backgroundColor, helpText, classes, showDynamicTitle } =
+        getUiOptions(uiSchema);
     const DEFAULT_BACKGROUND_COLOR = 'bg-gray-100';
     const DEFAULT_PADDING = 'p-6';
     const bgColor = backgroundColor ?? DEFAULT_BACKGROUND_COLOR;
     const customClasses = classes ?? DEFAULT_PADDING;
+
+    const addressTypeField = properties.find((p) => p.name === 'addressType');
+    const addressType = addressTypeField?.content?.props?.formData ?? '';
+
     return (
         <div
             className={`w-full max-w-[800px] rounded-md ${bgColor} ${customClasses}`}
@@ -38,6 +43,9 @@ export default function AddressFieldTemplate({
                     <div className={styles.text}>{title} </div>
                     {helpInformation((helpText as string) ?? '')}
                 </div>
+            )}
+            {showDynamicTitle && addressType && (
+                <div className="text-sm">{addressType} </div>
             )}
             <div className="flex flex-wrap gap-2">
                 {properties
