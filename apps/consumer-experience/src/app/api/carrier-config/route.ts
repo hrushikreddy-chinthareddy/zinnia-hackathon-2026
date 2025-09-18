68412ad4357974fc5ea8b216b7a70787771bcb13
Ed Sztukowski <edsztukowski@gmail.com>
2025-09-18 13:27:34 -0600

    fix carrier config route handler

--- apps/consumer-experience/src/app/api/carrier-config/route.ts
+++ apps/consumer-experience/src/app/api/carrier-config/route.ts
@@ -4,16 +4,18 @@ import { getCarrierConfig } from '@/services/carrier-config';
 import { buildCommonLogContext } from '@/utils/logging/server-logging';
 
 export async function GET() {
+  const commonLoggingContext = await buildCommonLogContext();
+
   try {
-    const commonLoggingContext = await buildCommonLogContext();
     return NextResponse.json({
-      data: await getCarrierConfig(commonLoggingContext),
+      data: (await getCarrierConfig(commonLoggingContext)).data, //Since its wrapped in withLogging, just return data here.
       error: null,
     });
   } catch (error) {
     return NextResponse.json({
       data: null,
       error: 'something went wrong',
+      correlationId: commonLoggingContext?.correlationId,
     });
   }
 }
