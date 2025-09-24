14e4e0080f58c70aa8ca323c33ccacfd7db66250
wesitos-globant <pedro.palaciosavila@zinnia.com>
2025-09-24 17:41:29 -0500

    [DEPU-6963] Fix m2m token api endpoints (#2971)

--- apps/ops/src/pages/api/distributors/v1/hierarchies/selling-code/[sellingCode]/downline.ts
+++ apps/ops/src/pages/api/distributors/v1/hierarchies/selling-code/[sellingCode]/downline.ts
@@ -1,29 +1,30 @@
 import { apiServerBaseUrl } from '@deps/queries/api-config';
 import { isApiResponseError } from '@deps/services/api-proxy';
 import { createEnterpriseTokenRequestProxy } from '@deps/services/enterprise-api-token-http';
-import { ProducersResponse } from '@deps/types/producers';
+import { GetDownlineResponse } from '@deps/types/producers';
 import { logWarn, withAuthAndLogging } from '@deps/utils/server-logging';
 
 // Will proxy any request made to the next server directly to the gateway apis
 export default withAuthAndLogging(
-    createEnterpriseTokenRequestProxy<ProducersResponse>({
+    createEnterpriseTokenRequestProxy<GetDownlineResponse[][]>({
         upstreamBaseURL: apiServerBaseUrl,
         matchedURLPath: '/api',
         allowedMethods: ['GET'],
+        omittedParams: ['sellingCode'],
         onProxyErr: (proxyRes, req, res, logCtx) => {
-            if (isApiResponseError(proxyRes?.data)) {
-                logWarn(
-                    'api/distributors/v1/producers/producers::proxyResHandler',
-                    {
-                        error: proxyRes.data.message,
-                        ...logCtx,
-                    }
-                );
+            const logPrefix =
+                'api/distributors/v1/hierarchies/selling-code/[sellingCode]/downline:routeHandler';
+
+            if (isApiResponseError(proxyRes.data)) {
+                logWarn(`${logPrefix}::error`, {
+                    error: proxyRes.data.message,
+                    ...logCtx,
+                });
             }
         },
     }),
     {
-        file: 'distributors/v1/producers/producers',
+        file: 'distributors/v1/hierarchies/selling-code/:selling-code/downline',
         function: 'routeHandler',
     }
 );
