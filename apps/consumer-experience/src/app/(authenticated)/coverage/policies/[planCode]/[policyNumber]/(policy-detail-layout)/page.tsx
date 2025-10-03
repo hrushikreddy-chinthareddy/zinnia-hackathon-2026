e116240f95c5ca96c9571289d163e752f5a7887c
Anssam <84331560+Anssam@users.noreply.github.com>
2025-10-03 14:55:39 -0400

    [CUI-1155] Lapsed Policy Still Showing Coverage Box (#3150)

--- apps/consumer-experience/src/app/(authenticated)/coverage/policies/[planCode]/[policyNumber]/(policy-detail-layout)/page.tsx
+++ apps/consumer-experience/src/app/(authenticated)/coverage/policies/[planCode]/[policyNumber]/(policy-detail-layout)/page.tsx
@@ -69,9 +69,6 @@ export default async function Page({
     if (data?.policyStatus === PolicyStatus.LAPSE) {
       return (
         <Suspense fallback={<LargeSkeleCard />}>
-          {visibility?.[ComponentName.OVERVIEW_COVERAGE]() && (
-            <Coverage planCode={planCode} policyNumber={policyNumber} />
-          )}
           <LapsedPolicy planCode={planCode} policyNumber={policyNumber} />
           <CallForAssistance customInstruction="for help with reinstatement." />
         </Suspense>
