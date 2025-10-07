2ec08a1eb5a2e3d195725bc44d25751bfbd1f133
Alex Sagalovskiy <alex.sagalovskiy@zinnia.com>
2025-10-06 21:03:14 -0600

    enable documentType and excludeDocumentTypes pass-through

--- apps/ops/src/types/documents-v3.ts
+++ apps/ops/src/types/documents-v3.ts
@@ -29,6 +29,7 @@ export type SearchRequest = SearchRequestBody & {
         periodQuarters: ('Q1' | 'Q2' | 'Q3' | 'Q4')[];
     }[];
     masterAgentNumber?: string;
+    excludeDocumentTypes?: string[]; // this will go away when v2 doc search is deprecated
 };
 // The v3 request is the same as the v2 request
 export interface TaxDocumentApiRequestInputs {
