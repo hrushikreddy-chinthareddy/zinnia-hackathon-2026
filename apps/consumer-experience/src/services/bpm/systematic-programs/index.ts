fedcad7179b6fd229d03cbce28402d6eea85d7ba
Ed Sztukowski <edsztukowski@gmail.com>
2025-10-08 13:09:51 -0600

    dont return a success response on 400

--- apps/consumer-experience/src/services/bpm/systematic-programs/index.ts
+++ apps/consumer-experience/src/services/bpm/systematic-programs/index.ts
@@ -247,22 +247,27 @@ export const submitSystematicProgram = withLogging(
     // This means the call successfully created a case OR that
     // there was some error submitting the transaction, but that
     // the call itself did successfully submit
-    if ('caseId' in response || rawResponse.status === 400) {
-      if (rawResponse.status === 400) {
-        logTrace('systematic premium submission status failure', {
-          status: rawResponse.status,
-          bpmResponse: response,
-        });
-      }
-
+    if ('caseId' in response) {
       return response;
     }
+
+    if (rawResponse.status === 400) {
+      logTrace('systematic premium submission status failure', {
+        status: rawResponse.status,
+        bpmResponse: response,
+      });
+    }
+
+    const apiNotOkDetails = await logApiNotOkDetails({
+      rawResponse,
+      parsedResponse: response,
+    });
 
     throw new Error('Error submitting systematic program', {
       // TODO: verify no PII, but i think this is okay
       cause: {
         requestBody,
-        ...logApiNotOkDetails({ rawResponse, parsedResponse: response }),
+        ...apiNotOkDetails,
       },
     });
   },
