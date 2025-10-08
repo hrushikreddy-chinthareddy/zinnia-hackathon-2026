6ab6eb1949847cb0d37c535ea49ddaf5e0921524
Alex Sagalovskiy <alex.sagalovskiy@zinnia.com>
2025-10-07 21:14:02 -0600

    enable prettier in pr check and trigger prettier error

--- apps/ops/src/setupTests.ts
+++ apps/ops/src/setupTests.ts
@@ -2,7 +2,7 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+		import '@testing-library/jest-dom';
 
 // for ResizeObserver used in PopoverOnTruncate
 import failOnConsole from 'jest-fail-on-console';
