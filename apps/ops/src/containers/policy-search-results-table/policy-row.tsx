b48263c6c2bafb3455e5dc2fcc23cc3a9e8d2232
Elizabeth Flournoy <elizabeth.flournoy@zinnia.com>
2025-10-22 13:56:56 -0600

    feat: update styles for info icon to meet WCAG

--- apps/ops/src/containers/policy-search-results-table/policy-row.tsx
+++ apps/ops/src/containers/policy-search-results-table/policy-row.tsx
@@ -188,7 +188,7 @@ export const PolicyRow: FC<PolicyRowProps> = ({ item }) => {
                 {hasCases ? (
                     <Link
                         aria-label={`View Cases for policy ${item.policyNumber}`}
-                        className={styles.infoColor}
+                        className={styles.infoCases}
                         href={`/cases${convertToQueryString({
                             policyNumber: item.policyNumber || '',
                         })}`}
@@ -196,7 +196,7 @@ export const PolicyRow: FC<PolicyRowProps> = ({ item }) => {
                     >
                         <Icon
                             type={IconType.CIRCLE_INFO}
-                            className={styles.infoColor}
+                            className={styles.infoIcon}
                         />
                         {caseData && 'total' in caseData && caseData?.total}
                     </Link>
