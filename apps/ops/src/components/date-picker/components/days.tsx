0fcbd4d059e00f6d316bb9547febac23cfd36516
Alex Sagalovskiy <alex.sagalovskiy@zinnia.com>
2025-10-23 08:58:24 -0600

    removed unnecessary styles

--- apps/ops/src/components/date-picker/components/days.tsx
+++ apps/ops/src/components/date-picker/components/days.tsx
@@ -203,10 +203,7 @@ export const Days = ({
                             className={`${containerClasses} ${getSelectedClasses(
                                 previousMonth,
                                 dayItem
-                            )} ${getDisabledClasses(
-                                previousMonth,
-                                dayItem
-                            )} flex items-center justify-center`}
+                            )} ${getDisabledClasses(previousMonth, dayItem)}`}
                             onClick={createClickHandler(() =>
                                 handleDateSelect(year, previousMonth, dayItem)
                             )}
@@ -249,10 +246,7 @@ export const Days = ({
                             className={`${containerClasses} ${getSelectedClasses(
                                 month,
                                 dayItem
-                            )} ${getDisabledClasses(
-                                month,
-                                dayItem
-                            )} flex items-center justify-center`}
+                            )} ${getDisabledClasses(month, dayItem)}`}
                             onClick={createClickHandler(() =>
                                 handleDateSelect(year, month, dayItem)
                             )}
@@ -298,10 +292,7 @@ export const Days = ({
                             className={`${containerClasses} ${getSelectedClasses(
                                 nextMonth,
                                 dayItem
-                            )} ${getDisabledClasses(
-                                nextMonth,
-                                dayItem
-                            )} flex items-center justify-center`}
+                            )} ${getDisabledClasses(nextMonth, dayItem)}`}
                             onClick={createClickHandler(() =>
                                 handleDateSelect(year, nextMonth, dayItem)
                             )}
