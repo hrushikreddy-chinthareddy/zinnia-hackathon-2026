98cd53badf06ad0e05c9087e3432770537bbf6cf
Ed Sztukowski <edsztukowski@gmail.com>
2025-10-31 13:38:05 -0600

    check feature flag variables based on theme

--- apps/consumer-experience/src/utils/optimizely/optimizely.ts
+++ apps/consumer-experience/src/utils/optimizely/optimizely.ts
@@ -2,11 +2,13 @@ import {
   Client,
   createInstance,
   OptimizelyDecideOption,
+  OptimizelyDecision,
 } from '@optimizely/optimizely-sdk';
 
 import { logError } from '@/utils/logging/log-fns';
 
 import { FEATURE_FLAGS } from './flags';
+import { getThemeCookies } from '../theme';
 
 export type FeatureFlags =
   | Record<FEATURE_FLAGS, boolean>
@@ -60,7 +62,24 @@ export class OptimizelyService {
         OptimizelyDecideOption.IGNORE_USER_PROFILE_SERVICE,
       ]);
 
+      // We need to get the theme to compare to the variables that come in with the optimizely feature flags
+      const theme = await getThemeCookies();
+
       return Object.keys(decisionResults).reduce((acc, curr) => {
+        const currentObject = decisionResults?.[curr];
+
+        // First we check to see if there are variables in this flag
+        const themeIsInVariables = checkForThemeVariables(currentObject, theme);
+
+        // If there are variables that match our current theme, we make sure we use them.
+        if (themeIsInVariables) {
+          return {
+            ...acc,
+            [curr]: theme && currentObject?.variables?.[theme],
+          };
+        }
+
+        // Otherwise we just return the enabled value
         return {
           ...acc,
           [curr]: decisionResults?.[curr]?.enabled,
@@ -84,3 +103,17 @@ export class OptimizelyService {
 export const optimizelyService = new OptimizelyService(
   process.env.OPTIMIZELY_SDK_KEY ?? ''
 );
+
+const checkForThemeVariables = (
+  optimizelyFlag: OptimizelyDecision | undefined,
+  theme: string | undefined
+) => {
+  const hasVariables =
+    optimizelyFlag && Object.keys(optimizelyFlag?.variables).length > 0;
+
+  const themeIsInVariables =
+    hasVariables &&
+    Object.keys(optimizelyFlag?.variables).some(key => key === theme);
+
+  return !!themeIsInVariables;
+};
