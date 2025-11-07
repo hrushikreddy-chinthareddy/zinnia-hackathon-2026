cfaac742d0642209d9f837baeccd8cfc257e9e50
Elizabeth Flournoy <elizabeth.flournoy@zinnia.com>
2025-11-07 10:10:10 -0700

    Hide enddated owners in policy & contracts table

--- apps/ops/src/containers/policy-search-results-table/policy-row.tsx
+++ apps/ops/src/containers/policy-search-results-table/policy-row.tsx
@@ -28,6 +28,7 @@ import { PolicyBadgeStatus } from '@deps/components/global-values/policy-info/po
 import { PiiWrapper } from '@deps/components/pii/PiiWrapper';
 import { useOptimizely } from '@deps/contexts/OptimizelyContext';
 import { usePermissionsContext } from '@deps/contexts/PermissionsContext';
+import { isEndDated } from '@deps/helpers/date.helpers';
 import { PolicyDetails } from '@deps/helpers/policy-sor/PolicyDetails';
 import { convertToQueryString } from '@deps/helpers/routing.helpers';
 import { formatSSN } from '@deps/helpers/string.helpers';
@@ -94,19 +95,24 @@ export const PolicyRow: FC<PolicyRowProps> = ({ item }) => {
     const policyOwner = useMemo(() => {
         // get policy owner id
         const policyOwnerId = policyData?.partyRoles?.find(
-            (pr) => pr.partyRole === PartyRole.OWNER
+            (pr) => pr.partyRole === PartyRole.OWNER && !isEndDated(pr.endDate)
         )?.partyId;
 
         //use ID to find the policy owner data
         const policyOwner = policyData?.parties?.find(
             (party) => party.partyId === policyOwnerId
         );
-        if (policyOwner?.fullName) {
-            return toSentenceCase(policyOwner.fullName);
+
+        if (policyOwner) {
+            if (policyOwner?.fullName) {
+                return toSentenceCase(policyOwner.fullName);
+            } else {
+                return `${toSentenceCase(item.firstName)} ${toSentenceCase(
+                    item.lastName
+                )}`;
+            }
         } else {
-            return `${toSentenceCase(item.firstName)} ${toSentenceCase(
-                item.lastName
-            )}`;
+            return undefined;
         }
     }, [item.firstName, item.lastName, policyData]);
 
@@ -171,12 +177,18 @@ export const PolicyRow: FC<PolicyRowProps> = ({ item }) => {
                 />
             </TableCell>
             <TableCell>
-                <PiiWrapper className={styles.ownerCell}>
-                    {policyOwner}
-                </PiiWrapper>
-                <PiiWrapper className="typography-content-body-sm text-[--color-base-text-text-secondary] block">
-                    {formatSSN(item.ssn)}
-                </PiiWrapper>
+                {policyOwner ? (
+                    <>
+                        <PiiWrapper className={styles.ownerCell}>
+                            {policyOwner}
+                        </PiiWrapper>
+                        <PiiWrapper className="typography-content-body-sm text-[--color-base-text-text-secondary] block">
+                            {formatSSN(item.ssn)}
+                        </PiiWrapper>
+                    </>
+                ) : (
+                    <span className={styles.noCases}>--</span>
+                )}
             </TableCell>
             {/* Case Table Cell */}
             <TableCell
