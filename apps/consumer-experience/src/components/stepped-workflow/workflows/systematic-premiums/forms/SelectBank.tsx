ea39f958545dec73b4c9610866eaa2e96719e96f
Ed Sztukowski <edsztukowski@gmail.com>
2025-11-10 14:18:02 -0700

    fix add bank ts

--- apps/consumer-experience/src/components/stepped-workflow/workflows/systematic-premiums/forms/SelectBank.tsx
+++ apps/consumer-experience/src/components/stepped-workflow/workflows/systematic-premiums/forms/SelectBank.tsx
@@ -10,7 +10,6 @@ import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 
 import { CarrierPhoneNumber } from '@/components/carrier-phone-number/CarrierPhoneNumber';
 import { ConfirmDialog } from '@/components/confirm-dialog/ConfirmDialog';
-import { FeatureFlagComponent } from '@/components/FeatureFlagComponent';
 import { NoDataAvailable } from '@/components/no-data-available/NoDataAvailable';
 import noDataStyles from '@/components/no-data-available/NoDataAvailable.module.css';
 import { PaymentusAddPaymentMethod } from '@/components/paymentus/PaymentusAddPaymentMethod';
@@ -21,7 +20,6 @@ import { SkeletonLoader } from '@/components/skeleton-loader/SkeletonLoader';
 import { useSteppedWorkflowContext } from '@/components/stepped-workflow/SteppedWorkflowContext';
 import { usePolicyUrlInputs } from '@/hooks/use-policy-url-inputs';
 import { PaymentMethod } from '@/types/payment';
-import { FEATURE_FLAGS } from '@/utils/optimizely/flags';
 
 import { SystematicPremiumsAction } from '../provider/types';
 import { useSystematicPremiums } from '../provider/useSystematicPremiums';
@@ -49,28 +47,23 @@ const AddBankInTransaction = ({
           />
         </div>
       ) : (
-        <FeatureFlagComponent
-          flagKey={FEATURE_FLAGS.ADD_EDIT_DELETE_BANK_ACCOUNT}
-          enabledComponent={
-            <div className={`my-lg mb-none ${styles.disclaimer}`}>
-              <p className="typography-content-body-sm">
-                Want to pay with another bank account? Go to{' '}
-                <ConfirmDialog
-                  confirmCallback={() => router.push(addBankUrl)}
-                  inline
-                  linkText="banking details"
-                  linkClassName={styles.linkClassname}
-                  confirmDescription="Navigate to the profile page and open the add bank sidesheet"
-                  message="If you leave now, your payment won't be submitted and you will have to start over."
-                  cancelDescription="Stay on the premium payment page"
-                  title="Leave payment?"
-                />{' '}
-                to add. If you're not seeing the account you want to pay with,
-                give us a call at <CarrierPhoneNumber />.
-              </p>
-            </div>
-          }
-        />
+        <div className={`my-lg mb-none ${styles.disclaimer}`}>
+          <p className="typography-content-body-sm">
+            Want to pay with another bank account? Go to{' '}
+            <ConfirmDialog
+              confirmCallback={() => router.push(addBankUrl)}
+              inline
+              linkText="banking details"
+              linkClassName={styles.linkClassname}
+              confirmDescription="Navigate to the profile page and open the add bank sidesheet"
+              message="If you leave now, your payment won't be submitted and you will have to start over."
+              cancelDescription="Stay on the premium payment page"
+              title="Leave payment?"
+            />{' '}
+            to add. If you're not seeing the account you want to pay with, give
+            us a call at <CarrierPhoneNumber />.
+          </p>
+        </div>
       )}
     </div>
   );
