2e4771cf069b62b363194f2276ef42fc9d40be95
shubhamzin1 <190578939+shubhamzin1@users.noreply.github.com>
2025-11-10 17:06:11 +0530

    Display open cases against transaction and send correlationId of selected Case (#3126)

--- apps/ops/src/containers/people-data-cards/email-card/side-sheet/side-sheet-email.tsx
+++ apps/ops/src/containers/people-data-cards/email-card/side-sheet/side-sheet-email.tsx
@@ -8,6 +8,7 @@ import {
 } from '@zinnia/api-types/types/sor';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
+import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
 import { Dispatch, SetStateAction, useState } from 'react';
 import { v4 as uuidV4 } from 'uuid';
@@ -90,7 +91,13 @@ const SideSheetEmail = ({
     const { t } = useTranslation(TranslationFiles.COMMON, {
         keyPrefix: 'people.sideSheet.email',
     });
+
     const { t: defaultT } = useTranslation();
+    const router = useRouter();
+    const correlationIdFromRoute =
+        typeof router?.query?.correlationId === 'string'
+            ? router?.query?.correlationId
+            : undefined;
     const { sessionId, partyId: userId } = usePermissionsContext();
 
     const INITIAL_EMAIL: Email = {
@@ -334,6 +341,9 @@ const SideSheetEmail = ({
                 setCaseDocumentOptions={setCaseDocumentOptions}
                 setCurrentErrors={setCurrentErrors}
                 setViewState={setViewState}
+                processSubType={[Processes.EmailChange]}
+                correlationId={correlationIdFromRoute}
+                body={body}
             />
 
             <div className="flex flex-col gap-8">
