11089a95daf1b171aee2c62a5347bf36d0e3dfce
Ed Sztukowski <edsztukowski@gmail.com>
2025-11-10 12:41:10 -0700

    rm annuity_mode

--- apps/consumer-experience/src/utils/optimizely/flags.ts
+++ apps/consumer-experience/src/utils/optimizely/flags.ts
@@ -1,7 +1,6 @@
 export enum FEATURE_FLAGS {
   ADD_EDIT_DELETE_BANK_ACCOUNT = 'add_edit_delete_bank_account',
   AMP_TRANSACTION_SUMMARY = 'amp_transaction_summary',
-  ANNUITY_MODE = 'annuity_mode',
   DOCUMENTS_V3 = 'documents-v3-update',
   COMMUNICATION_PREFERENCES = 'communication_preferences',
   TRANSACTION_NOTIFICATIONS = 'transaction_notifications',
