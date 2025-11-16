7e16387a6dd50c6d2c87b529af30b685a875607b
gunjan <gunjan2@zinnia.com>
2025-11-16 22:21:05 +0530

    check eligibility for transfer for premium autopay direct link

--- apps/ops/src/pages/policies/[planCode]/[id]/policy/premiums/add-premium-autopay.tsx
+++ apps/ops/src/pages/policies/[planCode]/[id]/policy/premiums/add-premium-autopay.tsx
@@ -1,9 +1,24 @@
-import { ArrangementType, Policy, Reason } from '@zinnia/api-types/types/sor';
+import { useQuery } from '@tanstack/react-query';
+import { TransactionPermission } from '@xd/utils/dist';
+import {
+    ArrangementType,
+    Policy,
+    Reason,
+    Status,
+} from '@zinnia/api-types/types/sor';
+import { useRouter } from 'next/router';
+import { useEffect, useMemo, useState } from 'react';
 
+import PageLoader, {
+    PageLoaderVariant,
+} from '@deps/components/page-loader/page-loader';
 import { PageHead } from '@deps/components/page-title';
 import { ParentPage } from '@deps/components/transaction-navigation-buttons/transaction-navigation-buttons';
 import AutopayContainer from '@deps/containers/financial-transactions/autopay/autopay-container';
 import { AutopayProvider } from '@deps/contexts/transactions/AutopayContext';
+import { useTransactionPermissionCheck } from '@deps/hooks/useTransactionPermissionCheck';
+import { TransactionResponseStatus } from '@deps/queries/api/bpm';
+import { checkSystematicProgramEligibilityQuery } from '@deps/queries/tanstack/checkEligibilityQueries/checkEligibilityQueries';
 import { getServerSidePropsPolicyDetailsPage } from '@deps/utils/page';
 import { withPageAuthAndLogging } from '@deps/utils/server-logging';
 
@@ -12,6 +27,80 @@ interface UpdateAutopayProps {
 }
 
 const AddPremiumAutopay = ({ policy }: UpdateAutopayProps) => {
+    const [isLoading, setIsLoading] = useState(true);
+    const router = useRouter();
+
+    const { systematicPrograms, policyNumber } = policy;
+    const { planCode } = policy.product ?? {};
+
+    const upcomingPayment = useMemo(
+        () =>
+            systematicPrograms?.find(
+                (sp) =>
+                    sp.reason === Reason.PREMIUM && sp.status === Status.ACTIVE
+            ),
+        [systematicPrograms]
+    );
+
+    const { data: setUpAutopayProgramsEligibility, isFetched } = useQuery({
+        queryKey: [
+            'checkSetUpAutopayProgramsEligibility',
+            planCode,
+            policyNumber,
+            upcomingPayment?.arrangementId,
+        ],
+        queryFn: () =>
+            checkSystematicProgramEligibilityQuery(
+                planCode as string,
+                policyNumber as string,
+                upcomingPayment?.arrangementId ?? '',
+
+                {
+                    systematicProgram: {
+                        arrangementType: ArrangementType.PAYMENT,
+                    },
+                }
+            ),
+        placeholderData: (previousData) => previousData,
+        select: (data) => {
+            return {
+                ...data,
+                isEligibleSetUpAutopay:
+                    data?.status === TransactionResponseStatus.Success,
+            };
+        },
+    });
+
+    const { isPermissioned: isUserPermissionedToAutopay } =
+        useTransactionPermissionCheck(
+            TransactionPermission.WritePolicy,
+            policyNumber,
+            planCode
+        );
+
+    useEffect(() => {
+        if (!isFetched) return;
+
+        const isEligible =
+            setUpAutopayProgramsEligibility?.isEligibleSetUpAutopay || false;
+        const isPermissioned = isUserPermissionedToAutopay || false;
+
+        if (!isEligible || !isPermissioned) {
+            router.replace('/403');
+        } else {
+            setIsLoading(false);
+        }
+    }, [
+        setUpAutopayProgramsEligibility,
+        isFetched,
+        router,
+        isUserPermissionedToAutopay,
+    ]);
+
+    if (isLoading) {
+        return <PageLoader variant={PageLoaderVariant.Center} />;
+    }
+
     return (
         <AutopayProvider>
             <PageHead titleKey="addPremiumAutopay" />
--- apps/ops/src/pages/policies/[planCode]/[id]/policy/premiums/new-premium.tsx
+++ apps/ops/src/pages/policies/[planCode]/[id]/policy/premiums/new-premium.tsx
@@ -1,8 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
+import { TransactionPermission } from '@xd/utils/dist';
 import { Policy } from '@zinnia/api-types/types/sor';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
+import PageLoader, {
+    PageLoaderVariant,
+} from '@deps/components/page-loader/page-loader';
 import { PageHead } from '@deps/components/page-title';
 import NewPremiumContainer from '@deps/containers/financial-transactions/premium/new-premium/new-premium-container';
 import { NewPremiumProvider } from '@deps/contexts/transactions/NewPremiumContext';
+import { useTransactionPermissionCheck } from '@deps/hooks/useTransactionPermissionCheck';
+import { TransactionResponseStatus } from '@deps/queries/api/bpm';
+import { checkOneTimePremiumEligibilityQuery } from '@deps/queries/tanstack/checkEligibilityQueries/checkEligibilityQueries';
 import { getServerSidePropsPolicyDetailsPage } from '@deps/utils/page';
 import { withPageAuthAndLogging } from '@deps/utils/server-logging';
 
@@ -11,6 +21,54 @@ interface NewPremiumProps {
 }
 
 const NewPremium = ({ policy }: NewPremiumProps) => {
+    const [isLoading, setIsLoading] = useState(true);
+    const router = useRouter();
+
+    const { policyNumber } = policy;
+    const { planCode } = policy.product ?? {};
+
+    const { data: oneTimePremiumEligibility, isFetched } = useQuery({
+        queryKey: ['checkOneTimePremiumEligibility', planCode, policyNumber],
+        queryFn: () =>
+            checkOneTimePremiumEligibilityQuery(
+                planCode as string,
+                policyNumber as string
+            ),
+        placeholderData: (previousData) => previousData,
+        select: (data) => {
+            return {
+                ...data,
+                isEligibleOneTimePremium:
+                    data?.status === TransactionResponseStatus.Success,
+            };
+        },
+    });
+
+    const { isPermissioned: isUserPermissionedToAutopay } =
+        useTransactionPermissionCheck(
+            TransactionPermission.WritePolicy,
+            policyNumber,
+            planCode
+        );
+
+    useEffect(() => {
+        if (!isFetched) return;
+
+        const isEligible =
+            oneTimePremiumEligibility?.isEligibleOneTimePremium || false;
+        const isPermissioned = isUserPermissionedToAutopay || false;
+
+        if (!isEligible || !isPermissioned) {
+            router.replace('/403');
+        } else {
+            setIsLoading(false);
+        }
+    }, [oneTimePremiumEligibility, isUserPermissionedToAutopay, router]);
+
+    if (isLoading) {
+        return <PageLoader variant={PageLoaderVariant.Center} />;
+    }
+
     return (
         <NewPremiumProvider>
             <PageHead titleKey="newPremium" />
