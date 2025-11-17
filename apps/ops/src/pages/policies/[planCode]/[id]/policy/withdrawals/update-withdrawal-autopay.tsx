0547f41b38ba68cdc8dfb5919c24e0771f2a9c01
gunjan <gunjan2@zinnia.com>
2025-11-17 17:19:23 +0530

    checks update withdrawal eligibility check for autopay link

--- apps/ops/src/pages/policies/[planCode]/[id]/policy/withdrawals/update-withdrawal-autopay.tsx
+++ apps/ops/src/pages/policies/[planCode]/[id]/policy/withdrawals/update-withdrawal-autopay.tsx
@@ -1,10 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
+import { TransactionPermission } from '@xd/utils/dist';
 import { ArrangementType, Policy, Reason } from '@zinnia/api-types/types/sor';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
+import {
+    PageLoader,
+    PageLoaderVariant,
+} from '@deps/components/page-loader/page-loader';
 import { PageHead } from '@deps/components/page-title';
 import { ParentPage } from '@deps/components/transaction-navigation-buttons/transaction-navigation-buttons';
 import AutopayContainer from '@deps/containers/financial-transactions/autopay/autopay-container';
 import { AutopayProvider } from '@deps/contexts/transactions/AutopayContext';
+import { useTransactionPermissionCheck } from '@deps/hooks/useTransactionPermissionCheck';
+import { TransactionResponseStatus } from '@deps/queries/api/bpm';
+import {
+    checkFullSurrenderWithdrawal,
+    checkPartialWithdrawalOneTimeEligibilityQuery,
+} from '@deps/queries/tanstack/checkEligibilityQueries/checkEligibilityQueries';
 import { getServerSidePropsPolicyDetailsPage } from '@deps/utils/page';
 import { withPageAuthAndLogging } from '@deps/utils/server-logging';
 
@@ -22,6 +35,95 @@ const UpdateWithdrawalAutoPay = ({ policy }: WithdrawalAutopayProps) => {
             : ArrangementType.WITHDRAWAL;
     const reason =
         type === 'RMD' ? Reason.REQUIREDMINIMUMDISTRIBUTION : Reason.WITHDRAWAL;
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    const { policyNumber } = policy;
+    const { planCode } = policy.product ?? {};
+
+    const {
+        data: partialWithdrawalOneTimeEligibility,
+        isFetched: isPartialWithdrawalOneTimeEligibilityFetched,
+    } = useQuery({
+        queryKey: [
+            'checkPartialWithdrawalOneTimeEligibility',
+            planCode,
+            policyNumber,
+        ],
+        queryFn: () =>
+            checkPartialWithdrawalOneTimeEligibilityQuery(
+                planCode as string,
+                policyNumber as string
+            ),
+        placeholderData: (previousData) => previousData,
+        select: (data) => {
+            return {
+                ...data,
+                isEligiblePartialWithdrawalOneTime:
+                    data?.status === TransactionResponseStatus.Success,
+            };
+        },
+    });
+
+    const { isPermissioned: isUserPermissionedToWithdraw } =
+        useTransactionPermissionCheck(
+            TransactionPermission.WritePolicy,
+            policyNumber,
+            planCode
+        );
+
+    const {
+        data: fullSurrenderEligibility,
+        isFetched: isFullSurrenderEligibilityFetched,
+    } = useQuery({
+        queryKey: ['checkFullSurrenderEligibility', planCode, policyNumber],
+        queryFn: () =>
+            checkFullSurrenderWithdrawal(
+                planCode as string,
+                policyNumber as string
+            ),
+        placeholderData: (previousData) => previousData,
+        select: (data) => {
+            return {
+                ...data,
+                isEligibleFullSurrender:
+                    data?.status === TransactionResponseStatus.Success,
+            };
+        },
+    });
+
+    useEffect(() => {
+        if (
+            !isPartialWithdrawalOneTimeEligibilityFetched ||
+            !isFullSurrenderEligibilityFetched
+        )
+            return;
+
+        const isEligible =
+            partialWithdrawalOneTimeEligibility?.isEligiblePartialWithdrawalOneTime ||
+            false;
+        const isEligibleFullSurrender =
+            fullSurrenderEligibility?.isEligibleFullSurrender || false;
+
+        const isPermissioned = isUserPermissionedToWithdraw || false;
+
+        if (!isEligible || !isEligibleFullSurrender || !isPermissioned) {
+            router.replace('/403');
+        } else {
+            setIsLoading(false);
+        }
+    }, [
+        partialWithdrawalOneTimeEligibility,
+        isUserPermissionedToWithdraw,
+        router,
+        fullSurrenderEligibility,
+        isPartialWithdrawalOneTimeEligibilityFetched,
+        isFullSurrenderEligibilityFetched,
+    ]);
+
+    if (isLoading) {
+        return <PageLoader variant={PageLoaderVariant.Center} />;
+    }
 
     return (
         <AutopayProvider>
