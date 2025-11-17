92864fbf70d4268b8a6581672e9f69af49821a28
gunjan <gunjan2@zinnia.com>
2025-11-17 13:56:43 +0530

    Eligibility checks for new withdrawals and cancel freelook

--- apps/ops/src/pages/policies/[planCode]/[id]/policy/freelook/cancel-freelook.tsx
+++ apps/ops/src/pages/policies/[planCode]/[id]/policy/freelook/cancel-freelook.tsx
@@ -1,12 +1,116 @@
+import { useQuery } from '@tanstack/react-query';
+import { TransactionPermission } from '@xd/utils/dist';
 import { Policy } from '@zinnia/api-types/types/sor';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
+import PageLoader, {
+    PageLoaderVariant,
+} from '@deps/components/page-loader/page-loader';
 import { PageHead } from '@deps/components/page-title';
 import FreeLookCancelContainer from '@deps/containers/financial-transactions/free-look-cancel/free-look-cancel-container';
 import { WithdrawalProvider } from '@deps/contexts/transactions/WithdrawalContext';
+import { useTransactionPermissionCheck } from '@deps/hooks/useTransactionPermissionCheck';
+import { TransactionResponseStatus } from '@deps/queries/api/bpm';
+import {
+    checkPartialWithdrawalOneTimeEligibilityQuery,
+    checkFullSurrenderWithdrawal,
+} from '@deps/queries/tanstack/checkEligibilityQueries/checkEligibilityQueries';
 import { getServerSidePropsPolicyDetailsPage } from '@deps/utils/page';
 import { withPageAuthAndLogging } from '@deps/utils/server-logging';
 
 const CancelFreeLook = ({ policy }: { policy: Policy }) => {
+    const router = useRouter();
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    const { policyNumber } = policy;
+    const { planCode } = policy.product ?? {};
+
+    const {
+        data: partialWithdrawalOneTimeEligibility,
+        isFetched: isPartialWithdrawalOneTimeEligibilityFetched,
+    } = useQuery({
+        queryKey: [
+            'checkPartialWithdrawalOneTimeEligibility',
+            planCode,
+            policyNumber,
+        ],
+        queryFn: () =>
+            checkPartialWithdrawalOneTimeEligibilityQuery(
+                planCode as string,
+                policyNumber as string
+            ),
+        placeholderData: (previousData) => previousData,
+        select: (data) => {
+            return {
+                ...data,
+                isEligiblePartialWithdrawalOneTime:
+                    data?.status === TransactionResponseStatus.Success,
+            };
+        },
+    });
+
+    const { isPermissioned: isUserPermissionedToWithdraw } =
+        useTransactionPermissionCheck(
+            TransactionPermission.WritePolicy,
+            policyNumber,
+            planCode
+        );
+
+    const {
+        data: fullSurrenderEligibility,
+        isFetched: isFullSurrenderEligibilityFetched,
+    } = useQuery({
+        queryKey: ['checkFullSurrenderEligibility', planCode, policyNumber],
+        queryFn: () =>
+            checkFullSurrenderWithdrawal(
+                planCode as string,
+                policyNumber as string
+            ),
+        placeholderData: (previousData) => previousData,
+        select: (data) => {
+            return {
+                ...data,
+                isEligibleFullSurrender:
+                    data?.status === TransactionResponseStatus.Success,
+            };
+        },
+    });
+
+    useEffect(() => {
+        if (
+            !isPartialWithdrawalOneTimeEligibilityFetched ||
+            !isFullSurrenderEligibilityFetched
+        )
+            return;
+
+        const isEligible =
+            partialWithdrawalOneTimeEligibility?.isEligiblePartialWithdrawalOneTime ||
+            false;
+        const isEligibleFullSurrender =
+            fullSurrenderEligibility?.isEligibleFullSurrender || false;
+
+        const isPermissioned = isUserPermissionedToWithdraw || false;
+
+        if (!isEligible || !isEligibleFullSurrender || !isPermissioned) {
+            router.replace('/403');
+        } else {
+            setIsLoading(false);
+        }
+    }, [
+        partialWithdrawalOneTimeEligibility,
+        isUserPermissionedToWithdraw,
+        router,
+        fullSurrenderEligibility,
+        isPartialWithdrawalOneTimeEligibilityFetched,
+        isFullSurrenderEligibilityFetched,
+    ]);
+
+    if (isLoading) {
+        return <PageLoader variant={PageLoaderVariant.Center} />;
+    }
+
     return (
         <WithdrawalProvider>
             <PageHead titleKey="cancelFreeLook" />
