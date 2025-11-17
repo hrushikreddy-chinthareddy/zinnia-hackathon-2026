25c3244ecce64383c52753d1a6340bb2d04040ea
Christian Bedón <christian.bedonvillalobos@zinnia.com>
2025-11-17 14:45:21 -0500

    fix: missing task type in groupByName

--- apps/ops/src/components/dashboard/utils.ts
+++ apps/ops/src/components/dashboard/utils.ts
@@ -2,6 +2,7 @@ import {
     CaseCountGroupByEnum,
     CaseCountInputFilter,
     CaseCountOutputLevel1,
+    CompletedTaskTimeGroupByEnum,
     ExceptionCountGroupByEnum,
     TaskCountGroupByEnum,
 } from '@zinnia/api-types/types/analytics';
@@ -283,7 +284,10 @@ export const generateCaseLink = ({
 };
 
 export const friendlyGroupByName: Record<
-    CaseCountGroupByEnum | ExceptionCountGroupByEnum | TaskCountGroupByEnum,
+    | CaseCountGroupByEnum
+    | ExceptionCountGroupByEnum
+    | TaskCountGroupByEnum
+    | CompletedTaskTimeGroupByEnum,
     string
 > = {
     [CaseCountGroupByEnum.APPLICATION_TYPE]: 'Application type',
@@ -307,7 +311,7 @@ export const friendlyGroupByName: Record<
     [TaskCountGroupByEnum.TASK_STATUS]: 'Task status',
     [TaskCountGroupByEnum.TASK_CREATED_DAY]: 'Task created date',
     [TaskCountGroupByEnum.TASK_UPDATED_DAY]: 'Task updated date',
-    [TaskCountGroupByEnum.TASK_TYPE]: 'Task type',
+    [CompletedTaskTimeGroupByEnum.TASK_TYPE]: 'Task type',
 };
 
 /**
