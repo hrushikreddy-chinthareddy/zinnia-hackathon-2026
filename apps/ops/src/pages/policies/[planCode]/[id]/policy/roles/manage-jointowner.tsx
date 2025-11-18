4c26a070ab53330fd2f61ca4799e3aeb2c6e9f55
gunjan <gunjan2@zinnia.com>
2025-11-18 17:47:58 +0530

    added roles chnages eligibility check API

--- apps/ops/src/pages/policies/[planCode]/[id]/policy/roles/manage-jointowner.tsx
+++ apps/ops/src/pages/policies/[planCode]/[id]/policy/roles/manage-jointowner.tsx
@@ -1,9 +1,17 @@
+import { useQuery } from '@tanstack/react-query';
 import { Policy } from '@zinnia/api-types/types/sor';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
+import PageLoader, {
+    PageLoaderVariant,
+} from '@deps/components/page-loader/page-loader';
 import { PageHead } from '@deps/components/page-title';
 import { PolicyRole, RoleLabel } from '@deps/constants/policy';
 import RoleChangeContainer from '@deps/containers/role-change/role-change-container';
 import { RoleChangeProvider } from '@deps/contexts/RoleChangeContext';
+import { TransactionResponseStatus } from '@deps/queries/api/bpm';
+import { checkManagRoleEligibilityQuery } from '@deps/queries/tanstack/checkEligibilityQueries/checkEligibilityQueries';
 import { getServerSidePropsPolicyDetailsPage } from '@deps/utils/page';
 import { withPageAuthAndLogging } from '@deps/utils/server-logging';
 
@@ -12,6 +20,48 @@ interface RoleProps {
 }
 
 const JointOwner = ({ policy }: RoleProps) => {
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(true);
+
+    const { policyNumber } = policy;
+    const { planCode } = policy.product ?? {};
+
+    const {
+        data: manageJointOwnerEligibility,
+        isFetched: isJointOwnerEligibilityFetched,
+    } = useQuery({
+        queryKey: ['checkManageJointOwnerEligibility', planCode, policyNumber],
+        queryFn: () =>
+            checkManagRoleEligibilityQuery(
+                planCode ?? '',
+                policyNumber ?? '',
+                PolicyRole.JOINTOWNER
+            ),
+        placeholderData: (previousData) => previousData,
+        select: (data) => ({
+            ...data,
+            isEligibleManageJOINTOWNER:
+                data?.status === TransactionResponseStatus.Success,
+        }),
+    });
+
+    useEffect(() => {
+        if (!isJointOwnerEligibilityFetched) return;
+
+        const isEligible =
+            manageJointOwnerEligibility?.isEligibleManageJOINTOWNER || false;
+
+        if (!isEligible) {
+            router.replace('/403');
+        } else {
+            setIsLoading(false);
+        }
+    }, [manageJointOwnerEligibility, router, isJointOwnerEligibilityFetched]);
+
+    if (isLoading) {
+        return <PageLoader variant={PageLoaderVariant.Center} />;
+    }
+
     return (
         <RoleChangeProvider>
             <PageHead titleKey="jointOwnerChange" />
--- apps/ops/src/pages/policies/[planCode]/[id]/policy/roles/manage-third-party-designee.tsx
+++ apps/ops/src/pages/policies/[planCode]/[id]/policy/roles/manage-third-party-designee.tsx
@@ -1,9 +1,17 @@
+import { useQuery } from '@tanstack/react-query';
 import { Policy } from '@zinnia/api-types/types/sor';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
+import PageLoader, {
+    PageLoaderVariant,
+} from '@deps/components/page-loader/page-loader';
 import { PageHead } from '@deps/components/page-title';
 import { PolicyRole, RoleLabel } from '@deps/constants/policy';
 import RoleChangeContainer from '@deps/containers/role-change/role-change-container';
 import { RoleChangeProvider } from '@deps/contexts/RoleChangeContext';
+import { TransactionResponseStatus } from '@deps/queries/api/bpm';
+import { checkManagRoleEligibilityQuery } from '@deps/queries/tanstack/checkEligibilityQueries/checkEligibilityQueries';
 import { getServerSidePropsPolicyDetailsPage } from '@deps/utils/page';
 import { withPageAuthAndLogging } from '@deps/utils/server-logging';
 
@@ -12,6 +20,49 @@ interface IThirdPartyProps {
 }
 
 const ThirdPartyChange = ({ policy }: IThirdPartyProps) => {
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(true);
+
+    const { policyNumber } = policy;
+    const { planCode } = policy.product ?? {};
+
+    const {
+        data: manageThirdPartyEligibility,
+        isFetched: isThirdPartyEligibilityFetched,
+    } = useQuery({
+        queryKey: ['checkManageThirdPartyEligibility', planCode, policyNumber],
+        queryFn: () =>
+            checkManagRoleEligibilityQuery(
+                planCode ?? '',
+                policyNumber ?? '',
+                PolicyRole.THIRDPARTYDESIGNEE
+            ),
+        placeholderData: (previousData) => previousData,
+        select: (data) => ({
+            ...data,
+            isEligibleManageTHIRDPARTYDESIGNEE:
+                data?.status === TransactionResponseStatus.Success,
+        }),
+    });
+
+    useEffect(() => {
+        if (!isThirdPartyEligibilityFetched) return;
+
+        const isEligible =
+            manageThirdPartyEligibility?.isEligibleManageTHIRDPARTYDESIGNEE ||
+            false;
+
+        if (!isEligible) {
+            router.replace('/403');
+        } else {
+            setIsLoading(false);
+        }
+    }, [manageThirdPartyEligibility, router, isThirdPartyEligibilityFetched]);
+
+    if (isLoading) {
+        return <PageLoader variant={PageLoaderVariant.Center} />;
+    }
+
     return (
         <RoleChangeProvider>
             <PageHead titleKey="thirdPartyDesigneeChange" />
