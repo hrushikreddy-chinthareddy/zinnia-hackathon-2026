b799538deb716f9180700efe19eb7371d1827fff
Rocco Sangellino <rocco.sangellino@zinnia.com>
2025-11-21 16:07:00 -0700

    chore: disable react-hooks/exhaustive-deps for OTP form files and fix missing dependencies
    Add ESLint override to disable react-hooks/exhaustive-deps rule for 200+ OTP form and component files. Fix missing router dependency in useEffect hooks across oft, renewal, rmd, ssw, and withdrawal case pages.

--- apps/ops/src/pages/create-case/renewal/[id]/index.tsx
+++ apps/ops/src/pages/create-case/renewal/[id]/index.tsx
@@ -151,7 +151,7 @@ export default function RenewalCase({
             router.push('/create-case');
             return;
         }
-    }, [document]);
+    }, [document, router]);
 
     // If the user has manually opened or closed the nav drawer, we want to override the default behavior
     const isNavDrawerOpen = useMemo(() => {
--- apps/ops/src/pages/create-case/withdrawal/[id]/index.tsx
+++ apps/ops/src/pages/create-case/withdrawal/[id]/index.tsx
@@ -256,7 +256,7 @@ export default function WithdrawalCase({
             router.push('/create-case');
             return;
         }
-    }, [document]);
+    }, [document, router]);
 
     // If the user has manually opened or closed the nav drawer, we want to override the default behavior
     const isNavDrawerOpen = useMemo(() => {
