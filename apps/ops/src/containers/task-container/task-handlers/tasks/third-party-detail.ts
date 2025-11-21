613d5a76b6c8247de657538f8b2df92c18da6826
Vidushi Aggarwal <vidushi.aggarwal@zinnia.com>
2025-11-21 22:08:48 +0530

    feat/DPEU-5416--add viewDetails api

--- apps/ops/src/containers/task-container/task-handlers/tasks/third-party-detail.ts
+++ apps/ops/src/containers/task-container/task-handlers/tasks/third-party-detail.ts
@@ -1,24 +1,208 @@
-import { NigoExceptionResponse } from '@deps/containers/nigo-entry-container/components/steps/nigo-details/nigo-details.types';
+import { EntityTypeValue, Roles } from '@deps/constants/policy';
+import { isEndDated } from '@deps/helpers/date.helpers';
+import { toTitleCase } from '@deps/helpers/string.helpers';
+import {
+    AddressType,
+    EmailType,
+    IdentificationType,
+    PartyType,
+    PhoneType,
+} from '@deps/models/policy/sor-policy';
 import { NigoSearch } from '@deps/queries/api/nigo-search';
+import { getPolicyDetailsSsr } from '@deps/queries/api/policies';
+import { LoggingContext } from '@deps/utils/server-logging';
 
-import { TaskHandler, ReviewPayload } from '../types';
+import { TaskHandler, ReviewPayload, Reason } from '../types';
 
-const thirdPartyDetailHandler: TaskHandler<
-    ReviewPayload,
-    NigoExceptionResponse[]
-> = {
-    api: NigoSearch,
+const getPrefix = (prefix: string | null) => {
+    switch (prefix) {
+        case 'MR':
+            return 'Mr.';
+        case 'MRS':
+            return 'Mrs.';
+        case 'MS':
+            return 'Ms.';
+        case 'DR':
+            return 'Dr.';
+        default:
+            return null;
+    }
+};
 
-    getPayload: (task: any) => ({
+const getAddresses = (addresses: any) => {
+    return addresses.length > 0
+        ? addresses.map((address: any) => ({
+              addressType: address?.addressType ?? AddressType.RESIDENCE,
+              addressLine1: address?.addressLine1 ?? null,
+              city: address?.city ?? null,
+              state: address?.state ?? null,
+              zipCode: address?.zipCode ?? null,
+              zipCodeExtension: address?.zipCodeExtension ?? null,
+              country: address?.country ?? 'USA',
+              endDate: address?.endDate ?? null,
+              isPreferred: address?.isPreferred ?? false,
+              startDate: address?.startDate ?? null,
+          }))
+        : [
+              {
+                  addressType: AddressType.RESIDENCE,
+                  addressLine1: '',
+                  city: '',
+                  state: '',
+                  zipCode: '',
+              },
+          ];
+};
+
+const getPhones = (phones: any) => {
+    return phones.length > 0
+        ? [phones[0]].map((phone: any) => ({
+              phoneType: phone.phoneType ?? PhoneType.HOME,
+              dialNumber: phone.dialNumber ?? null,
+              areaCode: phone.areaCode ?? null,
+              countryCode: phone.countryCode ?? 'USA',
+              endDate: phone.endDate ?? null,
+              isPreferred: phone.isPreferred ?? false,
+              startDate: phone.startDate ?? null,
+          }))
+        : [
+              {
+                  phoneType: PhoneType.HOME,
+                  dialNumber: null,
+              },
+          ];
+};
+
+const getEmails = (emails: any) => {
+    return emails.length > 0
+        ? emails.map((email: any) => ({
+              emailAddress: email.emailAddress ?? null,
+              emailType: email.emailType ?? EmailType.PERSONAL,
+              endDate: email.endDate ?? null,
+              isPreferred: email.isPreferred ?? false,
+              startDate: email.startDate ?? null,
+          }))
+        : [
+              {
+                  emailAddress: null,
+                  emailType: EmailType.PERSONAL,
+              },
+          ];
+};
+
+const getIdentifications = (identifications: any = []) => {
+    const ssnIdentification = (identifications ?? []).find(
+        (id: any) => id.identificationType === IdentificationType.SSN
+    );
+    return identifications.length > 0 && ssnIdentification
+        ? [ssnIdentification]
+        : [
+              {
+                  identificationValue: null,
+                  identificationType: IdentificationType.SSN,
+              },
+          ];
+};
+
+const formatParty = (policyResponse: any) => {
+    const partyId = policyResponse.partyRoles.find(
+        (role: any) =>
+            role.partyRole === Roles.THIRDPARTYDESIGNEE &&
+            (!role?.endDate || !isEndDated(role?.endDate))
+    )?.partyId;
+
+    const party = policyResponse.parties.find(
+        (p: any) => p.partyId === partyId
+    );
+
+    return {
+        partyId: party.partyId ?? null,
+        partyType: party.partyType ?? null,
+        prefix: getPrefix(party.prefix),
+        firstName: party.firstName ?? null,
+        middleName: party.middleName ?? null,
+        lastName:
+            party.lastName ||
+            (party.partyType !== PartyType.INDIVIDUAL
+                ? party.fullName
+                : null) ||
+            null,
+        fullName: toTitleCase(
+            [
+                party.prefix,
+                party.firstName,
+                party.middleName,
+                party.lastName,
+                party.suffix,
+            ]
+                .filter(Boolean)
+                .join(' ')
+        ),
+        entityType: party.entityType ?? EntityTypeValue.Other,
+        gender: null,
+        dateOfBirth: null,
+        trustType: party.trustType ?? null,
+        trustDate: party.trustDate ?? null,
+        preferredCommunicationType: party.preferredCommunicationType ?? null,
+        addresses: getAddresses(party.addresses),
+        phones: getPhones(party.phones),
+        emails: getEmails(party.emails),
+        identifications: getIdentifications(party.identifications),
+    };
+};
+
+const thirdPartyDetailHandler: TaskHandler<ReviewPayload, any> = {
+    api: async (payload: any, accessToken: string | undefined) => {
+        const {
+            category,
+            businessProcess,
+            carrier,
+            policyNumber,
+            planCode,
+            logCtx,
+        } = payload;
+        const [nigoSearchResult, policyResult] = await Promise.all([
+            NigoSearch(
+                {
+                    category,
+                    businessProcess,
+                    carrier,
+                },
+                accessToken,
+                (logCtx ?? {}) as LoggingContext
+            ),
+            getPolicyDetailsSsr(
+                policyNumber,
+                planCode,
+                accessToken,
+                (logCtx ?? {}) as LoggingContext,
+                true
+            ),
+        ]);
+        return {
+            nigoSearchResult,
+            policyResult,
+        };
+    },
+
+    getPayload: (task: any, logCtx?: LoggingContext) => ({
         category: ['Party Change', 'TPD Change'],
         businessProcess: task?.process,
         carrier: task?.carrier,
+        policyNumber: task?.data?.policyNumber,
+        planCode: task?.data?.planCode,
+        logCtx,
     }),
 
     transformResponse: (response, metadata, task) => {
         if (!response || response.length === 0) return;
 
-        const reasonList = Array.from(new Set(response.map((item) => item)));
+        const nigoResponse = response?.nigoSearchResult;
+        const policyResponse = response?.policyResult;
+
+        const reasonList: Reason[] = Array.from(
+            new Set(nigoResponse.map((item: any) => item))
+        );
 
         const seen = new Set<string>();
         const declineReasonEnum: string[] = [];
@@ -37,11 +221,11 @@ const thirdPartyDetailHandler: TaskHandler<
 
                 const selectOptions = r.exceptionSubRefs
                     .filter(
-                        (item) =>
+                        (item: any) =>
                             item.carrier === task?.carrier &&
                             item.process === task?.process
                     )
-                    .map((item) => ({
+                    .map((item: any) => ({
                         label: item.subNmIdDetail,
                         value: item.subNmId,
                     }));
@@ -68,6 +252,18 @@ const thirdPartyDetailHandler: TaskHandler<
             },
             'ui:dataPath': ['declineReason'],
         };
+
+        if (task) {
+            Object.assign(task, {
+                data: {
+                    ...task.data,
+                    partyId: null,
+                    effectiveDate: null,
+                    requestType: null,
+                    party: formatParty(policyResponse),
+                },
+            });
+        }
     },
 };
 
