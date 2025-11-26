5ad3e4aecd3bb7fc4b6a4d763048a33305300029
James Ordinola <james.ordinolabarrantes@zinnia.com>
2025-11-26 17:40:31 -0500

    Create SearchBarContext to abstract the responsibility of handling the display of the error message on the SearchBarComponent

---
+++ apps/ops/src/contexts/SearchBarContext.tsx
@@ -0,0 +1,32 @@
+import { createContext, useMemo, useState } from 'react';
+
+interface SearchBarProps {
+    showFieldErrorMessage: boolean;
+    setShowFieldErrorMessage: (v: boolean) => void;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+const noop = () => {};
+
+export const SearchBarContext = createContext<SearchBarProps>({
+    setShowFieldErrorMessage: noop,
+    showFieldErrorMessage: false,
+});
+
+export const SearchBarProvider = ({ children }: any) => {
+    const [showFieldErrorMessage, setShowFieldErrorMessage] = useState(false);
+
+    const memoizedValues = useMemo(
+        () => ({
+            showFieldErrorMessage,
+            setShowFieldErrorMessage,
+        }),
+        [showFieldErrorMessage, setShowFieldErrorMessage]
+    );
+
+    return (
+        <SearchBarContext.Provider value={memoizedValues}>
+            {children}
+        </SearchBarContext.Provider>
+    );
+};
