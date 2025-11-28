ddbc6cedb9a84324ded71fdbe40b7259b734c86d
RahulNarayanasamy-Zinnia <Rahul.Narayanasamy@zinnia.com>
2025-11-28 19:52:59 +0530

    feat(DEPU-7412): Added name change eligibility endpoint integration (#3421)

--- apps/ops/src/containers/page-header/interior-people-page-header.tsx
+++ apps/ops/src/containers/page-header/interior-people-page-header.tsx
@@ -1,5 +1,6 @@
+import { useQuery } from '@tanstack/react-query';
 import { TFunction, useTranslation } from 'next-i18next';
-import { useMemo } from 'react';
+import { useContext, useMemo } from 'react';
 
 import NavElement, {
     NavElementSize,
@@ -9,6 +10,7 @@ import { PageHeader } from '@deps/components/page-header/page-header';
 import PartyTag from '@deps/components/party/party-tag';
 import { PiiWrapper } from '@deps/components/pii/PiiWrapper';
 import { useOptimizely } from '@deps/contexts/OptimizelyContext';
+import { PolicyData } from '@deps/contexts/PolicyDataContext';
 import { calculateAgeNumber } from '@deps/helpers/age.helpers';
 import {
     getHeaderText,
@@ -16,6 +18,8 @@ import {
 } from '@deps/helpers/party-info-helpers';
 import { orderObjectsByString } from '@deps/helpers/sort.helpers';
 import { formatDate } from '@deps/helpers/string.helpers';
+import { TransactionResponseStatus } from '@deps/queries/api/bpm';
+import { checkExistingNameChangeEligibilityQuery } from '@deps/queries/tanstack/checkEligibilityQueries/checkEligibilityQueries';
 import { ReactComponent as EditIcon } from '@deps/styles/elements/icons/icons_outlined/edit-alt.svg';
 import { FEATURE_FLAGS } from '@deps/utils/optimizely/flags';
 import {
@@ -62,6 +66,34 @@ const InteriorPeoplePageHeaderContainer = ({
         selectedPartyRoles?.includes(
             PartyRole.PRIMARYSERVICINGAGENT.toLowerCase()
         );
+
+    const { policy } = useContext(PolicyData);
+
+    const { policyNumber, product } = policy ?? {};
+    const { planCode } = product ?? {};
+
+    const { data: existingNameChangeEligibility } = useQuery({
+        queryKey: [
+            'existingNameChangeEligibility',
+            planCode,
+            policyNumber,
+            selectedPolicyParty?.partyId,
+        ],
+        queryFn: () =>
+            checkExistingNameChangeEligibilityQuery(
+                planCode as string,
+                policyNumber as string,
+                selectedPolicyParty?.partyId as string
+            ),
+        placeholderData: (previousData) => previousData,
+        select: (data) => {
+            return {
+                ...data,
+                isEligibleExistingNameChange:
+                    data?.status === TransactionResponseStatus.Success,
+            };
+        },
+    });
 
     // for header text siblings group one
     // pronouns and edit button
@@ -168,7 +200,9 @@ const InteriorPeoplePageHeaderContainer = ({
         <NameCard
             t={t}
             selectedPolicyParty={selectedPolicyParty}
-            editable={editable}
+            editable={
+                existingNameChangeEligibility?.isEligibleExistingNameChange
+            }
             isUserPermissionedToEditCards={isUserPermissionedToEditCards}
         >
             {getHeaderText(selectedPolicyParty)}
