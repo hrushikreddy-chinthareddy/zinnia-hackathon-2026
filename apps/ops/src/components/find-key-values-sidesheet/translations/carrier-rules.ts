3d4c7b30d82f38cddfb1aac5b30e824e7016d6d4
Alex Sagalovskiy <alex.sagalovskiy@zinnia.com>
2025-12-02 08:53:05 -0700

    added missing file

---
+++ apps/ops/src/components/find-key-values-sidesheet/translations/carrier-rules.ts
@@ -0,0 +1,31 @@
+import { LineOfBusiness, ProductType } from '@zinnia/api-types/types/sor';
+
+type ExceptionalPlanCode = string;
+
+const LIFE_ONLY = new Set([LineOfBusiness.LIFE]);
+const ANNUITY_ONLY = new Set([LineOfBusiness.ANNUITY]);
+const UL_IUL_TR0101 = new Set([
+    ProductType.UNIVERSALLIFE,
+    ProductType.INDEXEDUNIVERSALLIFE,
+    'TR0101',
+]);
+const ANNUITY_UL_IUL = new Set([
+    LineOfBusiness.ANNUITY,
+    ProductType.UNIVERSALLIFE,
+    ProductType.INDEXEDUNIVERSALLIFE,
+]);
+/**
+ * Fields that should not be displayed
+ */
+export const sectionVisibility: Record<
+    string,
+    Set<LineOfBusiness | ProductType | ExceptionalPlanCode> | undefined
+> = {
+    // policy
+    loans: UL_IUL_TR0101,
+    marketValueAdjustment: ANNUITY_ONLY,
+    withdrawalValues: ANNUITY_UL_IUL,
+    testValues: LIFE_ONLY,
+    requiredMinimumDistribution: ANNUITY_ONLY,
+    combinedFunds: ANNUITY_UL_IUL,
+};
