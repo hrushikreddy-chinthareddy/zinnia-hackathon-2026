4b84a0205b9c257c3dfae7a69b29793282383a16
matt <matt.roe@zinnia.com>
2025-12-02 13:28:41 -0700

    remove unused type

--- apps/ops/src/components/find-key-values-sidesheet/types.ts
+++ apps/ops/src/components/find-key-values-sidesheet/types.ts
@@ -34,8 +34,6 @@ export const DocumentFormat = {
 export type DocumentFormatType =
     (typeof DocumentFormat)[keyof typeof DocumentFormat];
 
-export type CustomSectionGroups = [string, string[]];
-
 export type DataField = {
     type: typeof FieldType.field;
     label: string;
