583f6a410b9cdd3466a37c69936900812c8022a9
Christian Bedón <christian.bedonvillalobos@zinnia.com>
2025-12-12 14:05:27 -0500

    fix: no results with same date filter, relative path, and splitting context from provider file

--- apps/ops/src/components/dashboard/sections/tasks-volume/tab-content/shared/tasks-volume-filters.tsx
+++ apps/ops/src/components/dashboard/sections/tasks-volume/tab-content/shared/tasks-volume-filters.tsx
@@ -1,11 +1,10 @@
-import { FC, useContext } from 'react';
+import { FC } from 'react';
 
 import sharedStyles from '@deps/components/dashboard/dashboard-shared.module.css';
 import { TaskStatusFilter } from '@deps/components/dashboard/filters/task-status-filter';
 import { TimeFilter } from '@deps/components/dashboard/filters/time-filter/time-filter';
-import { TasksVolumeContext } from '@deps/components/dashboard/sections/tasks-volume/context/tasks-volume-context';
-
-import { TaskVolumeTimeframeOptions } from '../../utils';
+import { useTasksVolume } from '@deps/components/dashboard/sections/tasks-volume/context/tasks-volume-context';
+import { TaskVolumeTimeframeOptions } from '@deps/components/dashboard/sections/tasks-volume/utils';
 
 export const TasksVolumeFilters: FC = () => {
     const {
@@ -13,7 +12,7 @@ export const TasksVolumeFilters: FC = () => {
         handleTimeframeRadioChange,
         timerange,
         handleRangeChange,
-    } = useContext(TasksVolumeContext);
+    } = useTasksVolume();
 
     return (
         <div className={sharedStyles.filterContainer}>
