1dc150a98308864b1567bedf798a80864ce07b7b
Christian Bedón <christian.bedonvillalobos@zinnia.com>
2025-12-15 18:12:54 -0500

    fix: missing processes cause All fallback

--- apps/ops/src/models/case/case.ts
+++ apps/ops/src/models/case/case.ts
@@ -143,6 +143,13 @@ export enum Processes {
     FreeLookCancellation = 'Free Look Cancellation',
     PartialWithdrawal = 'Partial Withdrawal',
     FullSurrender = 'Full Surrender',
+    InitialPremium = 'Initial Premium',
+    Claims = 'Claims',
+    AgentDelegatoin = 'Agent Delegation',
+    DeathAudit = 'Death Audit',
+    Matching = 'Matching',
+    Reconciliation = 'Reconciliation',
+    OperationsReview = 'Operations Review',
 }
 
 export enum Actions {
