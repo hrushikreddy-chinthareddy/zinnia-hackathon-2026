181f8a47beb0a5fcf8fb006294c8ba92c1fde70a
--global <153061769+vijaya-zinnia@users.noreply.github.com>
2025-12-22 23:30:21 +0530

    DEPU-8389 context data code fixes

--- apps/ops/src/containers/task-container/components/steps/task-form/task-form.tsx
+++ apps/ops/src/containers/task-container/components/steps/task-form/task-form.tsx
@@ -251,7 +251,7 @@ export const TaskForm = React.forwardRef(function TaskFormComponent(
                 await fetchData();
             } else {
                 const taskPayload = buildTaskPayload(
-                    cleanForm({ ...task, ...customData?.task }, taskMetadata),
+                    cleanForm(task, taskMetadata),
                     initialTask
                 );
                 const success = await updateTask(
