08c3b32e11957a5572106ca9c31bcb25fa867726
gunjan <gunjan2@zinnia.com>
2026-01-01 13:43:42 +0530

    cancel freelook

--- apps/ops/src/pages/policies/[planCode]/[id]/policy/freelook/cancel-freelook.tsx
+++ apps/ops/src/pages/policies/[planCode]/[id]/policy/freelook/cancel-freelook.tsx
@@ -1,4 +1,3 @@
-import { useQuery } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
@@ -7,49 +6,26 @@ import PageLoader, {
 } from '@deps/components/page-loader/page-loader';
 import { PageHead } from '@deps/components/page-title';
 import FreeLookCancelContainer from '@deps/containers/financial-transactions/free-look-cancel/free-look-cancel-container';
+import { useOptimizely } from '@deps/contexts/OptimizelyContext';
 import { WithdrawalProvider } from '@deps/contexts/transactions/WithdrawalContext';
 import { useTransactionPermissionCheck } from '@deps/hooks/useTransactionPermissionCheck';
-import { TransactionResponseStatus } from '@deps/queries/api/bpm';
-import {
-    checkPartialWithdrawalOneTimeEligibilityQuery,
-    checkFullSurrenderWithdrawal,
-} from '@deps/queries/tanstack/checkEligibilityQueries/checkEligibilityQueries';
 import { TransactionPermission } from '@deps/utils/auth';
+import { FEATURE_FLAGS } from '@deps/utils/optimizely/flags';
 import { getServerSidePropsPolicyDetailsPage } from '@deps/utils/page';
 import { withPageAuthAndLogging } from '@deps/utils/server-logging';
 import { Policy } from '@zinnia/api-types/types/sor';
 
 const CancelFreeLook = ({ policy }: { policy: Policy }) => {
     const router = useRouter();
+    const { featureFlags } = useOptimizely();
+
+    const freeLookEnabled =
+        featureFlags[FEATURE_FLAGS.POLICY_FREE_LOOK_CANCELLATION];
 
     const [isLoading, setIsLoading] = useState(true);
 
     const { policyNumber } = policy;
     const { planCode } = policy.product ?? {};
-
-    const {
-        data: partialWithdrawalOneTimeEligibility,
-        isFetched: isPartialWithdrawalOneTimeEligibilityFetched,
-    } = useQuery({
-        queryKey: [
-            'checkPartialWithdrawalOneTimeEligibility',
-            planCode,
-            policyNumber,
-        ],
-        queryFn: () =>
-            checkPartialWithdrawalOneTimeEligibilityQuery(
-                planCode as string,
-                policyNumber as string
-            ),
-        placeholderData: (previousData) => previousData,
-        select: (data) => {
-            return {
-                ...data,
-                isEligiblePartialWithdrawalOneTime:
-                    data?.status === TransactionResponseStatus.Success,
-            };
-        },
-    });
 
     const { isPermissioned: isUserPermissionedToWithdraw } =
         useTransactionPermissionCheck(
@@ -58,54 +34,23 @@ const CancelFreeLook = ({ policy }: { policy: Policy }) => {
             planCode
         );
 
-    const {
-        data: fullSurrenderEligibility,
-        isFetched: isFullSurrenderEligibilityFetched,
-    } = useQuery({
-        queryKey: ['checkFullSurrenderEligibility', planCode, policyNumber],
-        queryFn: () =>
-            checkFullSurrenderWithdrawal(
-                planCode as string,
-                policyNumber as string
-            ),
-        placeholderData: (previousData) => previousData,
-        select: (data) => {
-            return {
-                ...data,
-                isEligibleFullSurrender:
-                    data?.status === TransactionResponseStatus.Success,
-            };
-        },
-    });
-
     useEffect(() => {
-        if (
-            !isPartialWithdrawalOneTimeEligibilityFetched ||
-            !isFullSurrenderEligibilityFetched
-        )
-            return;
+        console.log('Eligibility Checks:', {
+            freeLookEnabled,
+            isUserPermissionedToWithdraw,
+        });
 
-        const isEligible =
-            partialWithdrawalOneTimeEligibility?.isEligiblePartialWithdrawalOneTime ||
-            false;
-        const isEligibleFullSurrender =
-            fullSurrenderEligibility?.isEligibleFullSurrender || false;
+        // Wait until the permission state is defined
+        if (typeof isUserPermissionedToWithdraw !== 'undefined') {
+            const isPermissioned = isUserPermissionedToWithdraw;
 
-        const isPermissioned = isUserPermissionedToWithdraw || false;
-
-        if (!isEligible || !isEligibleFullSurrender || !isPermissioned) {
-            router.replace('/403');
-        } else {
-            setIsLoading(false);
+            if (!freeLookEnabled || !isPermissioned) {
+                router.replace('/403');
+            } else {
+                setIsLoading(false);
+            }
         }
-    }, [
-        partialWithdrawalOneTimeEligibility,
-        isUserPermissionedToWithdraw,
-        router,
-        fullSurrenderEligibility,
-        isPartialWithdrawalOneTimeEligibilityFetched,
-        isFullSurrenderEligibilityFetched,
-    ]);
+    }, [freeLookEnabled, isUserPermissionedToWithdraw, router]);
 
     if (isLoading) {
         return <PageLoader variant={PageLoaderVariant.Center} />;
