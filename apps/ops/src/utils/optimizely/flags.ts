c8fe67edff47d985f1379e67792dcb26569bd6f6
Krishna Agarwal <krishna.agarwal@zinnia.com>
2026-01-05 22:14:49 +0530

    fixing a prod bug

--- apps/ops/src/utils/optimizely/flags.ts
+++ apps/ops/src/utils/optimizely/flags.ts
@@ -189,4 +189,5 @@ export enum FEATURE_FLAGS {
     FINANCIAL_TRANSACTION = 'financial-transaction',
     DTCC_SECTION_ENABLED = 'dtcc_section_enabled',
     DASHBOARD_COMPLETED_TASK_TIME_TABLE = 'dashboard_completed_task_time_table',
+    ENABLE_BENE_CHANGE_PAPER_FORM_SUMMARY_SCREEN = 'enable_bene_change_paper_form_summary_screen',
 }
