c96192ace4c9445239255f314df551c3550998fd
gunjan <gunjan2@zinnia.com>
2026-01-05 18:14:18 +0530

    withdrawal autopay and RMD links fixes

--- apps/ops/src/pages/policies/[planCode]/[id]/policy/withdrawals/new-withdrawal-autopay.tsx
+++ apps/ops/src/pages/policies/[planCode]/[id]/policy/withdrawals/new-withdrawal-autopay.tsx
@@ -12,15 +12,14 @@ import { useOptimizely } from '@deps/contexts/OptimizelyContext';
 import { AutopayProvider } from '@deps/contexts/transactions/AutopayContext';
 import { formatValidationResult } from '@deps/helpers/bpm-transaction.helpers';
 import { useTransactionPermissionCheck } from '@deps/hooks/useTransactionPermissionCheck';
-import { Reason, Status } from '@deps/models/policy/sor-policy';
+import { Status } from '@deps/models/policy/sor-policy';
 import { TransactionResponseStatus } from '@deps/queries/api/bpm';
 import { checkSystematicProgramEligibilityQuery } from '@deps/queries/tanstack/checkEligibilityQueries/checkEligibilityQueries';
 import { TransactionPermission } from '@deps/utils/auth';
 import { FEATURE_FLAGS } from '@deps/utils/optimizely/flags';
 import { getServerSidePropsPolicyDetailsPage } from '@deps/utils/page';
 import { withPageAuthAndLogging } from '@deps/utils/server-logging';
-import { ArrangementType } from '@zinnia/api-types/types/bpm';
-import { Policy } from '@zinnia/api-types/types/sor';
+import { ArrangementType, Policy, Reason } from '@zinnia/api-types/types/sor';
 
 export interface WithdrawalAutopayProps {
     policy: Policy;
@@ -184,7 +183,7 @@ const NewWithdrawalAutoPay = ({ policy }: WithdrawalAutopayProps) => {
                 arrangementType={arrangementType}
                 policy={policy}
                 parentPage={ParentPage.Withdrawals}
-                systematicProgramReason={undefined}
+                systematicProgramReason={reason}
                 translationKeyPrefix="withdrawalAutopay"
                 isSetUp={true}
             />
