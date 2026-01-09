620efe774e5f61ee84f346b7dad2032f6bfe8b0a
Rocco Sangellino <rocco.sangellino@zinnia.com>
2026-01-09 13:41:33 -0700

    feat: add TypeScript declaration for zen-order-entry web component
    - Add JSX intrinsic element type definition for zen-order-entry custom element
    - Enable TypeScript support for ZEmbed web component in React

--- apps/ops/src/types/zembed.d.ts
+++ apps/ops/src/types/zembed.d.ts
@@ -6,3 +6,12 @@ declare module 'https://d1f4og57rdeq5x.cloudfront.net/zembed/v1.0.0/zembed-boots
         accessToken: () => Promise<string>;
     }): Promise<void>;
 }
+
+declare namespace JSX {
+    interface IntrinsicElements {
+        'zen-order-entry': React.DetailedHTMLProps<
+            React.HTMLAttributes<HTMLElement>,
+            HTMLElement
+        >;
+    }
+}
