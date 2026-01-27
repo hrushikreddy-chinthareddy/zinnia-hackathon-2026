e6782199a65cc3dc2ee82dc586d5b6a43dbac24f
Aarti-Zinnia123 <aarti.yadav1@zinnia.com>
2026-01-27 17:33:16 +0530

    work on task instance

--- apps/ops/src/models/case/task-instance.ts
+++ apps/ops/src/models/case/task-instance.ts
@@ -154,8 +154,8 @@ export interface TaskSideSheetProps {
     taskDescription?: string;
     taskName?: string;
     mappedDocuments?: TaskDocument[];
-    queue: string;
-    carrier: string;
+    queue?: string;
+    carrier?: string;
     onTaskClaimSuccess?: () => void;
     onTaskUpdated?: (updatedTask: Task) => void;
 }
