2450a872a49201ba20ddd0cb25ff1e6de2d5214c
Anshansh Suman <anshansh.suman@zinnia.com>
2026-02-02 19:53:48 +0530

    Agent displays duplicate SSN in Zinnia Live fixed

--- apps/ops/src/helpers/policy-sor/PomAgentParty.ts
+++ apps/ops/src/helpers/policy-sor/PomAgentParty.ts
@@ -14,20 +14,34 @@ export const transformPomAgentDataToParty = (
     // TODO: replace PomAgentData type with POM_Producer_Models_SearchProducersResult
     agentData: PomAgentData | undefined,
     partyData: Parties
-): Parties & {
-    producerType: string | undefined;
-    producerName: string | undefined;
-} => {
-    const party: Parties & {
-        producerType: string | undefined;
-        producerName: string | undefined;
-    } = {
+): Parties => {
+    // Filter out SSN and NPN from partyData if agent has them (agent data takes precedence)
+    // This prevents duplicate identifications when both sources have the same type
+    const filteredPartyIdentifications =
+        partyData?.identifications?.filter((id) => {
+            // If agent has SSN, filter out any existing SSNs from partyData
+            if (
+                agentData?.socialSecurityNumber &&
+                id.identificationType === Identification.identificationType.SSN
+            ) {
+                return false;
+            }
+            // If agent has NPN, filter out any existing NPNs from partyData
+            if (
+                agentData?.nationalProducerNumber &&
+                id.identificationType ===
+                    ('NPN' as Identification.identificationType)
+            ) {
+                return false;
+            }
+            return true;
+        }) ?? [];
+
+    const party: Parties = {
         ...partyData,
         firstName: agentData?.firstName,
         lastName: agentData?.lastName,
         middleName: agentData?.middleName,
-        producerType: agentData?.producerType,
-        producerName: agentData?.producerName,
         addresses: [
             {
                 addressLine1: agentData?.businessAddress.line || undefined,
@@ -60,18 +74,29 @@ export const transformPomAgentDataToParty = (
         ],
         partyId: agentData?.partyId || undefined,
         identifications: [
-            {
-                identificationType: Identification.identificationType.SSN,
-                identificationValue:
-                    agentData?.socialSecurityNumber || undefined,
-            },
-            {
-                // identificationType: IdentificationType.NPN, TODO: update to this instead of string when kong updates
-                identificationType: 'NPN' as Identification.identificationType,
-                identificationValue:
-                    agentData?.nationalProducerNumber || undefined,
-            },
-            ...(partyData.identifications ?? []),
+            // Add agent's SSN if available
+            ...(agentData?.socialSecurityNumber
+                ? [
+                      {
+                          identificationType:
+                              Identification.identificationType.SSN,
+                          identificationValue: agentData.socialSecurityNumber,
+                      },
+                  ]
+                : []),
+            // Add agent's NPN if available
+            ...(agentData?.nationalProducerNumber
+                ? [
+                      {
+                          // identificationType: IdentificationType.NPN, TODO: update to this instead of string when kong updates
+                          identificationType:
+                              'NPN' as Identification.identificationType,
+                          identificationValue: agentData.nationalProducerNumber,
+                      },
+                  ]
+                : []),
+            // Add remaining identifications from partyData (SSN/NPN already filtered out if agent has them)
+            ...filteredPartyIdentifications,
         ],
     };
     return party;
