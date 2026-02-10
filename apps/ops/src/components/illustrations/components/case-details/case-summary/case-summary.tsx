08fbd5814a92f9473cf8e8f0f137897ddef7546c
wesitos-globant <pedro.palaciosavila@zinnia.com>
2026-02-10 16:01:29 -0500

    [ZDR-4970] Fix DoB timezone handling on client case creation (#4115)

--- apps/ops/src/components/illustrations/components/case-details/case-summary/case-summary.tsx
+++ apps/ops/src/components/illustrations/components/case-details/case-summary/case-summary.tsx
@@ -137,13 +137,6 @@ const IllustrationCaseSumary = ({
     });
 
     const onSubmitForm = (clientCaseData: Partial<IllustrationsClientCase>) => {
-        // Discart this when date input is replaced with the final verstion of the date picker.
-        if (clientCaseData.insuredDetails) {
-            clientCaseData.insuredDetails.dateOfBirth = new Date(
-                clientCaseData.insuredDetails?.dateOfBirth ?? ''
-            );
-        }
-
         mutate(clientCaseData);
         closeSideSheet();
     };
