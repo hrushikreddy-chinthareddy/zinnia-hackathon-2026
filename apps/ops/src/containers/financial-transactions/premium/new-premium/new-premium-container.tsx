c901127b31526aac8b94850f6f565592414ce156
RahulNarayanasamy-Zinnia <Rahul.Narayanasamy@zinnia.com>
2026-02-17 18:43:50 +0530

    fix(DEPU-9170): Added codes for showing first step for OTP when there is one case (#4184)

--- apps/ops/src/containers/financial-transactions/premium/new-premium/new-premium-container.tsx
+++ apps/ops/src/containers/financial-transactions/premium/new-premium/new-premium-container.tsx
@@ -1,7 +1,11 @@
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import { useTranslation } from 'next-i18next';
+import { useCallback, useMemo } from 'react';
 
+import PageLoader, {
+    PageLoaderVariant,
+} from '@deps/components/page-loader/page-loader';
 import { ParentPage } from '@deps/components/transaction-navigation-buttons/transaction-navigation-buttons';
 import PaymentStep from '@deps/components/workflows/payment-step/payment-step';
 import { PaymentStepSetState } from '@deps/components/workflows/payment-step/types';
@@ -19,6 +23,7 @@ import { checkCustomPolicy } from '@deps/containers/policy-details/policy-detail
 import { Step } from '@deps/containers/progress-bar-steps/progress-bar-steps-item/progress-bar-steps-item';
 import WorkflowContainer from '@deps/containers/workflow-container/workflow-container';
 import { usePremium } from '@deps/contexts/transactions/NewPremiumContext';
+import { useCasesQuery } from '@deps/hooks/useCasesQuery';
 import { Processes } from '@deps/models/case/case';
 import { validateOneTimePremium } from '@deps/queries/api/bpm';
 import { TransactionStep } from '@deps/types/segment-analytics';
@@ -43,15 +48,22 @@ const NewPremiumContainer = ({ policy }: NewPremiumContainerProps) => {
     const summaryLabel = t('newPremium.summary.label');
     const confirmLabel = t('newPremium.confirm.label');
 
-    const validateCall = () => {
-        const query = buildNewPremiumRequestBody(premium);
+    const { data: casesResponse, isLoading } = useCasesQuery({
+        policyNumber: policy.policyNumber,
+        process: [Processes.OneTimePremium],
+        enabled: !!policy.policyNumber,
+    });
+    const hasCases =
+        Array.isArray(casesResponse?.data) && casesResponse.data.length > 0;
 
+    const validateCall = useCallback(() => {
+        const query = buildNewPremiumRequestBody(premium);
         return validateOneTimePremium(
             policy.product?.planCode,
             policy.policyNumber,
             query
         );
-    };
+    }, [premium, policy]);
 
     const customFarmerCheck = checkCustomPolicy(
         policy,
@@ -59,88 +71,126 @@ const NewPremiumContainer = ({ policy }: NewPremiumContainerProps) => {
         FarmersPlanCodes
     );
 
-    const steps: Step[] = [
-        {
-            component: (
-                <StartStep
-                    parentPage={ParentPage.Premiums}
-                    policy={policy}
-                    processType={Processes.OneTimePremium}
-                    setState={setPremium as StartStepSetState}
-                    state={premium}
-                    title={t('newPremium.start.title') as string}
-                    subtitle={t('newPremium.start.subtitle') as string}
-                    trackEventProps={{
-                        type: Transaction.transactionType
-                            .PAYMENT_ONE_TIME_PREMIUM,
-                        step: TransactionStep.Start,
-                    }}
-                />
-            ),
-            screenReaderLabel: startLabel,
-            index: 0,
-            text: startLabel,
-        },
-        {
-            component: (
-                <Amount policy={policy} customFarmerCheck={customFarmerCheck} />
-            ),
-            screenReaderLabel: amountLabel,
-            index: 1,
-            text: amountLabel,
-        },
-        {
-            component: (
-                <PayorStep
-                    parentPage={ParentPage.Premiums}
-                    policy={policy}
-                    setState={setPremium as PayorStepSetState}
-                    state={premium}
-                    trackEventProps={{
-                        type: Transaction.transactionType
-                            .PAYMENT_ONE_TIME_PREMIUM,
-                        step: TransactionStep.Payor,
-                    }}
-                />
-            ),
-            screenReaderLabel: payorLabel,
-            index: 2,
-            text: payorLabel,
-        },
-        {
-            component: (
-                <PaymentStep
-                    parentPage={ParentPage.Premiums}
-                    policy={policy}
-                    setState={setPremium as unknown as PaymentStepSetState}
-                    state={premium}
-                    validateTransaction={validateCall}
-                    trackEventProps={{
-                        type: Transaction.transactionType
-                            .PAYMENT_ONE_TIME_PREMIUM,
-                        step: TransactionStep.Payment,
-                    }}
-                />
-            ),
-            screenReaderLabel: paymentLabel,
-            index: 3,
-            text: paymentLabel,
-        },
-        {
-            component: <Summary policy={policy} />,
-            screenReaderLabel: summaryLabel,
-            index: 4,
-            text: summaryLabel,
-        },
-        {
-            component: <Confirm policy={policy} />,
-            screenReaderLabel: confirmLabel,
-            index: 5,
-            text: confirmLabel,
-        },
-    ];
+    const steps: Step[] = useMemo(
+        () => [
+            {
+                component: (
+                    <StartStep
+                        parentPage={ParentPage.Premiums}
+                        policy={policy}
+                        processType={Processes.OneTimePremium}
+                        setState={setPremium as StartStepSetState}
+                        state={premium}
+                        title={t('newPremium.start.title') as string}
+                        subtitle={t('newPremium.start.subtitle') as string}
+                        trackEventProps={{
+                            type: Transaction.transactionType
+                                .PAYMENT_ONE_TIME_PREMIUM,
+                            step: TransactionStep.Start,
+                        }}
+                    />
+                ),
+                screenReaderLabel: startLabel,
+                index: 0,
+                text: startLabel,
+                isVisible: () => hasCases,
+            },
+            {
+                component: (
+                    <Amount
+                        policy={policy}
+                        customFarmerCheck={customFarmerCheck}
+                    />
+                ),
+                screenReaderLabel: amountLabel,
+                index: 1,
+                text: amountLabel,
+                isVisible: () => true,
+            },
+            {
+                component: (
+                    <PayorStep
+                        parentPage={ParentPage.Premiums}
+                        policy={policy}
+                        setState={setPremium as PayorStepSetState}
+                        state={premium}
+                        trackEventProps={{
+                            type: Transaction.transactionType
+                                .PAYMENT_ONE_TIME_PREMIUM,
+                            step: TransactionStep.Payor,
+                        }}
+                    />
+                ),
+                screenReaderLabel: payorLabel,
+                index: 2,
+                text: payorLabel,
+                isVisible: () => true,
+            },
+            {
+                component: (
+                    <PaymentStep
+                        parentPage={ParentPage.Premiums}
+                        policy={policy}
+                        setState={setPremium as unknown as PaymentStepSetState}
+                        state={premium}
+                        validateTransaction={validateCall}
+                        trackEventProps={{
+                            type: Transaction.transactionType
+                                .PAYMENT_ONE_TIME_PREMIUM,
+                            step: TransactionStep.Payment,
+                        }}
+                    />
+                ),
+                screenReaderLabel: paymentLabel,
+                index: 3,
+                text: paymentLabel,
+                isVisible: () => true,
+            },
+            {
+                component: <Summary policy={policy} />,
+                screenReaderLabel: summaryLabel,
+                index: 4,
+                text: summaryLabel,
+                isVisible: () => true,
+            },
+            {
+                component: <Confirm policy={policy} />,
+                screenReaderLabel: confirmLabel,
+                index: 5,
+                text: confirmLabel,
+                isVisible: () => true,
+            },
+        ],
+        [
+            policy,
+            setPremium,
+            premium,
+            t,
+            startLabel,
+            amountLabel,
+            payorLabel,
+            paymentLabel,
+            summaryLabel,
+            confirmLabel,
+            hasCases,
+            customFarmerCheck,
+            validateCall,
+        ]
+    );
 
-    return <WorkflowContainer policy={policy} steps={steps} />;
+    const filteredSteps: Step[] = useMemo(
+        () =>
+            steps
+                .filter((item) => item.isVisible?.())
+                .map((item, index) => ({ ...item, index })),
+        [steps]
+    );
+
+    return isLoading ? (
+        <PageLoader variant={PageLoaderVariant.Center} />
+    ) : (
+        <WorkflowContainer policy={policy} steps={filteredSteps} />
+    );
 };
 
 export default NewPremiumContainer;
