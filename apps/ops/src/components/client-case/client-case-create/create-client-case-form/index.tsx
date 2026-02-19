246b1456dd1b4a0b23e744bf9e3c689a08771d26
Jeff Park <jeff.park@zinnia.com>
2026-02-19 15:10:00 -0700

    QUILL-99 added a comment

--- apps/ops/src/components/client-case/client-case-create/create-client-case-form/index.tsx
+++ apps/ops/src/components/client-case/client-case-create/create-client-case-form/index.tsx
@@ -150,6 +150,7 @@ const CreateClientCaseForm: React.FC<CreateClientCaseFormProps> = ({
         [dateOfBirth]
     );
 
+    //Need to have currentAge > 0 cause it's computed to be 0 initially
     const displayNicotineSection = !(currentAge > 0 && currentAge < 18);
 
     const isFetchingAgencies =
