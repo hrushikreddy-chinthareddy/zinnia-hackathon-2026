76a3cf84cd3c30ffbad1c7243f1a9b8fbfbbfc53
Dave Schwartz <dave.schwartz@zinnia.com>
2026-02-23 12:11:32 -0500

    [DEPU-9176] Some Cleanup

--- apps/ops/vitest/stubs/next-i18next.ts
+++
@@ -1,3 +0,0 @@
-// next-i18next uses its own i18next instance in non-Next.js environments;
-// delegate to react-i18next so it reads from I18nextProvider instead.
-export { useTranslation } from 'react-i18next';
