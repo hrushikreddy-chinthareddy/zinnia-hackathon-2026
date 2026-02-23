4ee2f30b8713f9949bfccf0c6f3b70f1a8852954
sweta-zinnia <sweta.kumari@zinnia.com>
2026-02-23 19:06:15 +0530

    added feature flag for owner mismatch (#4175)
    Co-authored-by: Krishna Agarwal <krishna.agarwal@zinnia.com>

--- apps/ops/src/containers/task-container/steps-helper/bene-change-steps.tsx
+++ apps/ops/src/containers/task-container/steps-helper/bene-change-steps.tsx
@@ -1,9 +1,16 @@
+import { TabTitle } from '@deps/components/dynamic-form/customization/templates/transaction-accordion/types';
 import { FEATURE_FLAGS } from '@deps/utils/optimizely/flags';
 
 import { GetStepsProps, StepTitle } from './types';
 import { Step } from '../../progress-bar-steps/progress-bar-steps-item/progress-bar-steps-item';
 import ConfirmStep from '../components/steps/confirm/confirm-step';
 import { MemoizedTaskFormStep as TaskFormStep } from '../components/steps/task-form/task-form-step';
+
+enum IssueResolution {
+    Yes = 'yes',
+    NoMissing = 'no_missing',
+    NoMismatched = 'no_mismatched',
+}
 
 export const getBeneChangeSteps = ({
     taskInfoLink,
@@ -20,25 +27,52 @@ export const getBeneChangeSteps = ({
         featureFlags[
             FEATURE_FLAGS.ENABLE_BENE_CHANGE_PAPER_FORM_SUMMARY_SCREEN
         ];
+    const isOwnerMismatchEnabled =
+        featureFlags[FEATURE_FLAGS.BENE_CHANGE_OWNER_MISMATCH];
 
-    const filteredTaskMetadata = taskMetadata.filter(
-        (item) => item.title !== StepTitle.Summary
-    );
-    const dynamicSteps = (
-        isBeneChangePaperFormSummaryEnabled
-            ? taskMetadata
-            : filteredTaskMetadata
-    ).map((metadata, index) => ({
+    const issueResolvedArray = Array.isArray(isIssueResolved)
+        ? isIssueResolved
+        : [];
+    const isNigoCase =
+        issueResolvedArray.includes(IssueResolution.NoMissing) ||
+        issueResolvedArray.includes(IssueResolution.NoMismatched) ||
+        !isIssueResolved;
+
+    const isSuccessCase = isOwnerMismatchEnabled
+        ? issueResolvedArray.includes(IssueResolution.Yes)
+        : !!isIssueResolved;
+
+    const baseMetadata = isOwnerMismatchEnabled
+        ? taskMetadata.filter((item) => item.title !== TabTitle.ReviewFormData)
+        : taskMetadata.filter((item) => item.title !== TabTitle.FormReview);
+
+    const filteredMetadata = isBeneChangePaperFormSummaryEnabled
+        ? baseMetadata
+        : baseMetadata.filter((item) => item.title !== StepTitle.Summary);
+
+    const dynamicSteps = filteredMetadata.map((metadata, index) => ({
         ariaLabel: metadata?.title || '',
-        isVisible: () => index === 0 || isIssueResolved,
+        isVisible: () => {
+            if (index === 0) return true;
+
+            if (metadata.title === TabTitle.NIGOSummary) {
+                return isOwnerMismatchEnabled && isNigoCase;
+            }
+
+            if (metadata.title === TabTitle.OwnerDetails) {
+                return !isOwnerMismatchEnabled && isSuccessCase;
+            }
+
+            return isSuccessCase;
+        },
         component: (
             <TaskFormStep
                 readonly={readOnly}
                 taskInfoLink={taskInfoLink}
                 isSubmit={
-                    (index === (isBeneChangePaperFormSummaryEnabled ? 4 : 3) &&
-                        !readOnly) ||
-                    !isIssueResolved
+                    metadata.title === TabTitle.NIGOSummary ||
+                    metadata.title === StepTitle.Summary ||
+                    (index === (filteredMetadata?.length ?? 1) - 1 && !readOnly)
                 }
                 taskMetadata={metadata}
                 key={`step_${index}`}
