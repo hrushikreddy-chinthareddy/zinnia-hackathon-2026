d3394d8dabe1e242ef043913ea9353747794781a
Alex Sagalovskiy <alex.sagalovskiy@zinnia.com>
2026-03-10 20:41:07 -0600

    fixed typo

--- apps/ops/src/components/dynamic-form/customization/templates/party-info-list-template/party-info-list.tsx
+++ apps/ops/src/components/dynamic-form/customization/templates/party-info-list-template/party-info-list.tsx
@@ -126,7 +126,7 @@ export default function PartyInfoListTemplate(
                             <Icon type={IconType.ADD} small />
                             <>{addButtonCTA}</>
                         </button>
-                    }
+                    )}
                 </div>
             </div>
 
