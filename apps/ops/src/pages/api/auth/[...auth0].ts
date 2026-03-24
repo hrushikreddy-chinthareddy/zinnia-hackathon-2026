8bbbf04191560b0507b28c2dbf2db36f16d2a6b1
Wassim Mouawad <wassim.mouawad@zinnia.com>
2026-03-24 18:18:41 -0400

    returnTo without slash test

--- apps/ops/src/pages/api/auth/[...auth0].ts
+++ apps/ops/src/pages/api/auth/[...auth0].ts
@@ -169,6 +169,10 @@ export default handleAuth({
 
     async logout(req, res) {
         deleteCookie('role', { req, res });
-        await handleLogout(req, res);
+
+        const baseUrl = process.env.AUTH0_BASE_URL;
+        const returnTo = baseUrl?.endsWith('/') ? baseUrl : `${baseUrl}/`;
+
+        await handleLogout(req, res, { returnTo });
     },
 });
